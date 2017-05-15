f30b921c34544331328bea50b635046b1674c609
gogogogi <trebelnik2@gmail.com>
2017-05-15 18:38:59 +0200

    Screenshot+record desktop@tech71 fix (#643)
    * Fix Videos and Pictures folder not opening, fix recording scripts, new screenshot
    
    * Remove version

--- ScreenShot+RecordDesktop@tech71/files/ScreenShot+RecordDesktop@tech71/screencapturesound.sh
+++ ScreenShot+RecordDesktop@tech71/files/ScreenShot+RecordDesktop@tech71/screencapturesound.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [ ! -e $HOME/Videos ]
-then
-mkdir -p $HOME/Videos
-fi
-dir="$HOME/Videos"
+set -e
+
+. "$HOME/.config/user-dirs.dirs"
+###   Date and time format   ###
+#format="$( date +'%b-%d-%Y_%I:%M:%S%#p' )"   ## 12-Hour time
+format="$( date +'%b-%d-%Y_%H:%M:%S%#p' )"   ## 24-Hour time
 size=$( xdpyinfo | grep 'dimensions:' | awk '{print $2}' )
-name=$( date +'%b.%d_%I:%M%#p' )
-video="$dir/$name.mkv"
-ffmpeg -f alsa -ac 2 -i pulse -f x11grab -s $size -r 30 -qscale 1  -i :0.0 $video
+name=Cinnamon-$format
+video="$XDG_VIDEOS_DIR/$name.mkv"
+ffmpeg -f alsa -ac 2 -i pulse -f x11grab -s $size -r 30 -i :0.0 -qscale 1 $video
