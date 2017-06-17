6e93802a6a91b6946adaf5bda1103201e52cef15
cardsurf <cardsurf@mail.com>
2017-06-17 10:23:14 +0200

    [hideable-applets@cardsurf] Add a new applet

---
+++ hideable-applets@cardsurf/files/hideable-applets@cardsurf/appletConstants.js
@@ -0,0 +1,17 @@
+
+const ActionType = {
+    TOGGLE_POPUP: 0,
+    TOGGLE_APPLETS_SIDE: 1
+}
+
+const ActionTriggerType = {
+    CLICK: 0,
+    HOVER: 1,
+    NONE: 2
+}
+
+const GuiSortType = {
+    ALPHABETICALLY: 0,
+    PANEL_POSITION: 1
+}
+
