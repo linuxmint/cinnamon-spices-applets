bdcea4912c5d24877e718a689611f5ec5c4b7f47
cardsurf <cardsurf@users.noreply.github.com>
2017-10-08 07:44:46 +0200

    [disk-read-and-write-speed@cardsurf] Add a new applet (#1359)
    * [disk-read-and-write-speed@cardsurf] Add a new applet
    
    * [disk-read-and-write-speed@cardsurf] Removed unused imports
    
    * [disk-read-and-write-speed@cardsurf] Changed icon size
    
    * [disk-read-and-write-speed@cardsurf] Declared variables

---
+++ disk-read-and-write-speed@cardsurf/files/disk-read-and-write-speed@cardsurf/appletConstants.js
@@ -0,0 +1,29 @@
+
+const DisplayMode = {
+    SPEED: 0,
+    DATA: 1
+}
+
+const UnitType = {
+    BYTES: 0,
+    BITS: 1
+}
+
+const GuiSpeedType = {
+    COMPACT: 0,
+    LARGE: 1
+}
+
+const DecimalPlaces = {
+    AUTO: -1,
+    ZERO: 0,
+    ONE: 1,
+    TWO: 2,
+    THREE: 3
+}
+
+const SortOrder = {
+    ASCENDING: 0,
+    DESCENDING: 1,
+}
+
