c486d969993b29e392b1bcb485a59b84d59ead17
claudiux <claude.clerc@gmail.com>
2017-12-15 18:21:31 +0100

    Del vpnLookOut@claudiux from master branch

--- vpnLookOut@claudiux/files/vpnLookOut@claudiux/scripts/stop_transmission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-RET=1
-if [ `pidof transmission-gtk` != "" ]; then { 
-	RET=0
-	kill -15 `pidof transmission-gtk` & 
-} fi
-
-exit $RET
--- vpnLookOut@claudiux/files/vpnLookOut@claudiux/scripts/vpn_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# This script verifies if vpn interface is connected and writes state ("on" or "off") to temporary files
-# which can be read by the VPNstatus applet
-VPNINTERFACE=$1
-## [[ "x$VPNINTERFACE" == "x" ]] && VPNINTERFACE="tun0"
-cat /proc/net/dev | grep $VPNINTERFACE && (echo -n "on" > /tmp/.vpn_status ; echo -n `nmcli connection show --active | grep vpn | awk '{print$1}'` > /tmp/.vpn_name) || echo -n "off" > /tmp/.vpn_status
-
-exit 0
