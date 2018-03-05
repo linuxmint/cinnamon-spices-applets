45413f84bea0710854f32af5b153fca5d0edffbd
NikoKrause <nikokrause@gmx.de>
2018-03-05 20:41:38 +0100

    clipboard-qr@wrousenel: code cleanup
    fix whitespaces and remove unused variables

--- clipboard-qr@wrouesnel/files/clipboard-qr@wrouesnel/applet.js
+++ clipboard-qr@wrouesnel/files/clipboard-qr@wrouesnel/applet.js
@@ -16,15 +16,11 @@
 */
 const Applet = imports.ui.applet;
 const Lang = imports.lang;
-const Clutter = imports.gi.Clutter;
 const St = imports.gi.St;
 const PopupMenu = imports.ui.popupMenu;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
-const Gdk = imports.gi.Gdk;
 const GUdev = imports.gi.GUdev;
-
-const Main = imports.ui.main;
 
 const AppletDir = imports.ui.appletManager.appletMeta['clipboard-qr@wrouesnel'].path;
 imports.ui.searchPath.unshift(AppletDir);
@@ -37,7 +33,7 @@ const MAX_BYTES = 1048576;
 const Gettext = imports.gettext;
 const UUID = "clipboard-qr@wrouesnel";
 
-Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale")
+Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale");
 
 function _(str) {
   return Gettext.dgettext(UUID, str);
@@ -50,42 +46,42 @@ function MyApplet(orientation) {
 MyApplet.prototype = {
     __proto__: Applet.IconApplet.prototype,
 
-    _init: function(orientation) {        
+    _init: function(orientation) {
         Applet.IconApplet.prototype._init.call(this, orientation);
-        
+
         try {
             // Holds the subprocess reference to a zbarimg
             this._qrprocess = null;
-        
+
             this.menuManager = new PopupMenu.PopupMenuManager(this);
             this.menu = new Applet.AppletPopupMenu(this, orientation);
-            this.menuManager.addMenu(this.menu);            
+            this.menuManager.addMenu(this.menu);
             this.set_applet_tooltip(_("Create QR code from clipboard"));
             this.set_applet_icon_symbolic_name('edit-paste-symbolic');
 
-			// The QR code error string
+            // The QR code error string
             this._errorString = new PopupMenu.PopupMenuItem('', { reactive: false });
             this.menu.addMenuItem(this._errorString);
-            
+
             // The QR code main window
             this._maincontainer = new St.BoxLayout({ style_class: 'qrappletqrcode' });
             this.menu.addActor(this._maincontainer);
-            
+
             // Add a separator
             this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-            
+
             // Link to udev to track connected cameras and provide appropriate
             // capture items on the menu.
             this.camera_menus = {};	// Track which cameras we have in the menu
-            
+
             this.udev_client = new GUdev.Client ({subsystems: ["video4linux"]});
-			this.udev_client.connect ("uevent", Lang.bind(this, this.refresh_cameras));
-			
-			// Populate the menu with an initial list of cameras
-			this.refresh_cameras();
+            this.udev_client.connect ("uevent", Lang.bind(this, this.refresh_cameras));
+
+            // Populate the menu with an initial list of cameras
+            this.refresh_cameras();
 
             this._qr = new QRLib.QR(0, this._maincontainer);
-            
+
             this._maincontainer.add_actor(this._qr.actor);
 
         }
@@ -97,21 +93,21 @@ MyApplet.prototype = {
     _launch_qr_reader: function (camera_path) {
         try {
             if (this._qrprocess == null) {
-            	// Spawn the python helper
+                // Spawn the python helper
                 this._qrprocess = Gio.Subprocess.new(
                     [GLib.build_filenamev([AppletDir,QRReaderHelper]), camera_path],
                     Gio.SubprocessFlags.STDOUT_PIPE);
-				// Read from stdout
+                // Read from stdout
                 let streamOut = this._qrprocess.get_stdout_pipe();
                 streamOut.read_bytes_async(MAX_BYTES, 0, null, Lang.bind(this, function (o,result) {
-                	let data = o.read_bytes_finish(result);
-                	let clipboard = St.Clipboard.get_default();
-			        clipboard.set_text(data.get_data().toString());    
+                    let data = o.read_bytes_finish(result);
+                    let clipboard = St.Clipboard.get_default();
+                    clipboard.set_text(data.get_data().toString());
                 }));
 
                 this._qrprocess.wait_async(null, Lang.bind(this, function(o,result) {
-                	this._qrprocess = null;
-                	o.wait_finish(result);
+                    this._qrprocess = null;
+                    o.wait_finish(result);
                 }));
             }
         } catch (e) {
@@ -119,30 +115,30 @@ MyApplet.prototype = {
         }
     },
 
-	// Dynamically updates the applet menu as cameras are plugged/unplugged
-	refresh_cameras: function () {
-		// Refresh camera list
-		for (item in this.camera_menus) {
-			this.menu.remove(item);
-		}
-		this.camera_menus = {};
-		
-		let camera_devices = this.udev_client.query_by_subsystem ("video4linux");
-		for (var n = 0; n < camera_devices.length; n++) {
-			let devfile = camera_devices[n].get_device_file ();
-			let description = camera_devices[n].get_property('ID_V4L_PRODUCT');
-			
+    // Dynamically updates the applet menu as cameras are plugged/unplugged
+    refresh_cameras: function () {
+        // Refresh camera list
+        for (let item in this.camera_menus) {
+            this.menu.remove(item);
+        }
+        this.camera_menus = {};
+
+        let camera_devices = this.udev_client.query_by_subsystem ("video4linux");
+        for (var n = 0; n < camera_devices.length; n++) {
+            let devfile = camera_devices[n].get_device_file ();
+            let description = camera_devices[n].get_property('ID_V4L_PRODUCT');
+
             let menuItem = new PopupMenu.PopupMenuItem(_("Scan QR Code") + ' (' + description +')');
-            
+
             menuItem.connect('activate', Lang.bind(this, function (menuItem, event) {
-            	this._launch_qr_reader(devfile);
-            	return false;
+                this._launch_qr_reader(devfile);
+                return false;
             }));
-            
+
             this.menu.addMenuItem(menuItem);
             this.camera_menus[devfile+description] = menuItem;
-		}
-	},
+        }
+    },
 
     on_applet_clicked: function(event) {
         let clipboard = St.Clipboard.get_default()
@@ -159,7 +155,7 @@ MyApplet.prototype = {
     }
 };
 
-function main(metadata, orientation) {  
+function main(metadata, orientation) {
     let myApplet = new MyApplet(orientation);
-    return myApplet;      
+    return myApplet;
 }
