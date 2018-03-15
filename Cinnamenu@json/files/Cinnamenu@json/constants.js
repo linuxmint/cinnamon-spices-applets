034a842d36adbdb03ed31e37c9e0618c301cd04b
Jason Hicks <jaszhix@gmail.com>
2018-03-15 11:26:25 -0500

    Cinnamenu@json: Fix exported variables

--- Cinnamenu@json/files/Cinnamenu@json/constants.js
+++ Cinnamenu@json/files/Cinnamenu@json/constants.js
@@ -9,25 +9,25 @@ function _(str) {
   return Gettext.dgettext('Cinnamenu@json', str);
 }
 
-const REMEMBER_RECENT_KEY = 'remember-recent-files';
+var REMEMBER_RECENT_KEY = 'remember-recent-files';
 
-const ApplicationType = {
+var ApplicationType = {
   _applications: 0,
   _places: 1,
   _recent: 2,
   _windows: 3,
   _providers: 4
 };
-const AppTypes = Object.keys(ApplicationType);
+var AppTypes = Object.keys(ApplicationType);
 
-const ApplicationsViewMode = {
+var ApplicationsViewMode = {
   LIST: 0,
   GRID: 1
 };
 
-const fuzzyOptions = {
+var fuzzyOptions = {
   before: '<b><u>',
   after: '</u></b>'
 }
 
-const gridWidths = [0, 0, 0, 625, 700, 725, 900, 1025];
+var gridWidths = [0, 0, 0, 625, 700, 725, 900, 1025];
