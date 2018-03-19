4f1d0b93c1ad0a764c43767d92626b9d41aa0d05
Bruno Meneguello <bruno@meneguello.com>
2018-03-19 16:47:55 -0300

    cryptocoin@guantanamoe: applet icon

--- cryptocoin@guantanamoe/files/cryptocoin@guantanamoe/applet.js
+++ cryptocoin@guantanamoe/files/cryptocoin@guantanamoe/applet.js
@@ -5,6 +5,7 @@ const Lang = imports.lang;
 const PopupMenu = imports.ui.popupMenu;
 const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
 const Util = imports.misc.util;
 const Mainloop = imports.mainloop;
 const Applet = imports.ui.applet;
@@ -19,15 +20,15 @@ function _(str) {
     return Gettext.dgettext(UUID, str);
 }
 
-function MyApplet(metadata, orientation, instance_id) {
-    this._init(metadata, orientation, instance_id);
+function MyApplet(metadata, orientation, panel_height, instance_id) {
+    this._init(metadata, orientation, panel_height, instance_id);
 }
 
 MyApplet.prototype = {
-    __proto__: Applet.TextApplet.prototype,
+    __proto__: Applet.TextIconApplet.prototype,
 
-    _init: function(metadata, orientation, instance_id) {
-        Applet.TextApplet.prototype._init.call(this, orientation, instance_id);
+    _init: function(metadata, orientation, panel_height, instance_id) {
+        Applet.TextIconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
 
         try {
             this.httpSession = new Soup.SessionAsync();
@@ -37,17 +38,22 @@ MyApplet.prototype = {
         } catch (e){ throw 'LiveScore: Adding ProxyResolverDefault failed: ' + e; }
 
         this.settings = new Settings.AppletSettings(this, metadata.uuid, instance_id);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "currency", "currency", this._update_value, null);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "ticker", "ticker", this._update_value, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "currency", "currency", this._update_settings, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "ticker", "ticker", this._update_settings, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "show-ticker-icon", "show_ticker_icon", this._update_settings, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "show-ticker-name", "show_ticker_name", this._update_settings, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "show-currency-name", "show_currency_name", this._update_settings, null);
 
+        this._update_settings();
+    },
+
+    _update_settings: function () {
         this._update_value();
+        this._loadIcon();
     },
 
     _update_value: function () {
-        let tickers = [];
-        this.ticker.split(",").forEach((t) => { tickers.push(t.trim()) });
-        this._loadTrackers(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tickers.join()}&tsyms=${this.currency}`);
-
+        this._loadTrackers(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${this.ticker}&tsyms=${this.currency}`);
         Mainloop.timeout_add(5000, Lang.bind(this, this._update_value));
     },
 
@@ -66,15 +72,10 @@ MyApplet.prototype = {
             this.set_applet_label('Error loading prices...');
             global.logError(response.Message);
         } else {
-            let message = [];
-            for (let tracker in response) {
-                const trackerInfo = response[tracker];
-                if (trackerInfo.hasOwnProperty(this.currency)) {
-                    message.push(`${tracker}:${trackerInfo[this.currency]}`);
-                }
-            }
-
-            this.set_applet_label(message.join(" | "));
+            let name = this.show_ticker_name ? `${this.ticker}:` : '';
+            let currency_name = this.show_currency_name ? ` ${this.currency}` : '';
+            let message = `${name}${response[this.ticker][this.currency]}${currency_name}`;
+            this.set_applet_label(message);
         }
     },
 
@@ -84,11 +85,53 @@ MyApplet.prototype = {
         this.httpSession.queue_message(message, function(session,message){this_._onHandleResponse(session,message)});
     },
 
+    _loadIcon: function() {
+        if (!this.show_ticker_icon) {
+            this.hide_applet_icon();
+            return;
+        }
+
+        let this_ = this;
+        let message = Soup.Message.new('GET', 'https://min-api.cryptocompare.com/data/all/coinlist');
+        this.httpSession.queue_message(message, function(session,message){this_._onHandleIconResponse(session,message)});
+    },
+
+    _onHandleIconResponse: function (session, message) {
+        if (message.status_code !== 200) {
+            this.hide_applet_icon();
+            global.logWarning('Failure to receive valid response from remote api');
+        } else {
+            this._parseIconJSON(message.response_body.data);
+        }
+    },
+
+    _parseIconJSON: function (data) {
+        const response = Json.json_parse(data, null);
+        if (response.hasOwnProperty('Response') && response['Response'] === 'Error') {
+            this.hide_applet_icon();
+            global.logError(response.Message);
+        } else {
+            if (response.hasOwnProperty('Data') && 
+                response.Data.hasOwnProperty(this.ticker) &&
+                response.Data[this.ticker].hasOwnProperty('ImageUrl')) {
+                let iconPath = response.Data[this.ticker].ImageUrl;
+                let iconFile = Gio.file_new_for_uri(`https://www.cryptocompare.com${iconPath}`);
+                let gicon = new Gio.FileIcon({ file: iconFile });
+                if (this._applet_icon_box.child) this._applet_icon_box.child.destroy();
+                this._applet_icon = new St.Icon({ gicon: gicon, icon_size: 16, icon_type: St.IconType.SYMBOLIC, reactive: true, track_hover: true, style_class: "applet-icon" });
+                this._applet_icon_box.child = this._applet_icon;
+            } else {
+                this.hide_applet_icon();
+                global.logWarning('Missing icon URL');
+            }
+        }
+    },
+
     on_applet_removed_from_panel: function() {
       this.settings.finalize();
     }
 };
 
 function main(metadata, orientation, panel_height, instance_id) {
-    return new MyApplet(metadata, orientation, instance_id);
+    return new MyApplet(metadata, orientation, panel_height, instance_id);
 }
