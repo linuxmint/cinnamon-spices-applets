ea1b2a167d5c09c012f5416d649bbd724b1678e3
Jason Hicks <jaszhix@gmail.com>
2018-06-07 20:53:07 -0500

    [WIP] IcingTaskManager@json: Fix hover pseudo class not resetting on focus events (#1856)
    * appGroup: Fix hover pseudo class not resetting on focus events
    
    * 6.3.6
    
    Closes #1855 and closes
    https://github.com/jaszhix/icingtaskmanager/issues/199
    
    * Fix hover pseudo class removal condition

--- IcingTaskManager@json/files/IcingTaskManager@json/3.8/appGroup.js
+++ IcingTaskManager@json/files/IcingTaskManager@json/3.8/appGroup.js
@@ -526,6 +526,18 @@ class AppGroup {
       return false;
     }
 
+    this.resetHoverStatus();
+
+    if (this.hadClosedPseudoClass && this.groupState.metaWindows.length === 0) {
+      this.hadClosedPseudoClass = false;
+      this.actor.add_style_pseudo_class('closed');
+    }
+
+    this._setFavoriteAttributes();
+    this.hoverMenu._onMenuLeave();
+  }
+
+  resetHoverStatus() {
     let hoverPseudoClass = getPseudoClass(this.state.settings.hoverPseudoClass);
     let focusPseudoClass = getPseudoClass(this.state.settings.focusPseudoClass);
     let activePseudoClass = getPseudoClass(this.state.settings.activePseudoClass);
@@ -539,17 +551,9 @@ class AppGroup {
     });
 
     if (!focused
-      && (hoverPseudoClass !== focusPseudoClass || hoverPseudoClass !== activePseudoClass)) {
+      || (hoverPseudoClass !== focusPseudoClass && hoverPseudoClass !== activePseudoClass)) {
       this.actor.remove_style_pseudo_class(hoverPseudoClass);
     }
-
-    if (this.hadClosedPseudoClass && this.groupState.metaWindows.length === 0) {
-      this.hadClosedPseudoClass = false;
-      this.actor.add_style_pseudo_class('closed');
-    }
-
-    this._setFavoriteAttributes();
-    this.hoverMenu._onMenuLeave();
   }
 
   setActiveStatus(windows){
@@ -597,6 +601,7 @@ class AppGroup {
     if (this.state.settings.showActive && this.groupState.metaWindows.length > 0) {
       this.actor.add_style_pseudo_class(getPseudoClass(this.state.settings.activePseudoClass));
     }
+    this.resetHoverStatus();
   }
 
   _onWindowDemandsAttention (metaWindow) {
