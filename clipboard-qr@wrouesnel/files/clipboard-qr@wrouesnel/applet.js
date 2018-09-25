baca38bba64599a5ee882bedd416eddf893b3983
mi2ek <43220550+mi2ek@users.noreply.github.com>
2018-09-26 01:45:14 +0200

    clipboard-qr@wrouesnel: Added python interpreter into clipboard-qr.py launch command to circumvent lack of execute bit on clipboar-qr.py helper program. (#1980)
    clipboard-qr@wrouesnel: Added python interpreter into clipboard-qr.py launch command to circu…

--- clipboard-qr@wrouesnel/files/clipboard-qr@wrouesnel/applet.js
+++ clipboard-qr@wrouesnel/files/clipboard-qr@wrouesnel/applet.js
@@ -95,7 +95,7 @@ MyApplet.prototype = {
             if (this._qrprocess == null) {
                 // Spawn the python helper
                 this._qrprocess = Gio.Subprocess.new(
-                    [GLib.build_filenamev([AppletDir,QRReaderHelper]), camera_path],
+                    ["/usr/bin/python", GLib.build_filenamev([AppletDir,QRReaderHelper]), camera_path],
                     Gio.SubprocessFlags.STDOUT_PIPE);
                 // Read from stdout
                 let streamOut = this._qrprocess.get_stdout_pipe();
