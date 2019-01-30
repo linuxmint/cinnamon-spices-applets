9d10e7f1527284ce9b96b997bfc56cebf79f2eeb
Attila Greguss <attila.greguss@student.anglia.ac.uk>
2019-01-30 01:33:26 +0000

    weather@mockurtl 2.1.0 Update
    * weather@mockturtl 2.0.1 Update
    
    * Fixed issue where sunset/sunrise displayed twice
    * Forecast compiling uses timezone calculation to separate forecasts to the correct days now
    * Fixed issue where days were displayed incorrectly, now are calculated displayed with timezone offset (was bad with people near the day boundary i guess)
    * using HTTPS to call OpenWeather
    
    * Adding some additional information on API and TZ handling
    
    * Added higher min refresh limit, switched from request callbacks to fully async functions for more manageable flow
    
    * * Moved Error messages to the top as consts,
    * Weather data is now stored inside the Applet object,
    * Preparing to port API related functions to a separate object,
    * Preparing to use API to fetch Timezone information on demand,
    * Fixed logging,
    * Cleanup
    
    * Added DarkSky API support
    
    * DarkSky fully implemented, long conditions are not in the correct units
    * Started ReImplementing OpenWeather
    * Greatly Improved Logging, try/catch used in correct places
    
    * DarkSky and OpenWeather Premium fully implemented
    
    * Preparing to phase out OpenWeatherMap free
    * Now Mainloop is always preserved by a try/catch now
    * Error Messages are getting phased out to the new ones.
    
    * Automatic location lookup added
    
    * Minor darksky icon and condition text fixes
    * Hungarian Translation
    * Old error messages removed
    
    * Made Main Loop Independent from Refresh
    
    Weather Updates every refreshinterval based on elapsed time since last weather update.
    
    Any action can refresh weather, it does not affect the loop, It might skip updating the weather.
    
    Good Laptops after waking up from sleep and stuff or for spotty internet connections
    
    * Changed version number

---
+++ weather@mockturtl/files/weather@mockturtl/ipApi.js
@@ -0,0 +1,90 @@
+var exports = module.exports = {}
+const Soup = imports.gi.Soup;
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+///////////                                       ////////////
+///////////                  IpApi                ////////////
+///////////                                       ////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+exports.IpApi = function(app) {
+    this.query = "https://ipapi.co/json";
+
+    this.GetLocation = async function() {
+        let json;
+        try {
+            let message = Soup.Message.new('GET', this.query);
+                app._httpSession.send_message(message);
+                json = JSON.parse(message.response_body.data);
+        }
+        catch(e) {
+            app.log.Error("IpApi service error: " + e);
+            app.showError(app.errMsg.label.generic, app.errMsg.desc.cantGetLoc);
+            return false;
+        }
+
+        if (!json) {
+            return false;
+        }
+
+        if (json.error) {
+            this.HandleErrorResponse(json);
+            return false;
+        }
+
+        return this.ParseInformation(json);
+    };
+
+    this.ParseInformation = function(json) {
+        try {
+            let loc = json.latitude + "," + json.longitude;
+            //app._location == (json.latitude + "," + json.longitude);
+            app.settings.setValue('location', loc);
+            app.weather.location.timeZone = json.timezone;
+            app.weather.location.city = json.city;
+            app.weather.location.country = json.country;
+            app.log.Print("Location obtained");
+            app.log.Debug("Location:" + json.latitude + "," + json.longitude);
+            app.log.Debug("Location setting is now: " + app._location);
+            return true;
+        }
+        catch(e) {
+            app.log.Error("IPapi parsing error: " + e);
+            app.showError(app.errMsg.label.generic, app.errMsg.desc.cantGetLoc);
+            return false;
+        }
+    };
+
+    this.HandleErrorResponse = function(json) {
+        app.log.Error("IpApi error response: " + json.reason);       
+    };
+};
+
+
+/*
+
+Half sanitized example payload, courtesy of gr3q
+{   
+    "ip": "8.8.8.8",
+    "city": "Cambridge",
+    "region": "England",
+    "region_code": "ENG",
+    "country": "GB",
+    "country_name": "United Kingdom",
+    "continent_code": "EU",
+    "in_eu": true,
+    "postal": "XX0",
+    "latitude": 52.2333,
+    "longitude": 0.15,
+    "timezone": "Europe/London",
+    "utc_offset": "+0000",
+    "country_calling_code": "+44",
+    "currency": "GBP",
+    "languages": "en-GB,cy-GB,gd",
+    "asn": "AS5089",
+    "org": "Virgin Media Limited"
+}
+
+*/
