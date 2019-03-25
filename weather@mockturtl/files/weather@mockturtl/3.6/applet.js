2fae9d14b6997d1137380d173261d83b47b7988d
Attila Greguss <attila.greguss@student.anglia.ac.uk>
2019-03-25 21:04:24 +0000

    Auto stash before merge of "backport-babel" and "origin/backport-babel"

--- weather@mockturtl/files/weather@mockturtl/3.6/applet.js
+++ weather@mockturtl/files/weather@mockturtl/3.6/applet.js
@@ -1,10 +1,60 @@
 "use strict";
 
-// Import Polyfill
+// Import Polyfills
 /*var AppletDir = imports.ui.appletManager.applets['weather@mockturtl'];
 var regenerator = AppletDir.regenerator;
 var regeneratorRuntime = regenerator.runtime;*/
-//require("../regenerator");
+
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function(valueToFind, fromIndex) {
+
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      // 1. Let O be ? ToObject(this value).
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      var n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(valueToFind, elementK) is true, return true.
+        if (sameValueZero(o[k], valueToFind)) {
+          return true;
+        }
+        // c. Increase k by 1. 
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
 
 /**
  * Copyright (c) 2014-present, Facebook, Inc.
@@ -1636,6 +1686,7 @@ MyApplet.prototype = (_MyApplet$prototype = {
                 if (this._showSunrise) {
                   sunriseText = _('Sunrise');
                   sunsetText = _('Sunset');
+
                   /*if (this.weather.location.timeZone != null) {
                     //have TZ, en-GB returns time in the correct format
                     sunrise = this.weather.sunrise.toLocaleString("en-GB", {
