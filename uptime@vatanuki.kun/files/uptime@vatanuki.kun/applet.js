25c461187f9514b5f92774b695311cafaed50c6a
Per Hultqvist <per@softteam.se>
2019-04-26 21:17:51 +0200

    uptime@vatanuki.kun : Major improvements

--- uptime@vatanuki.kun/files/uptime@vatanuki.kun/applet.js
+++ uptime@vatanuki.kun/files/uptime@vatanuki.kun/applet.js
@@ -1,3 +1,4 @@
+// Imports
 const Applet = imports.ui.applet;
 const Cinnamon = imports.gi.Cinnamon;
 const Lang = imports.lang;
@@ -6,26 +7,47 @@ const Mainloop = imports.mainloop;
 const Gettext = imports.gettext;
 const UUID = "uptime@vatanuki.kun";
 const GLib = imports.gi.GLib;
+const Settings = imports.ui.settings;
 
 Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale")
 
 function _(str) {
-  return Gettext.dgettext(UUID, str);
+	return Gettext.dgettext(UUID, str);
 }
 
-function MyApplet(orientation, panel_height, instane_id) {
-	this._init(orientation, panel_height, instane_id);
+function MyApplet(metadata, orientation, panel_height, instane_id) {
+	this._init(metadata, orientation, panel_height, instane_id);
 }
 
 MyApplet.prototype = {
-	__proto__: Applet.TextApplet.prototype,
+	__proto__: Applet.TextIconApplet.prototype,
 	_timeout: null,
 
-	_init: function(orientation, panel_height, instane_id) {
-		Applet.TextApplet.prototype._init.call(this, orientation, panel_height, instane_id);
+	_init: function (metadata, orientation, panel_height, instane_id) {
+		Applet.TextIconApplet.prototype._init.call(this, orientation, panel_height, instane_id);
 
 		try {
-			this._applet_label.set_style('text-align: left');
+			// Variables needed for the settings
+			this._display = "";
+			this._prefix = "";
+			this._pattern = "";
+			this._text_css = "";
+
+			// Setup the settings
+			this.settings = new Settings.AppletSettings(this, metadata.uuid, this.instance_id);
+			this.settings.bind("display", "_display", this.settings_changed);
+			this.settings.bind("prefix", "_prefix", this.settings_changed);
+			this.settings.bind("pattern", "_pattern", this.settings_changed);
+			this.settings.bind("text_css", "_text_css", this.settings_changed);
+
+			// Set a clock icon on the applet
+			this.set_applet_icon_symbolic_name("preferences-system-time-symbolic");
+
+			// Set tooltip of the applet
+			this.set_applet_tooltip(_("Uptime : Shows the time that have passed since last reboot. Click to update..."));
+
+			// Set label style, refresh and hookup a one minute update function
+			this._applet_label.set_style(this._text_css);
 			this._refresh();
 			this._timeout = Mainloop.timeout_add_seconds(60, Lang.bind(this, this._refresh));
 		}
@@ -34,41 +56,85 @@ MyApplet.prototype = {
 		}
 	},
 
-	_refresh: function()
-	{
-		let timestamps_s=Cinnamon.get_file_contents_utf8_sync('/proc/uptime').split(" ")[0];
-		let minutes=Math.floor((timestamps_s/60)%60);
-		let hours=Math.floor((timestamps_s/3600)%24);
-		let days=Math.floor((timestamps_s/86400)%365);
-		let years=Math.floor(timestamps_s/31536000);
-		let label_text="?";
-		if(years>0)
-			label_text=years+_("Y")+days+"D";
-		else if(days>99)
-			label_text=days+_("D");
-		else if(days>0){
-			if(hours < 10)
-				hours="0" + hours;
-			label_text=days+_("D")+hours+_("h");
-		}
-		else{
-			if(minutes < 10)
-				minutes="0" + minutes;
-			label_text=hours+":"+minutes;
-		}
-		this.set_applet_label(label_text);
+	_refresh: function () {
+		// Get uptime from /proc/uptime
+		let timestamps_s = Cinnamon.get_file_contents_utf8_sync('/proc/uptime').split(" ")[0];
+
+		// Calculate uptime years, days, hours and minutes
+		let minutes = Math.floor((timestamps_s / 60) % 60);
+		let hours = Math.floor((timestamps_s / 3600) % 24);
+		let days = Math.floor((timestamps_s / 86400) % 365);
+		let years = Math.floor(timestamps_s / 31536000);
+		let label_text = "?";
+
+		// Get the correct description based on user preferences
+		if (this._display == "classic")
+			label_text = this.GetClassicDescription(years, days, hours, minutes)
+		else if (this._display == "simple")
+			label_text = this.GetSimpleDescription(years, days, hours, minutes)
+		else if (this._display == "complex")
+			label_text = this.GetComplexDescription(years, days, hours, minutes)
+
+		// Set application label (including prefix)
+		this.set_applet_label(this._prefix + label_text);
+
 		return true;
 	},
-
-	on_applet_removed_from_panel: function(){
-		if(this._timeout){
+	// Get the classic description
+	GetClassicDescription: function (years, days, hours, minutes) {
+		if (years > 0)
+			return years + _("Y") + days + _("D");
+		else if (days > 99)
+			return days + _("D");
+		else if (days > 0) {
+			if (hours < 10)
+				hours = "0" + hours;
+			return days + _("D") + hours + _("h");
+		}
+		else {
+			if (minutes < 10)
+				minutes = "0" + minutes;
+			return hours + ":" + minutes;
+		}
+	},
+	// Get the new simple description
+	GetSimpleDescription: function (years, days, hours, minutes) {
+		if (years > 0)
+			return years + " " + _("years") + " " + _("and") + " " + days + " " + _("days");
+		else if (days > 0)
+			return days + " " + _("days") + " " + _("and") + " " + hours + " " + _("hours");
+		else if (hours > 0)
+			return hours + " " + _("hours") + " " + _("and") + " " + minutes + " " + _("minutes");
+		else
+			return minutes + " " + _("minutes");
+	},
+	// Get the complex description
+	GetComplexDescription: function (years, days, hours, minutes) {
+		let text = this._pattern;
+		text = text.replace('%y', years).replace('%d', days).replace('%h', hours).replace('%m', minutes);
+		text = text.replace('%H', hours.toString().padStart(2, '0')).replace('%M', minutes.toString().padStart(2, '0'));
+		return text;
+	},
+	// Called when the settings have changed
+	settings_changed: function () {
+		this._applet_label.set_style(this._text_css);
+		this._refresh();
+	},
+	// Refresh the uptime when the application is clicked
+	on_applet_clicked: function (event) {
+		this._refresh();
+	},
+	// Clean up when the application is removed from the panel
+	on_applet_removed_from_panel: function () {
+		if (this._timeout) {
 			Mainloop.source_remove(this._timeout);
-			this._timeout=null;
+			this._timeout = null;
 		}
 	},
 };
 
-function main(metadata, orientation, panel_height, instance_id){
-	let myApplet = new MyApplet(orientation, panel_height, instance_id);
+// Main function
+function main(metadata, orientation, panel_height, instance_id) {
+	let myApplet = new MyApplet(metadata, orientation, panel_height, instance_id);
 	return myApplet;
 }
