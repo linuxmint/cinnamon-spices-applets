c8555e81f8196de4cc2ba4c654d702f0ce00cf14
Jakub Stonavsky <jakub@foobar.beer>
2019-07-28 17:51:37 +0200

    SSH Connect Applet update - Cinnamon Settings Configuration
    - Moving configuration to Cinnamon Applet settings
    - SSH connections list is now configuration table (grouped by Group
    Name)
    - Terminal application is now configurable via the Applet Settings
    interface.

--- sshconnect@foobar-beer/README.md
+++ sshconnect@foobar-beer/README.md
@@ -4,7 +4,8 @@ Applet for providing configurable UI for storing and opening SSH connections.
 
 - Install
 - Add to panel
-- Click `Edit Config JSON`
+- Edit Applet configuration 
+- Add your own connections
 - ??? 
 - Profit!
 
--- sshconnect@foobar-beer/files/sshconnect@foobar-beer/applet.js
+++ sshconnect@foobar-beer/files/sshconnect@foobar-beer/applet.js
@@ -12,8 +12,8 @@ const Lang = imports.lang;
 const PopupMenu = imports.ui.popupMenu;
 const Main = imports.ui.main;
 const MessageTray = imports.ui.messageTray;
-const Gio = imports.gi.Gio;
 const Gettext = imports.gettext;
+const Settings = imports.ui.settings;
 const UUID = 'sshconnect@foobar-beer';
 const AppletDir = imports.ui.appletManager.appletMeta[UUID].path;
 
@@ -35,12 +35,13 @@ MyApplet.prototype = {
 
     this.set_applet_tooltip('SSH Connect');
 
+    this.settings = new Settings.AppletSettings(this, UUID, instance_id);
+
     try {
       this.set_applet_icon_path(AppletDir + '/icon.png');
       this.menuManager = new PopupMenu.PopupMenuManager(this);
       this.menu = new Applet.AppletPopupMenu(this, orientation);
       this.menuManager.addMenu(this.menu);
-      this.gsettings = Gio.Settings.new('org.gnome.desktop.default-applications.terminal');
       this.msgSource = new MessageTray.SystemNotificationSource('SSH Connect');
 
       Main.messageTray.add(this.msgSource);
@@ -54,53 +55,47 @@ MyApplet.prototype = {
   updateMenu: function() {
     this.menu.removeAll();
 
-    let menuitemEdit = new PopupMenu.PopupMenuItem(_('Edit Config JSON'));
-    menuitemEdit.connect('activate', Lang.bind(this, this.editConfig));
-    this.menu.addMenuItem(menuitemEdit);
-
     let menuitemReload = new PopupMenu.PopupMenuItem(_('Reload Config'));
     menuitemReload.connect('activate', Lang.bind(this, this.updateMenu));
     this.menu.addMenuItem(menuitemReload);
 
     this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-    let configFile = Gio.file_new_for_path(AppletDir + '/config.json');
-    configFile.load_contents_async(null, function(file, res) {
-      try {
-        let content = configFile.load_contents_finish(res)[1];
-        let json = JSON.parse(content);
+    let connections = this.settings.getValue('connections');
+    let groups = connections.reduce(function(r, c) {
+      r[c.group] = (r[c.group] || []).concat(c);
+      return r;
+    }, Object.create(null));
 
-        json.forEach(function(group) {
-          let subMenu = new PopupMenu.PopupSubMenuMenuItem(group.groupName);
-          group.group.forEach(function(entry) {
-            let label = entry.name;
-            let item = new PopupMenu.PopupMenuItem(label);
-            item.connect('activate', function() { this.connectTo(label, entry.host, entry.flags, entry.profile); }.bind(this));
-            subMenu.menu.addMenuItem(item);
-          }.bind(this));
-          this.menu.addMenuItem(subMenu);
-        }.bind(this));
-      } catch (error) {
-        global.logError(error);
-      }
-    }.bind(this));
+    for (let group in groups) {
+      let subMenu = new PopupMenu.PopupSubMenuMenuItem(group);
+      groups[group].forEach(function(entry) {
+        let label = entry.name;
+        let item = new PopupMenu.PopupMenuItem(label);
+        item.connect('activate', function() { this.connectTo(label, entry.host, entry.flags, entry.profile); }.bind(this));
+        subMenu.menu.addMenuItem(item);
+      }.bind(this));
+      this.menu.addMenuItem(subMenu);
+    }
   },
 
   connectTo: function(name, host, flags, profile) {
-    let terminal = this.gsettings.get_string('exec');
+    let terminal = this.settings.getValue('terminal-exec');
 
-    let addProfile = (profile !== undefined && profile !== '' ? ' --profile="' + profile + '"' : '');
+    let setTitle = this.settings.getValue('customize-title');
+    let addTitle = setTitle ? ' ' + this.settings.getValue('title-flag') + '"' + name + '"' : '';
+
+    let setProfile = this.settings.getValue('customize-profile');
+    let addProfile = (setProfile && profile !== undefined && profile !== '' ? ' ' + this.settings.getValue('profile-flag') + '"' + profile + '"' : '');
+
     let addFlag = (flags !== undefined && flags !== '' ? flags : '');
+    let addExecStr = ' ' + this.settings.getValue('exec-flag') + '"ssh ' + addFlag + ' ' + host + '"';
     
-    Main.Util.spawnCommandLine(terminal + ' -t "' + name + '"' + addProfile + ' -e "ssh ' + addFlag + ' ' + host + '"');
+    Main.Util.spawnCommandLine(terminal + addTitle + addProfile + addExecStr);
 
     let notification = new MessageTray.Notification(this.msgSource, 'SSH Connect', _('Connection opened for ') + name);
     notification.setTransient(true);
     this.msgSource.notify(notification);
-  },
-
-  editConfig: function() {
-    Main.Util.spawn_async(["xdg-open", AppletDir + '/config.json'], null);
   },
 
   on_applet_clicked: function(event) {
