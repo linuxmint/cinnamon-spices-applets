83b8ba6b28fb67ed5e6dfa509e9fc4b90005c9a1
claudiux <claude.clerc@gmail.com>
2019-08-08 20:36:58 +0200

    generate_mo.sh - appeases Codacy

--- SpicesUpdate@claudiux/files/SpicesUpdate@claudiux/scripts/generate_mo.sh
+++ SpicesUpdate@claudiux/files/SpicesUpdate@claudiux/scripts/generate_mo.sh
@@ -4,8 +4,8 @@
 UUID="SpicesUpdate@claudiux"
 cd $HOME/.local/share/cinnamon/applets/$UUID/po/
 
-for l in `ls -1A *.po | sed s/"\.po$"/" "/g | tr "\n" " "`
+for l in $(ls -1A *.po | sed s/"\.po$"/" "/g | tr "\n" " ")
 do
   mkdir -p $HOME/.local/share/locale/$l/LC_MESSAGES
-  msgfmt -cv -o $HOME/.local/share/locale/$l/LC_MESSAGES/${UUID}.mo $l.po 
+  msgfmt -cv -o $HOME/.local/share/locale/$l/LC_MESSAGES/${UUID}.mo $l.po
 done
