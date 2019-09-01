7dd82101734c1dbade99ca6424cd91b165de3416
claudiux <33965039+claudiux@users.noreply.github.com>
2019-09-01 12:09:32 +0200

    Fixes #2371

--- hwmonitor@sylfurd/files/hwmonitor@sylfurd/applet.js
+++ hwmonitor@sylfurd/files/hwmonitor@sylfurd/applet.js
@@ -518,6 +518,7 @@ CpuDataProvider.prototype = {
         this.last = 0;
         this.usage = 0;
         this.last_total = 0;
+        this.last_delta = 0;
         this.name = _("CPU");
         this.type = "CPU";
     },
@@ -528,10 +529,15 @@ CpuDataProvider.prototype = {
         this.current = this.gtop.idle;
 
         let delta = this.gtop.total - this.last_total;
+        //global.log("Moniteur: delta " + delta );
+        if (delta < -50000) {
+            delta = this.last_delta;
+        }
         if (delta > 0) {
             this.usage = (this.current - this.last) / delta;
             this.last = this.current;
             this.last_total = this.gtop.total;
+            this.last_delta = delta;
         }
 
         return 1 - this.usage;
