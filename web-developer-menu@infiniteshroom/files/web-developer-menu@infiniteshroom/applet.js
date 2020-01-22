474dcd0d05f4587562d0c8f4e90c9650d4270b99
claudiux <33965039+claudiux@users.noreply.github.com>
2020-01-22 21:21:59 +0100

    [web-developer-menu@infiniteshroom] Fix issue #2760 (#2820)
    Bug fix. This PR fixes #2760 (undeclared variables).

--- web-developer-menu@infiniteshroom/files/web-developer-menu@infiniteshroom/applet.js
+++ web-developer-menu@infiniteshroom/files/web-developer-menu@infiniteshroom/applet.js
@@ -3,21 +3,21 @@ const PopupMenu = imports.ui.popupMenu;
 const Gettext = imports.gettext;
 const UUID = "web-developer-menu@infiniteshroom";
 const Util = imports.misc.util;
-const Lang = imports.lang; 
+const Lang = imports.lang;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 
 //applet command constants
 var CommandConstants = new function() {
-	this.COMMAND_START_APACHE = "gksu service apache2 restart";
-	this.COMMAND_STOP_APACHE = "gksu service apache2 stop";
-	this.COMMAND_START_MYSQL = "gksu service mysql restart";
-	this.COMMAND_STOP_MYSQL = "gksu service mysql stop";
-	this.COMMAND_APACHE_CONFIG_EDIT = "gksu gedit /etc/apache2/sites-enabled/000-default";
-	this.COMMAND_PHP_CONFIG_EDIT = "gksu gedit /etc/php5/apache2/php.ini";
-	this.COMMAND_LAUNCH_PHPMYADMIN = "xdg-open http://localhost/phpmyadmin/";
-	this.COMMAND_LAUNCH_WEBDIR = "xdg-open http://localhost/";
-	this.COMMAND_OPEN_WEBDIR = "nemo /var/www/";
+  this.COMMAND_START_APACHE = "gksu service apache2 restart";
+  this.COMMAND_STOP_APACHE = "gksu service apache2 stop";
+  this.COMMAND_START_MYSQL = "gksu service mysql restart";
+  this.COMMAND_STOP_MYSQL = "gksu service mysql stop";
+  this.COMMAND_APACHE_CONFIG_EDIT = "gksu gedit /etc/apache2/sites-enabled/000-default";
+  this.COMMAND_PHP_CONFIG_EDIT = "gksu gedit /etc/php5/apache2/php.ini";
+  this.COMMAND_LAUNCH_PHPMYADMIN = "xdg-open http://localhost/phpmyadmin/";
+  this.COMMAND_LAUNCH_WEBDIR = "xdg-open http://localhost/";
+  this.COMMAND_OPEN_WEBDIR = "nemo /var/www/";
 }
 
 
@@ -27,116 +27,111 @@ function _(str) {
   return Gettext.dgettext(UUID, str);
 }
 
-function MyApplet(orientation){
-    this._init(orientation);
+function checkService(service) {
+  let s=GLib.spawn_async_with_pipes(null, ["pgrep",service], null, GLib.SpawnFlags.SEARCH_PATH,null)
+  let c=GLib.IOChannel.unix_new(s[3])
+
+  let [res, pid, in_fd, out_fd, err_fd] =
+    GLib.spawn_async_with_pipes(null, ["pgrep",service], null, GLib.SpawnFlags.SEARCH_PATH, null);
+
+  let out_reader = new Gio.DataInputStream({ base_stream: new Gio.UnixInputStream({fd: out_fd}) });
+
+  let [out, size] = out_reader.read_line(null);
+
+  var result = false;
+  if(out != null) {
+    result = true;
+  }
+
+  return result;
 }
 
 
+function MyApplet(orientation){
+  this._init(orientation);
+}
 
 MyApplet.prototype = {
-    __proto__: Applet.IconApplet.prototype,
+  __proto__: Applet.IconApplet.prototype,
 
-    _init: function(orientation){
-        Applet.IconApplet.prototype._init.call(this, orientation);
-        this.set_applet_icon_symbolic_name("network-server-symbolic");
-        this.set_applet_tooltip("WebMenu");
-        
-        //setup a new menuManager and add the main context main to the manager
+  _init: function(orientation){
+    Applet.IconApplet.prototype._init.call(this, orientation);
+    this.set_applet_icon_symbolic_name("network-server-symbolic");
+    this.set_applet_tooltip("WebMenu");
 
-		this.menuManager = new PopupMenu.PopupMenuManager(this);
-		this.menu = new Applet.AppletPopupMenu(this, orientation);
-		this.menuManager.addMenu(this.menu);
+    //setup a new menuManager and add the main context main to the manager
 
-		
-		//add two Toggle buttons one for Apache and one for mysql
-		this.apacheEnabledSwitch = new PopupMenu.PopupSwitchMenuItem(_("Apache Web Server"), checkService("apache"));
-		this.mysqlEnabledSwitch = new PopupMenu.PopupSwitchMenuItem(_("MySQL Server"), checkService("mysql"))
-		this.menu.addMenuItem(this.apacheEnabledSwitch);
-		this.menu.addMenuItem(this.mysqlEnabledSwitch);
-		
-		//add a separator to separate the toggle buttons and actions
-		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+    this.menuManager = new PopupMenu.PopupMenuManager(this);
+    this.menu = new Applet.AppletPopupMenu(this, orientation);
+    this.menuManager.addMenu(this.menu);
 
 
-		this.menu.addAction(_("Open Web Dir"), function(event) {
-						Util.spawnCommandLine(CommandConstants.COMMAND_OPEN_WEBDIR);
+    //add two Toggle buttons one for Apache and one for mysql
+    this.apacheEnabledSwitch = new PopupMenu.PopupSwitchMenuItem(_("Apache Web Server"), checkService("apache"));
+    this.mysqlEnabledSwitch = new PopupMenu.PopupSwitchMenuItem(_("MySQL Server"), checkService("mysql"))
+    this.menu.addMenuItem(this.apacheEnabledSwitch);
+    this.menu.addMenuItem(this.mysqlEnabledSwitch);
 
-		});
-
-		this.menu.addAction(_("Launch Web Dir"), function(event) {
-						Util.spawnCommandLine(CommandConstants.COMMAND_LAUNCH_WEBDIR);
-		});
-
-		this.menu.addAction(_("Launch phpMyAdmin"), function(event) {
-						Util.spawnCommandLine(CommandConstants.COMMAND_LAUNCH_PHPMYADMIN);
-
-		});
-
-		this.menu.addAction(_("Edit default php.ini"), function(event) {
-						Util.spawnCommandLine(CommandConstants.COMMAND_PHP_CONFIG_EDIT);
-		});
-
-		this.menu.addAction(_("Edit Apache Conf"), function(event) {
-						Util.spawnCommandLine(CommandConstants.COMMAND_APACHE_CONFIG_EDIT);
-		});
+    //add a separator to separate the toggle buttons and actions
+    this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
 
-		this.apacheEnabledSwitch.connect('toggled', Lang.bind(this, this.onapacheSwitchPressed));
-		this.mysqlEnabledSwitch.connect('toggled', Lang.bind(this, this.onmysqlSwitchPressed));
-    },
+    this.menu.addAction(_("Open Web Dir"), function(event) {
+      Util.spawnCommandLine(CommandConstants.COMMAND_OPEN_WEBDIR);
+    });
 
-	on_applet_clicked: function(){
-		this.menu.toggle();
-		this.apacheEnabledSwitch.setToggleState(checkService("apache")); //reload state
-		this.mysqlEnabledSwitch.setToggleState(checkService("mysql"));   //reload state
-    },
+    this.menu.addAction(_("Launch Web Dir"), function(event) {
+      Util.spawnCommandLine(CommandConstants.COMMAND_LAUNCH_WEBDIR);
+    });
 
-    onapacheSwitchPressed: function(item){
-	    	this.menu.toggle();	//Close before calling gksu	
-		if(item.state){
-			Util.spawnCommandLine(CommandConstants.COMMAND_START_APACHE);
-		}
-		
-		else {
-			Util.spawnCommandLine(CommandConstants.COMMAND_STOP_APACHE);
-		}
-    },
-    
-     onmysqlSwitchPressed: function(item){
-	     	this.menu.toggle();	//Close before calling gksu	
-		if(item.state){
-			
-			Util.spawnCommandLine(CommandConstants.COMMAND_START_MYSQL);
-		}
-		
-		else {
-			Util.spawnCommandLine(CommandConstants.COMMAND_STOP_MYSQL);
-		}
-    },
+    this.menu.addAction(_("Launch phpMyAdmin"), function(event) {
+      Util.spawnCommandLine(CommandConstants.COMMAND_LAUNCH_PHPMYADMIN);
+    });
+
+    this.menu.addAction(_("Edit default php.ini"), function(event) {
+      Util.spawnCommandLine(CommandConstants.COMMAND_PHP_CONFIG_EDIT);
+    });
+
+    this.menu.addAction(_("Edit Apache Conf"), function(event) {
+      Util.spawnCommandLine(CommandConstants.COMMAND_APACHE_CONFIG_EDIT);
+    });
+
+    this.apacheEnabledSwitch.connect('toggled', Lang.bind(this, this.onapacheSwitchPressed));
+    this.mysqlEnabledSwitch.connect('toggled', Lang.bind(this, this.onmysqlSwitchPressed));
+  },
+
+  on_applet_clicked: function(){
+    this.menu.toggle();
+    this.apacheEnabledSwitch.setToggleState(checkService("apache")); //reload state
+    this.mysqlEnabledSwitch.setToggleState(checkService("mysql"));   //reload state
+  },
+
+  onapacheSwitchPressed: function(item){
+    this.menu.toggle(); //Close before calling gksu
+    if(item.state){
+      Util.spawnCommandLine(CommandConstants.COMMAND_START_APACHE);
+    }
+
+    else {
+      Util.spawnCommandLine(CommandConstants.COMMAND_STOP_APACHE);
+    }
+  },
+
+  onmysqlSwitchPressed: function(item){
+    this.menu.toggle(); //Close before calling gksu
+    if(item.state){
+
+      Util.spawnCommandLine(CommandConstants.COMMAND_START_MYSQL);
+    }
+
+    else {
+      Util.spawnCommandLine(CommandConstants.COMMAND_STOP_MYSQL);
+    }
+  },
 }
 
 
 function main(metadata, orientation){
-    let myApplet = new MyApplet(orientation);
-    return myApplet;
-}
-
-function checkService(service) {
-
-	s=GLib.spawn_async_with_pipes(null, ["pgrep",service], null, GLib.SpawnFlags.SEARCH_PATH,null)
-	c=GLib.IOChannel.unix_new(s[3])
-	       
-	let [res, pid, in_fd, out_fd, err_fd] =
-	  GLib.spawn_async_with_pipes(
-	    null, ["pgrep",service], null, GLib.SpawnFlags.SEARCH_PATH, null);
-	out_reader = new Gio.DataInputStream({ base_stream: new Gio.UnixInputStream({fd: out_fd}) });
-	       
-	let [out, size] = out_reader.read_line(null);
-
-	var result = false;
-	if(out != null) {
-		result = true;
-	}
-
-	return result;
+  let myApplet = new MyApplet(orientation);
+  return myApplet;
 }
