6cdc9b20615168acca90de048f342e4468faa143
Alex <aluton@gmail.com>
2020-04-26 20:15:04 +0100

    bing-wallpaper@starcross.dev: Fix image refresh across different timezones/markets (#2975)
    * bing-wallpaper@starcross.dev: Add new Bing Desktop Wallpaper spice
    
    * bing-wallpaper@starcross.dev: Refresh image based on startdate in metadata
    Cache metadata locally
    Move data file location to .config folder

--- bing-wallpaper@starcross.dev/files/bing-wallpaper@starcross.dev/applet.js
+++ bing-wallpaper@starcross.dev/files/bing-wallpaper@starcross.dev/applet.js
@@ -11,9 +11,7 @@ const _httpSession = new Soup.SessionAsync();
 Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
 
 const bingHost = 'https://www.bing.com';
-const bingRequestPath = '/HPImageArchive.aspx?format=js&idx=0&n=1&mbl=1&mkt=';
-const default_file_name = 'Bing Desktop Wallpaper';
-var file_name;
+const bingRequestPath = '/HPImageArchive.aspx?format=js&idx=0&n=1&mbl=1';
 
 function log(message) {
     if (logging) global.log(`[bing-wallpaper@starcross.dev]: ${message}`);
@@ -26,57 +24,28 @@ function BingWallpaperApplet(orientation, panel_height, instance_id) {
 BingWallpaperApplet.prototype = {
     __proto__: Applet.IconApplet.prototype,
 
-    _init: function(orientation, panel_height, instance_id) {
+    _init: function (orientation, panel_height, instance_id) {
 
         // Generic Setup
         Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
         this.set_applet_icon_name("bing-wallpaper-symbolic");
-        file_name = default_file_name;
-        this.set_applet_tooltip(_(`${file_name}`));
+        this.set_applet_tooltip('Bing Desktop Wallpaper');
 
-        // Find file path to write
-        const userPicturesDir = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PICTURES);
-        this.wallpaperPath = `${userPicturesDir}/BingWallpaper.jpg`;
+        // Path to store data in
+        this.wallpaperDir = `${GLib.get_user_config_dir()}/bingwallpaper`;
+        let dir = Gio.file_new_for_path(this.wallpaperDir);
+        if (!dir.query_exists(null))
+            dir.make_directory(null);
+        this.wallpaperPath = `${this.wallpaperDir}/BingWallpaper.jpg`;
+        this.metaDataPath = `${this.wallpaperDir}/meta.json`;
 
         // Begin refresh loop
         this._refresh();
     },
 
-    _refresh: function() {
+    _refresh: function () {
         log(`Beginning refresh`);
-        const now = GLib.DateTime.new_now_local();
-        const today = GLib.DateTime.new_local(now.get_year(), now.get_month(), now.get_day_of_month(), 0, 0, 0);
-
-        let file = Gio.file_new_for_path(this.wallpaperPath);
-        let file_exists = file.query_exists(null);
-
-        if (file_exists) {
-            let file_info = file.query_info('*', Gio.FileQueryInfoFlags.NONE, null);
-            let file_size = file_info.get_size();
-            let file_mod_secs = file_info.get_modification_time().tv_sec;
-
-            if ((file_mod_secs < today.to_unix()) || !file_size) { // Is the image old, or empty?
-                log("Calling loadImageData");
-                this._loadImageData();
-            } else {
-                log("Skipping, image appears up to date");
-                // get only the image name again (and only when it was lost)
-                if (`${file_name}` == default_file_name) {
-                    log("image name is lost, get it again");
-                    let request = Soup.Message.new('GET', `${bingHost}${bingRequestPath}`);
-                    _httpSession.queue_message(request, (_httpSession, message) => {
-                        if (message.status_code === 200) {
-                            const json = JSON.parse(message.response_body.data);
-                            this.imageData = json.images[0];
-                            file_name = this.imageData.copyright;
-                            this.set_applet_tooltip(`${file_name}`);
-                        }
-                    })
-                }
-            }
-        } else { // Image does not exist yet
-            this._loadImageData()
-        }
+        this._getMetaData();
         this._setTimeout(300);
     },
 
@@ -87,7 +56,7 @@ BingWallpaperApplet.prototype = {
         }
     },
 
-    _setTimeout: function(seconds) {
+    _setTimeout: function (seconds) {
         /** Cancel current timeout in event of an error and try again shortly */
         this._removeTimeout();
         log(`Setting timeout (${seconds}s)`);
@@ -101,34 +70,99 @@ BingWallpaperApplet.prototype = {
         this._removeTimeout();
     },
 
-    _loadImageData: function() {
-        // Get json data
+    _getMetaData: function () {
+
+        /** Check for local metadata  */
+        try {
+            const jsonString = GLib.file_get_contents(this.metaDataPath)[1];
+            const json = JSON.parse(jsonString);
+
+            this.imageData = json.images[0];
+            this.set_applet_tooltip(this.imageData.copyright);
+            log(`Got image url from local file : ${this.imageData.url}`);
+
+            /** See if this data is current */
+            const start_date = GLib.DateTime.new(
+              GLib.TimeZone.new_utc(),
+              this.imageData.fullstartdate.substring(0,4),
+              this.imageData.fullstartdate.substring(4,6),
+              this.imageData.fullstartdate.substring(6,8),
+              this.imageData.fullstartdate.substring(8,10),
+              this.imageData.fullstartdate.substring(10,12),
+              0
+            );
+            const end_date = start_date.add_days(1);
+            const now = GLib.DateTime.new_now_utc();
+
+            if (now.to_unix() < end_date.to_unix()) {
+                log('metadata up to date');
+
+                // Look for image file, check this is up to date
+                let image_file = Gio.file_new_for_path(this.wallpaperPath);
+
+                if (image_file.query_exists(null)) {
+
+                    let image_file_info = image_file.query_info('*', Gio.FileQueryInfoFlags.NONE, null);
+                    let image_file_size = image_file_info.get_size();
+                    let image_file_mod_secs = image_file_info.get_modification_time().tv_sec;
+
+                    if ((image_file_mod_secs > end_date.to_unix()) || !image_file_size) { // Is the image old, or empty?
+                        this._downloadImage();
+
+                    } else {
+                        log("image appears up to date");
+                    }
+
+                } else {
+                    log("No image file found");
+                    this._downloadImage();
+                }
+
+            }
+            else {
+                log('metadata is old, requesting new...');
+                this._downloadMetaData();
+            }
+
+
+        } catch (err) {
+            log(`Unable to get local metadata ${err}`);
+            /** File does not exist or there was an error processing it */
+            this._downloadMetaData();
+        }
+    },
+
+    _downloadMetaData: function () {
+        // Retrieve json metadata, either from local file or remote
         let request = Soup.Message.new('GET', `${bingHost}${bingRequestPath}`);
         _httpSession.queue_message(request, (_httpSession, message) => {
             if (message.status_code === 200) {
+
+                // Write to meta data file
+                let gFile = Gio.file_new_for_path(this.metaDataPath);
+                let fStream = gFile.replace(null, false, Gio.FileCreateFlags.NONE, null);
+                let toWrite  = message.response_body.data.length;
+                while (toWrite > 0)
+                    toWrite -= fStream.write(message.response_body.data, null);
+                fStream.close(null);
+
                 const json = JSON.parse(message.response_body.data);
                 this.imageData = json.images[0];
-                file_name = this.imageData.copyright;
-                this.set_applet_tooltip(`${file_name}`);
-                log(`Got image url: ${this.imageData.url}`);
+                this.set_applet_tooltip(this.imageData.copyright);
+                log(`Got image url from download: ${this.imageData.url}`);
 
-                /**const end_date = GLib.DateTime.new_local(
-                  this.imageData.enddate.substring(0,4),
-                  this.imageData.enddate.substring(4,6),
-                  this.imageData.enddate.substring(6,8),
-                  0, 0, 0
-                );*/
                 this._downloadImage();
 
             } else {
-                log(`Failed to acquire image url (${message.status_code})`);
+                log(`Failed to acquire image metadata (${message.status_code})`);
                 this._setTimeout(60)  // Try again
             }
         });
     },
 
-    _downloadImage: function() {
+    _downloadImage: function () {
 
+        log('downloading new image');
         const url = `${bingHost}${this.imageData.url}`;
         let gFile = Gio.file_new_for_path(this.wallpaperPath);
 
@@ -139,7 +173,7 @@ BingWallpaperApplet.prototype = {
         let request = Soup.Message.new('GET', url);
 
         // got_chunk event
-        request.connect('got_chunk', function(message, chunk) {
+        request.connect('got_chunk', function (message, chunk) {
             if (message.status_code === 200) { // only save the data we want, not content of 301 redirect page
                 fStream.write(chunk.get_data(), null);
             }
@@ -159,7 +193,7 @@ BingWallpaperApplet.prototype = {
         });
     },
 
-    _setBackground: function() {
+    _setBackground: function () {
         let gSetting = new Gio.Settings({schema: 'org.cinnamon.desktop.background'});
         const uri = 'file://' + this.wallpaperPath;
         gSetting.set_string('picture-uri', uri);
