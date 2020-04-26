ea15b4b1ef9907b8efb811968646ea0d8d7bb635
Simonmicro <simon@simonmicro.de>
2020-04-26 21:20:26 +0200

    Added Better Backgrounds Applet (#2981)

---
+++ better-backgrounds@simonmicro/files/better-backgrounds@simonmicro/README.md
@@ -0,0 +1,9 @@
+# Better Backgrounds for Cinnamon #
+
+A Cinnamon desktop applet to change your wallpaper to Unsplash random images.
+
+The icon has been merged from the following Pixabay images:
+- https://pixabay.com/vectors/recycle-reload-icon-arrown-circle-159650/
+- https://pixabay.com/vectors/background-mesh-triangle-polygon-1430103/
+
+_Official GitHub repo is available [here](https://github.com/Simonmicro/cinnamon-spices-applets)_
---
+++ better-backgrounds@simonmicro/files/better-backgrounds@simonmicro/applet.js
@@ -0,0 +1,126 @@
+const Applet = imports.ui.applet;
+const AppletManager = imports.ui.appletManager;
+const Soup = imports.gi.Soup;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Tweener = imports.ui.tweener;
+
+const uuid = "better-backgrounds@simonmicro";
+const appletPath = AppletManager.appletMeta[uuid].path;
+const imagePath = appletPath + '/background';
+
+function log(msg) {
+    global.log('[' + uuid + '] ' + msg);
+};
+
+class UnsplashBackgroundApplet extends Applet.IconApplet {
+    constructor(orientation, panel_height, instance_id) {
+        super(orientation, panel_height, instance_id);
+
+        this.settings = new imports.ui.settings.AppletSettings(this, uuid, instance_id);
+
+        this.settings.bind("change-onstart", "change_onstart", this.on_settings_changed);
+        this.settings.bind("change-onclick", "change_onclick", this.on_settings_changed);
+        this.settings.bind("change-ontime", "change_ontime", this.on_settings_changed);
+        this.settings.bind("change-time", "change_time", this.on_settings_changed);
+        this.settings.bind("image-res-manual", "image_res_manual", this.on_settings_changed);
+        this.settings.bind("image-res-width", "image_res_width", this.on_settings_changed);
+        this.settings.bind("image-res-height", "image_res_height", this.on_settings_changed);
+        this.settings.bind("image-tag", "image_tag", this.on_settings_changed);
+        this.settings.bind("image-tag-data", "image_tag_data", this.on_settings_changed);
+
+        this.set_applet_icon_name("applet");
+        this.httpSession = new Soup.SessionAsync();
+        Soup.Session.prototype.add_feature.call(this.httpSession, new Soup.ProxyResolverDefault());
+
+        this.on_settings_changed();
+
+        if(this.change_onstart)
+            this._change_background();
+    }
+
+    _timeout_update() {
+        if(this.change_ontime)
+            this._timeout_enable();
+        else
+            this._timeout_disable();
+    }
+
+    _timeout_disable() {
+        if (this._timeout)
+            imports.mainloop.source_remove(this._timeout);
+        this._timeout = null;
+    }
+
+    _timeout_enable() {
+        this._timeout_disable();
+        this._timeout = imports.mainloop.timeout_add_seconds(this.change_time * 60, imports.lang.bind(this, this._change_background));
+    }
+
+    _change_background() {
+        let gFile = Gio.file_new_for_path(imagePath);
+        let fStream = gFile.replace(null, false, Gio.FileCreateFlags.NONE, null);
+        let resStr = 'featured';
+        if(this.image_res_manual)
+            resStr = this.image_res_width + 'x' + this.image_res_height;
+        let tagStr = '';
+        if(this.image_tag)
+            tagStr = '?' + this.image_tag_data;
+        let imageUri = 'https://source.unsplash.com/' + resStr + '/' + tagStr;
+        let request = Soup.Message.new('GET', imageUri);
+
+        request.connect('got_chunk', function(message, chunk) {
+            if (message.status_code === 200)
+                fStream.write(chunk.get_data(), null);
+        });
+
+        this.httpSession.queue_message(request, function(http, message) {
+            fStream.close(null);
+
+            if (message.status_code === 200) {
+                let gSetting = new Gio.Settings({schema: 'org.cinnamon.desktop.background'});
+                gSetting.set_string('picture-uri', 'file://' + imagePath);
+                Gio.Settings.sync();
+                gSetting.apply();
+            } else
+                log('Could not download image!');
+            this._timeout_update();
+        });
+        log('Downloading ' + imageUri);
+    }
+
+    _update_tooltip() {
+        let tooltipStr = '';
+        if(this.change_onstart)
+            tooltipStr += 'Background changed on last startup';
+        if(this.change_onstart && (this.change_onclick || this.change_ontime))
+            tooltipStr += "\n";
+        if(this.change_onclick)
+            tooltipStr += 'Click here to change background';
+        if(this.change_ontime && this.change_onclick)
+            tooltipStr += "\n";
+        if(this.change_ontime)
+            tooltipStr += 'Every ' + this.change_time + ' minutes the background changes itself';
+        if(!this.change_ontime && !this.change_onclick)
+            tooltipStr += 'No action defined! Please enable at least on in the configuration!';
+        this.set_applet_tooltip(_(tooltipStr));
+    }
+
+    on_settings_changed() {
+        this._update_tooltip();
+        this._timeout_update();
+    }
+
+    on_applet_clicked() {
+        if(this.change_onclick)
+            this._change_background();
+    }
+
+    on_applet_removed_from_panel() {
+        this.settings.finalize();
+    }
+};
+
+function main(metadata, orientation, panel_height, instance_id) {
+    return new UnsplashBackgroundApplet(orientation, panel_height, instance_id);
+}
