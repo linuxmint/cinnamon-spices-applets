b52871a947f994f2253d5ee9ac91f9476cd7fbd8
NikoKrause <nikokrause@gmx.de>
2020-05-23 16:08:01 +0200

    force-quit@cinnamon.org: Fix icon size memory
    If you use a custom height for your panel the icon scales properly, but as soon as you restart Cinnamon, the icon gets back it's default size. This will fix this issue.
    
    Fixes https://github.com/linuxmint/cinnamon-spices-applets/issues/2879

--- force-quit@cinnamon.org/files/force-quit@cinnamon.org/applet.js
+++ force-quit@cinnamon.org/files/force-quit@cinnamon.org/applet.js
@@ -10,15 +10,15 @@ function _(str) {
     return Gettext.dgettext(UUID, str);
 }
 
-function MyApplet(orientation) {
-    this._init(orientation);
+function MyApplet(metadata, orientation, panelHeight, instanceId) {
+    this._init(metadata, orientation, panelHeight, instanceId);
 }
 
 MyApplet.prototype = {
     __proto__: Applet.IconApplet.prototype,
 
-    _init: function(orientation) {
-        Applet.IconApplet.prototype._init.call(this, orientation);
+    _init: function(metadata, orientation, panelHeight, instanceId) {
+        Applet.IconApplet.prototype._init.call(this, orientation, panelHeight, instanceId);
 
         try {
             this.set_applet_icon_symbolic_name("window-close");
@@ -45,7 +45,7 @@ MyApplet.prototype = {
 
 };
 
-function main(metadata, orientation) {
-    let myApplet = new MyApplet(orientation);
+function main(metadata, orientation, panelHeight, instanceId) {
+    let myApplet = new MyApplet(metadata, orientation, panelHeight, instanceId);
     return myApplet;
 }
