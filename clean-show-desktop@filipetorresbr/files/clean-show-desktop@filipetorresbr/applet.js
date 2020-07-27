7ce8422d8a745b25f6f5ba7b5470a70f28724418
FilipeTorresBR <51489686+FilipeTorresBR@users.noreply.github.com>
2020-07-27 05:06:28 -0300

    [clean-show-desktop@filipetorresbr] Now is possible change border and fill color (#3145)
    * New icon
    
    * Settings schema file
    
    * metadata file
    
    * Now is possible change the border and fill color
    
    * translation to portuguese

--- clean-show-desktop@filipetorresbr/files/clean-show-desktop@filipetorresbr/applet.js
+++ clean-show-desktop@filipetorresbr/files/clean-show-desktop@filipetorresbr/applet.js
@@ -1,17 +1,23 @@
 const Applet = imports.ui.applet;
 const Main = imports.ui.main;
 const Settings = imports.ui.settings;
-const Tweener = imports.ui.tweener;
 const St = imports.gi.St;
 const { AppletSettings } = imports.ui.settings; 
-const SignalManager = imports.misc.signalManager;
-const Mainloop = imports.mainloop;
 const PopupMenu = imports.ui.popupMenu;
-const Clutter = imports.gi.Clutter;
 const Lang = imports.lang;
+const Gettext = imports.gettext;
+const GLib = imports.gi.GLib;
+const SignalManager = imports.misc.signalManager;
+const Tweener = imports.ui.tweener;
+const Clutter = imports.gi.Clutter;
+const Mainloop = imports.mainloop;
 
+const UUID = "clean-show-desktop@filipetorresbr";
+Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale");
 
-
+function _(str) {
+    return Gettext.dgettext(UUID, str);
+}
 
 class CleanShowDesktop extends Applet.IconApplet {
     constructor(orientation, panel_height, instance_id) {
@@ -19,19 +25,30 @@ class CleanShowDesktop extends Applet.IconApplet {
 
         this.settings = new AppletSettings(this, "clean-show-desktop@filipetorresbr", instance_id);
         this.settings.bind("peek-at-desktop", "peek_at_desktop");
+        this.settings.bind("peek-blur", "peek_blur");
         this.settings.bind("peek-delay", "peek_delay");
         this.settings.bind("peek-opacity", "peek_opacity");
-        this.settings.bind("peek-blur", "peek_blur");
+        this.settings.bind("width", "width");
+        this.settings.bind("background", "background");
+        this.settings.bind("border", "border");
+        this.settings.bind("on-hover-background", "hoverbackground");
+        this.settings.bind("on-hover-border", "hoverborder");
+        this.settings.bind("border-width", "borderwidth");
         this.actor.track_hover = true;
+
+        this.signals = new SignalManager.SignalManager(null);
+        this.actor.connect('enter-event', Lang.bind(this, this._on_enter));
+        this.actor.connect('leave-event', Lang.bind(this, this._on_leave));
+        this.signals.connect(global.stage, 'notify::key-focus', this._on_leave, this);
+
+        this._appletEnterEventId = 0;
+        this._appletLeaveEventId = 0;
+        this._appletHoverDelayId = 0;
+        this._updateActivateOnHover();
         this.setAllowedLayout(Applet.AllowedLayout.BOTH);
-        this.actor.set_style_class_name("spacer-box");
         this.bin = new St.Bin();
         this.actor.add(this.bin);
-        this.settings.bind("width", "width", this.size_changed);
-        global.settings.connect('changed::panel-edit-mode', Lang.bind(this, this.on_pane_edit_mode_changed));
-        this.orientation = orientation;
-        this.size_changed();
-        this.on_pane_edit_mode_changed();
+        this.onSettingsChanged();
         this._did_peek = true;
         this._peek_timeout_id = 0;
 
@@ -48,6 +65,66 @@ class CleanShowDesktop extends Applet.IconApplet {
         this.signals.disconnectAllSignals();
     }
 
+    onSettingsChanged(){
+        const BACKGROUND = this.background;
+        const BORDERCOLOR = this.border; 
+        const BORDERWIDTH = this.borderwidth;
+        const WIDTH = this.width / 3;
+        this.actor.set_style("width: " + WIDTH + "px;background: " + BACKGROUND + ";border-color: " + BORDERCOLOR +  ";border-width: " + BORDERWIDTH + "px;");
+    }
+
+    _clearDelayCallbacks() {
+        if (this._appletHoverDelayId > 0) {
+            Mainloop.source_remove(this._appletHoverDelayId);
+            this._appletHoverDelayId = 0;
+        }
+        if (this._appletLeaveEventId > 0) {
+            this.actor.disconnect(this._appletLeaveEventId);
+            this._appletLeaveEventId = 0;
+        }
+
+        return false;
+    }
+
+    _updateActivateOnHover() {
+        if (this._appletEnterEventId > 0) {
+            this.actor.disconnect(this._appletEnterEventId);
+            this._appletEnterEventId = 0;
+        }
+
+        this._clearDelayCallbacks();
+
+        this._appletEnterEventId = this.actor.connect('enter-event', () => {
+            if (this.hover_delay_ms > 0) {
+                this._appletHoverDelayId = Mainloop.timeout_add(this.hover_delay_ms,
+                    () => {
+                        const HOVERBACKGROUND = this.hoverbackground;
+                        const HOVERBORDER = this.hoverborder; 
+                        const BORDERWIDTH = this.borderwidth;
+                        const WIDTH = this.width / 3;
+                        this.actor.set_style("width: " + WIDTH + "px;background: " + HOVERBACKGROUND + ";border-color: " + HOVERBORDER +  ";border-width: " + BORDERWIDTH + "px;");
+                        this._clearDelayCallbacks();
+                    });
+            } else {
+                        const HOVERBACKGROUND = this.hoverbackground;
+                        const HOVERBORDER = this.hoverborder; 
+                        const BORDERWIDTH = this.borderwidth;
+                        const WIDTH = this.width / 3;
+
+                        this.actor.set_style("width: " + WIDTH + "px;background: " + HOVERBACKGROUND + ";border-color: " + HOVERBORDER +  ";border-width: " + BORDERWIDTH + "px;");
+
+            }
+        });
+        
+        this._appletLeaveEventId = this.actor.connect('leave-event', () => { 
+                        const BACKGROUND = this.background;
+                        const BORDERCOLOR =  this.border; 
+                        const BORDERWIDTH = this.borderwidth;
+                        const WIDTH = this.width / 3;
+
+                        this.actor.set_style("width: " + WIDTH + "px;background: " + BACKGROUND + ";border-color: " + BORDERCOLOR +  ";border-width: " + BORDERWIDTH + "px;");
+        });
+    }
     show_all_windows(time) {
         let windows = global.get_window_actors();
         for(let i = 0; i < windows.length; i++){
@@ -77,7 +154,7 @@ class CleanShowDesktop extends Applet.IconApplet {
                     !global.settings.get_boolean("panel-edit-mode")) {
 
                     Tweener.addTween(global.window_group,
-                                     {opacity: 255, time: 0.275, transition: "easeInSine"});
+                                     {opacity: this.peek_opacity, time: 0.275, transition: "easeInSine"});
 
                     let windows = global.get_window_actors();
                     for (let i = 0; i < windows.length; i++) {
@@ -127,40 +204,9 @@ class CleanShowDesktop extends Applet.IconApplet {
             Main.deskletContainer.raise();
         }
     }
-    on_pane_edit_mode_changed(settings, key) {
-        if (global.settings.get_boolean("panel-edit-mode")) {
-            this.actor.add_style_class_name("edit-mode");
-        } else {
-            this.actor.add_style_class_name("edit-mode");
-        }
-    }
-
-    on_orientation_changed(neworientation) {
-        this.orientation = neworientation;
-        this.size_changed();
-    }
-
-    size_changed() {
-        let scaled_width = this.width * global.ui_scale;
-
-        if (this.orientation == St.Side.TOP || this.orientation == St.Side.BOTTOM) {
-            this.bin.natural_width = scaled_width;
-            this.bin.natural_height_set = false;
-        }
-        else {
-            this.bin.natural_height = scaled_width;
-            this.bin.natural_width_set = false
-        }
-    }
-
-    on_applet_removed_from_panel() {
-        this.settings.finalize();
-    }
 }
 
 function main(metadata, orientation, panel_height, instance_id) {
     return new CleanShowDesktop(orientation, panel_height, instance_id);
 }
-
-
 
