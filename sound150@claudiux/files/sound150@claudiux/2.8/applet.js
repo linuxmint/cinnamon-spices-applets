36d3daf5f8da360206affac7c21e06638597064a
claudiux <33965039+claudiux@users.noreply.github.com>
2020-07-31 23:34:26 +0200

    [sound150@claudiux] v2.0.0 (#3157)
    * sound150@claudiux v2.0.0
    
    * remove an useless file

--- sound150@claudiux/files/sound150@claudiux/2.8/applet.js
+++ sound150@claudiux/files/sound150@claudiux/2.8/applet.js
@@ -787,6 +787,8 @@ Player.prototype = {
             if ( this._name === "spotify" ) {
                 artUrl = artUrl.replace("/thumb/", "/300/"); // Spotify 0.9.x
                 artUrl = artUrl.replace("/image/", "/300/"); // Spotify 0.27.x
+            } else if (this._name == "Spotify") {
+                artUrl = artUrl.replace("open.spotify.com","i.scdn.co"); // Fix missing cover art
             }
             if (this._trackCoverFile != artUrl) {
                 this._trackCoverFile = artUrl;
