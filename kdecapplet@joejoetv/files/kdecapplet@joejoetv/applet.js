7788b49b9598fcbc6020effb090cb702aaba9cc7
JoeJoeTV <hannes1033@gmail.com>
2020-09-20 13:41:19 +0200

    [kdecapplet@joejoetv] Update Language Files; Fix Menu Bug;

--- kdecapplet@joejoetv/files/kdecapplet@joejoetv/applet.js
+++ kdecapplet@joejoetv/files/kdecapplet@joejoetv/applet.js
@@ -387,6 +387,7 @@ KDEConnectApplet.prototype = {
         try {
             deviceIDs = this.kdecProxy.devicesSync(true, true)[0];
             deviceNames = this.kdecProxy.deviceNamesSync(true, true)[0];
+            this.onlyOneDevice = deviceIDs.length == 1;
         }
         catch (error) {
             global.logError(error);
@@ -399,8 +400,6 @@ KDEConnectApplet.prototype = {
             this.set_applet_tooltip(_("No reachable paired devices!"));
             this.set_applet_label("");
         } else {
-            this.onlyOneDevice = deviceIDs.length == 1;
-
             this.set_applet_label(deviceIDs.length.toString());
             if (deviceIDs.length > 1) {
                 this.set_applet_tooltip(deviceIDs.length+" "+_("Devices"));
