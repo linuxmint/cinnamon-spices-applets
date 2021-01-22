bbd3b180df13885431115d09888c09eeabb77091
Gr3q <floyd0122@gmail.com>
2021-01-22 05:49:14 +0000

    Factored out Bottom Bar from ui

--- weather@mockturtl/files/weather@mockturtl/3.8/ui.js
+++ weather@mockturtl/files/weather@mockturtl/3.8/ui.js
@@ -1,24 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UI = void 0;
-const consts_1 = require("./consts");
 const uiCurrentWeather_1 = require("./uiCurrentWeather");
 const logger_1 = require("./logger");
 const utils_1 = require("./utils");
-const weatherbutton_1 = require("./weatherbutton");
 const uiForecasts_1 = require("./uiForecasts");
 const uiHourlyForecasts_1 = require("./uiHourlyForecasts");
+const uiBar_1 = require("./uiBar");
 const { PopupMenuManager, PopupSeparatorMenuItem } = imports.ui.popupMenu;
-const { Bin, BoxLayout, Side, IconType, Label, ScrollView, Icon, Align, Widget } = imports.gi.St;
-const { PolicyType } = imports.gi.Gtk;
-const { addTween } = imports.ui.tweener;
+const { BoxLayout, IconType, Label } = imports.gi.St;
 const Lang = imports.lang;
-const { GridLayout } = imports.gi.Clutter;
 const { AppletPopupMenu } = imports.ui.applet;
 const { themeManager } = imports.ui.main;
 const { SignalManager } = imports.misc.signalManager;
 const STYLE_WEATHER_MENU = 'weather-menu';
-const STYLE_BAR = 'bottombar';
 class UI {
     constructor(app, orientation) {
         this.lightTheme = false;
@@ -37,14 +32,12 @@ class UI {
     ShowHourlyWeather() {
         this.HourlyWeather.Show();
         this._separatorAreaHourly.actor.show();
-        if (!!this._hourlyButton.child)
-            this._hourlyButton.child.icon_name = "custom-up-arrow-symbolic";
+        this.Bar.SwitchButtonToHide();
     }
     HideHourlyWeather() {
         this.HourlyWeather.Hide();
         this._separatorAreaHourly.actor.hide();
-        if (!!this._hourlyButton.child)
-            this._hourlyButton.child.icon_name = "custom-down-arrow-symbolic";
+        this.Bar.SwitchButtonToShow();
     }
     ToggleHourlyWeather() {
         if (this.HourlyWeather.Toggled) {
@@ -90,6 +83,8 @@ class UI {
         this.CurrentWeather = new uiCurrentWeather_1.CurrentWeather(this.App, this);
         this.FutureWeather = new uiForecasts_1.UIForecasts(this.App);
         this.HourlyWeather = new uiHourlyForecasts_1.UIHourlyForecasts(this.App, this.menu);
+        this.Bar = new uiBar_1.UIBar(this.App);
+        this.Bar.ToggleClicked.Subscribe(Lang.bind(this, this.ToggleHourlyWeather));
         this._separatorArea = new PopupSeparatorMenuItem();
         this._separatorAreaHourly = new PopupSeparatorMenuItem();
         this._separatorArea2 = new PopupSeparatorMenuItem();
@@ -97,7 +92,6 @@ class UI {
         this._separatorAreaHourly.actor.remove_style_class_name("popup-menu-item");
         this._separatorArea2.actor.remove_style_class_name("popup-menu-item");
         this._separatorAreaHourly.actor.hide();
-        this._bar = new BoxLayout({ vertical: false, style_class: STYLE_BAR });
         let mainBox = new BoxLayout({ vertical: true });
         mainBox.add_actor(this.CurrentWeather.actor);
         mainBox.add_actor(this._separatorAreaHourly.actor);
@@ -105,18 +99,18 @@ class UI {
         mainBox.add_actor(this._separatorArea.actor);
         mainBox.add_actor(this.FutureWeather.actor);
         mainBox.add_actor(this._separatorArea2.actor);
-        mainBox.add_actor(this._bar);
+        mainBox.add_actor(this.Bar.Actor);
         this.menu.addActor(mainBox);
     }
     Toggle() {
         this.menu.toggle();
     }
     Rebuild(config) {
-        this.showLoadingUi();
+        this.ShowLoadingUi();
         this.CurrentWeather.Rebuild(config, this.GetTextColorStyle());
         this.HourlyWeather.Rebuild(config, this.GetTextColorStyle());
         this.FutureWeather.Rebuild(config, this.GetTextColorStyle());
-        this.rebuildBar(config);
+        this.Bar.Rebuild(config);
     }
     UpdateIconType(iconType) {
         if (iconType == IconType.FULLCOLOR && this.App.config._useCustomMenuIcons)
@@ -126,88 +120,25 @@ class UI {
         this.HourlyWeather.UpdateIconType(iconType);
     }
     DisplayErrorMessage(msg) {
-        this._timestamp.text = msg;
+        this.Bar.DisplayErrorMessage(msg);
     }
     Display(weather, config, provider) {
         this.CurrentWeather.Display(weather, config);
         this.FutureWeather.Display(weather, config);
         let shouldShowToggle = this.HourlyWeather.Display(weather.hourlyForecasts, config, weather.location.timeZone);
-        if (!shouldShowToggle)
-            this.HideHourlyToggle();
-        this.DisplayBar(weather, provider, config);
+        this.Bar.Display(weather, provider, config, shouldShowToggle);
         return true;
     }
-    DisplayBar(weather, provider, config) {
-        this._providerCredit.label = utils_1._("Powered by") + " " + provider.prettyName;
-        this._providerCredit.url = provider.website;
-        this._timestamp.text = utils_1._("As of") + " " + utils_1.AwareDateString(weather.date, this.App.config.currentLocale, config._show24Hours);
-        if (weather.location.distanceFrom != null) {
-            this._timestamp.text += (", " + utils_1.MetreToUserUnits(weather.location.distanceFrom, this.App.config.DistanceUnit)
-                + this.BigDistanceUnitFor(this.App.config.DistanceUnit) + " " + utils_1._("from you"));
-        }
-        return true;
-    }
-    BigDistanceUnitFor(unit) {
-        if (unit == "imperial")
-            return utils_1._("mi");
-        return utils_1._("km");
-    }
-    destroyBar() {
-        this._bar.destroy_all_children();
-    }
-    showLoadingUi() {
+    ShowLoadingUi() {
         this.CurrentWeather.Destroy();
         this.FutureWeather.Destroy();
-        this.destroyBar();
+        this.Bar.Destroy();
         this.CurrentWeather.actor.set_child(new Label({
             text: utils_1._('Loading current weather ...')
         }));
         this.FutureWeather.actor.set_child(new Label({
             text: utils_1._('Loading future weather ...')
         }));
-    }
-    rebuildBar(config) {
-        this.destroyBar();
-        this._timestamp = new Label({ text: "Placeholder" });
-        this._bar.add(this._timestamp, {
-            x_fill: false,
-            x_align: Align.START,
-            y_align: Align.MIDDLE,
-            y_fill: false,
-            expand: true
-        });
-        this._hourlyButton = new weatherbutton_1.WeatherButton({
-            reactive: true,
-            can_focus: true,
-            child: new Icon({
-                icon_type: IconType.SYMBOLIC,
-                icon_size: 12,
-                icon_name: "custom-down-arrow-symbolic"
-            }),
-        }).actor;
-        this._hourlyButton.connect(consts_1.SIGNAL_CLICKED, Lang.bind(this, this.ToggleHourlyWeather));
-        this._bar.add(this._hourlyButton, {
-            x_fill: false,
-            x_align: Align.MIDDLE,
-            y_align: Align.MIDDLE,
-            y_fill: false,
-            expand: true
-        });
-        if (this.App.GetMaxHourlyForecasts() <= 0) {
-            this.HideHourlyToggle();
-        }
-        this._providerCredit = new weatherbutton_1.WeatherButton({ label: utils_1._(consts_1.ELLIPSIS), reactive: true }).actor;
-        this._providerCredit.connect(consts_1.SIGNAL_CLICKED, Lang.bind(this, this.App.OpenUrl));
-        this._bar.add(this._providerCredit, {
-            x_fill: false,
-            x_align: Align.END,
-            y_align: Align.MIDDLE,
-            y_fill: false,
-            expand: true
-        });
-    }
-    HideHourlyToggle() {
-        this._hourlyButton.child = null;
     }
 }
 exports.UI = UI;
--- weather@mockturtl/src/ui.ts
+++ weather@mockturtl/src/ui.ts
@@ -1,27 +1,22 @@
-import { Config, DistanceUnits } from "./config";
-import { ELLIPSIS, FORWARD_SLASH, APPLET_ICON, SIGNAL_CLICKED } from "./consts";
+import { Config } from "./config";
 import { CurrentWeather } from "./uiCurrentWeather";
 import { Log } from "./logger";
 import { WeatherApplet } from "./main";
-import { WeatherData, WeatherProvider, HourlyForecastData } from "./types";
-import { shadeHexColor, delay, capitalizeFirstLetter, _, AwareDateString, TempToUserConfig, GetHoursMinutes, GetDayName, MetreToUserUnits, MillimeterToUserUnits, UnitToUnicode } from "./utils";
-import { WeatherButton } from "./weatherbutton";
+import { WeatherData, WeatherProvider } from "./types";
+import { shadeHexColor, delay, _ } from "./utils";
 import { UIForecasts } from "./uiForecasts";
 import { UIHourlyForecasts } from "./uiHourlyForecasts";
+import { UIBar } from "./uiBar";
 
 const { PopupMenuManager, PopupSeparatorMenuItem } = imports.ui.popupMenu;
-const { Bin, BoxLayout, Side, IconType, Label, ScrollView, Icon, Align, Widget } = imports.gi.St;
-const { PolicyType } = imports.gi.Gtk;
-const { addTween } = imports.ui.tweener;
+const { BoxLayout, IconType, Label} = imports.gi.St;
 const Lang: typeof imports.lang = imports.lang;
-const { GridLayout } = imports.gi.Clutter;
 const { AppletPopupMenu } = imports.ui.applet;
 const { themeManager } = imports.ui.main;
 const { SignalManager } = imports.misc.signalManager;
 
 // stylesheet.css
 const STYLE_WEATHER_MENU = 'weather-menu'
-const STYLE_BAR = 'bottombar'
 
 /** Roll-down Popup Menu */
 export class UI {
@@ -31,18 +26,9 @@ export class UI {
     private _separatorAreaHourly: imports.ui.popupMenu.PopupSeparatorMenuItem;
 
     private CurrentWeather: CurrentWeather;
-
-    // Daily Weather
     private FutureWeather: UIForecasts;
-
-    // Bottom Bar
-    private _providerCredit: imports.gi.St.Button;
-    private _bar: imports.gi.St.BoxLayout;
-    private _hourlyButton: imports.gi.St.Button;
-    private _timestamp: imports.gi.St.Label;
-
-    // Hourly Weather
     private HourlyWeather: UIHourlyForecasts;
+    private Bar: UIBar;
 
     // State variables
     private lightTheme: boolean = false;
@@ -75,13 +61,13 @@ export class UI {
     public ShowHourlyWeather(): void {
         this.HourlyWeather.Show();
         this._separatorAreaHourly.actor.show();
-        if (!!this._hourlyButton.child) this._hourlyButton.child.icon_name = "custom-up-arrow-symbolic";
+        this.Bar.SwitchButtonToHide();
     }
 
     public HideHourlyWeather(): void {
         this.HourlyWeather.Hide();
         this._separatorAreaHourly.actor.hide();
-        if (!!this._hourlyButton.child) this._hourlyButton.child.icon_name = "custom-down-arrow-symbolic";
+        this.Bar.SwitchButtonToShow();
     }
 
     public ToggleHourlyWeather(): void {
@@ -152,12 +138,11 @@ export class UI {
 
     /** Creates th skeleton of the popup menu */
     private BuildPopupMenu(): void {
-        //  Current Weather
         this.CurrentWeather = new CurrentWeather(this.App, this);
-        //  Daily Weather
         this.FutureWeather = new UIForecasts(this.App);
-
         this.HourlyWeather = new UIHourlyForecasts(this.App, this.menu);
+        this.Bar = new UIBar(this.App);
+        this.Bar.ToggleClicked.Subscribe(Lang.bind(this, this.ToggleHourlyWeather));
 
         // Separators and removing styling to make them span full width 
         this._separatorArea = new PopupSeparatorMenuItem()
@@ -170,9 +155,6 @@ export class UI {
         
         this._separatorAreaHourly.actor.hide();
 
-        // Bottom bar
-        this._bar = new BoxLayout({ vertical: false, style_class: STYLE_BAR });
-
         // Add everything to the PopupMenu
         let mainBox = new BoxLayout({ vertical: true })
         mainBox.add_actor(this.CurrentWeather.actor)
@@ -181,7 +163,7 @@ export class UI {
         mainBox.add_actor(this._separatorArea.actor)
         mainBox.add_actor(this.FutureWeather.actor)
         mainBox.add_actor(this._separatorArea2.actor)
-        mainBox.add_actor(this._bar)
+        mainBox.add_actor(this.Bar.Actor)
         this.menu.addActor(mainBox)
 	}
 	
@@ -191,11 +173,11 @@ export class UI {
 
     /** Fully rebuilds UI */
     public Rebuild(config: Config): void {
-        this.showLoadingUi();
+        this.ShowLoadingUi();
         this.CurrentWeather.Rebuild(config, this.GetTextColorStyle());
         this.HourlyWeather.Rebuild(config, this.GetTextColorStyle());
         this.FutureWeather.Rebuild(config, this.GetTextColorStyle());
-        this.rebuildBar(config);
+        this.Bar.Rebuild(config);
     }
 
 	/** Changes all icon's type what are affected by
@@ -209,106 +191,27 @@ export class UI {
     }
 
     public DisplayErrorMessage(msg: string) {
-        this._timestamp.text = msg;
+        this.Bar.DisplayErrorMessage(msg);
 	}
 
 	public Display(weather: WeatherData, config: Config, provider: WeatherProvider): boolean {
 		this.CurrentWeather.Display(weather, config);
 		this.FutureWeather.Display(weather, config);
         let shouldShowToggle = this.HourlyWeather.Display(weather.hourlyForecasts, config, weather.location.timeZone);
-        if (!shouldShowToggle)
-            this.HideHourlyToggle();
-		this.DisplayBar(weather, provider, config);
+		this.Bar.Display(weather, provider, config, shouldShowToggle);
 		return true;
 	}
 
-    private DisplayBar(weather: WeatherData, provider: WeatherProvider, config: Config): boolean {
-        this._providerCredit.label = _("Powered by") + " " + provider.prettyName;
-        this._providerCredit.url = provider.website;
-        this._timestamp.text = _("As of") + " " + AwareDateString(weather.date, this.App.config.currentLocale, config._show24Hours);
-        if (weather.location.distanceFrom != null) {
-            this._timestamp.text += (
-                ", " + MetreToUserUnits(weather.location.distanceFrom, this.App.config.DistanceUnit)
-                + this.BigDistanceUnitFor(this.App.config.DistanceUnit) + " " + _("from you")
-            );
-        }
-        return true;
-    }
-
-	/**
-	 * 
-	 * @param unit 
-	 * @return km or mi, based on unit
-	 */
-    private BigDistanceUnitFor(unit: DistanceUnits) {
-        if (unit == "imperial") return _("mi");
-        return _("km");
-    }
-
-    private destroyBar(): void {
-        this._bar.destroy_all_children();
-    }
-
     /** Destroys UI first then shows initial UI */
-    private showLoadingUi(): void {
+    private ShowLoadingUi(): void {
         this.CurrentWeather.Destroy();
         this.FutureWeather.Destroy();
-        this.destroyBar()
+        this.Bar.Destroy()
         this.CurrentWeather.actor.set_child(new Label({
             text: _('Loading current weather ...')
         }))
         this.FutureWeather.actor.set_child(new Label({
             text: _('Loading future weather ...')
         }))
-    }
-
-    private rebuildBar(config: Config) {
-        this.destroyBar();
-        this._timestamp = new Label({ text: "Placeholder" });
-        this._bar.add(this._timestamp, {
-            x_fill: false,
-            x_align: Align.START,
-            y_align: Align.MIDDLE,
-            y_fill: false,
-            expand: true
-        })
-
-        this._hourlyButton = new WeatherButton({
-            reactive: true,
-            can_focus: true,
-            child: new Icon({
-                icon_type: IconType.SYMBOLIC,
-                icon_size: 12,
-                icon_name: "custom-down-arrow-symbolic"
-            }),
-        }).actor;
-        this._hourlyButton.connect(SIGNAL_CLICKED, Lang.bind(this, this.ToggleHourlyWeather));
-        this._bar.add(this._hourlyButton, {
-            x_fill: false,
-            x_align: Align.MIDDLE,
-            y_align: Align.MIDDLE,
-            y_fill: false,
-            expand: true
-        })
-
-        // Hide if Hourly forecasts are not supported
-        if (this.App.GetMaxHourlyForecasts() <= 0) {
-            this.HideHourlyToggle();
-        }
-
-        this._providerCredit = new WeatherButton({ label: _(ELLIPSIS), reactive: true }).actor;
-        this._providerCredit.connect(SIGNAL_CLICKED, Lang.bind(this, this.App.OpenUrl));
-
-        this._bar.add(this._providerCredit, {
-            x_fill: false,
-            x_align: Align.END,
-            y_align: Align.MIDDLE,
-            y_fill: false,
-            expand: true
-        });
-    }
-
-    private HideHourlyToggle() {
-        this._hourlyButton.child = null;
     }
 }
