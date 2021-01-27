8aa07c40622efc2ad2abff8c1778080ee34cdffa
Attila Greguss <floyd0122@gmail.com>
2021-01-27 08:50:05 +0000

    Remove accidentally left file

--- weather@mockturtl/files/weather@mockturtl/3.8/services.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Http = exports.Logger = void 0;
-const httpLib_1 = require("./httpLib");
-const logger_1 = require("./logger");
-exports.Logger = new logger_1.Log();
-exports.Http = new httpLib_1.HttpLib();
