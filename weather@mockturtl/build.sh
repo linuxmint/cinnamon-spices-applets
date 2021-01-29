2a3ce4dd33affb3f44c3629bc070b98708feec84
Gr3q <floyd0122@gmail.com>
2021-01-29 18:46:25 +0000

    Move 3.8 code into it's own folder

--- weather@mockturtl/build.sh
+++ weather@mockturtl/build.sh
@@ -14,7 +14,7 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
 # Save current dir for convenience
 path=${PWD}
 
-cd $DIR/src
+cd $DIR/src/3_8
 echo Building 3.8...
-tsc -p ../tsconfig.json
+tsc -p tsconfig.json
 cd $DIR
