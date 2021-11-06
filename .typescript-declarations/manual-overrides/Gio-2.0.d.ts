b84136b39ba47572f143815b5559862cc56677e1
Gr3qi <floyd0122@gmail.com>
2021-11-06 09:48:27 +0000

    Change docstring

--- .typescript-declarations/manual-overrides/Gio-2.0.d.ts
+++ .typescript-declarations/manual-overrides/Gio-2.0.d.ts
@@ -6,10 +6,17 @@ declare namespace imports.gi.Gio {
     type ChangedVariable = `changed::${string}`;
     interface ISettings {
         /**
-         * Generic signal function for support for any schema
-         * @param event 
-         * @param callback 
-         */
+		 * The "changed" signal is emitted when a key has potentially changed.
+		 * You should call one of the g_settings_get() calls to check the new
+		 * value.
+		 * 
+		 * This signal supports detailed connections.  You can connect to the
+		 * detailed signal "changed::x" in order to only receive callbacks
+		 * when key "x" changes.
+		 * 
+		 * Note that #settings only emits this signal if you have read #key at
+		 * least once while a signal handler was already connected for #key.
+		 */
         connect(event: ChangedVariable, callback: (...args: any[]) => void): number;
     }
 }
