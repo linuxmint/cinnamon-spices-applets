477bcbb7f0ef2267354282e9d33f8f7316cdf6ba
Peter Tandler <p.github@tandlers.de>
2021-12-08 23:25:49 +0100

    converted the Mini-Calc desklet to an applet

---
+++ calc@ptandler/README.md
@@ -0,0 +1,101 @@
+# Mini-Calculator Applet based on JavaScript Expressions
+
+Very simple and minimalistic but yet quite powerful calculator that evaluates [JavaScript expressions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators). The complete power of JS directly at your hand! ;-)
+
+Original repository: [https://github.com/ptandler/cinnamon-spices-applets](https://github.com/ptandler/cinnamon-spices-applets/tree/calc%40ptandler/calc%40ptandler)
+
+This is actually the new version of the [Mini-Calc _desklet_](https://github.com/ptandler/cinnamon-spices-desklets/tree/calc%40ptandler/calc%40ptandler). As getting the keyboard focus is a bit tricky for a desklet, I decided to go for an applet.
+
+
+## Features and Example Expressions
+
+- `1 + 2` = `3`
+- `(1 + 2) * (3 + 4)` = `21`
+- All functions defined in the [JavaScript `Math` object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math) are included using the [`with` statement](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/with) (knowing that is not generally recommended using `with` in JS, but I think here it's kind of convenient):
+    - `min(1,2)` = `1`
+    - `max(1,2)` = `2`
+    - `sqrt(16)` = `4`
+    - `abs(-1)` = `1`
+    - The **trigonometric functions** `sin(), cos(), tan(), asin(), acos(), atan(), and atan2()` expect (and return) angles in radians.
+        - Helper functions: `degToRad(180)` = approx. `3.14`, `radToDeg(PI)` = `180`
+    - `random()` returns a pseudo-random number between 0 and 1.
+    - Euler's constant: `E` = approx. `2.718`
+    - `PI` = approx. `3.14159`
+- You can define **variables**; this expression will return the assigned value. The variables will keep their value until the desklet is restarted:
+    - `a=2` = `2`
+- You can use the **comma `,` operator** to evaluate several expressions and return just the value of the last one. Can be used to define variables:
+    - `a=2, b=3, a*b`
+- For conditional expression you can use the JS `? :` operator, e.g. `1 < 2 ? "that's true" : "no, wrong"`
+- You can define **functions**:
+    - `fib = (n) => n <= 0 ? 0 : (n <= 1 ? 1 : (fib(n - 1) + fib(n - 2))), fib(7)` = `13`
+      CAUTION: Be careful with recursive functions, I'm not sure if there is a timeout for the evaluation of the expression and if it's quite long. So you can crash your desktop if evaluation hangs ... believe me, I tried ;-)
+- You can use all **builtin JavaScript** functions and global objects (available in GJS / CJS), such as `JSON.stringify`
+
+## ToDo: Ideas for Further Development
+
+- [ ] automatically set the keyboard focus as soon as the mini-calc is shown (e.g. click on icon)
+- [ ] add scrollbar to historyBox if it gets longer _(how does this work with St?)_ I might want to use a sub menu as well?
+- [ ] make max number of history entries configurable & ensure not to store more entries in the list
+- [ ] add option to convert locale number format (e.g. `1,23`) to JS notation (i.e. `1.23`) to avoid errors for those used to one of these formats!
+- [ ] add button and keyboard shortcut for "copy value to **clipboard**"
+- [ ] add icon to open message box for documentation and introduction that **explains some JS expression syntax** (e.g. the examples above)
+- [ ] add **translations** (i18n)
+- [ ] add option to use the result of current expression as new input when pressing `Enter` key (instead of empty string)
+- [ ] add keyboard shortcuts to show / hide history (e.g. Ctrl H)
+- [ ] add keyboard shortcuts to navigate in history (e.g. Ctrl arrow up / down) - should also show history if hidden
+- [ ] add option to make history persistent and save last N entries it to some sensible place (where? settings?)
+- [ ] add option to clear history
+- [ ] add option to turn off to use `JSON.stringify()` to display results
+
+## Changelog
+
+### version 0.3, xxx
+
+- migrated the desklet to an applet, hope that's more convenient w.r.t. getting the keyboard focus
+
+### version 0.2, 2021-12-08
+
+- [x] add expression & result **history** as dropdown with scrollbar (on pressing "enter" + put result as input text and place cursor & focus)
+- [x] add action copy input or result from history to current expression (by click)
+
+### version 0.1, 2021-09-21
+
+- initial version: just simple evaluation
+
+## Development Hints
+
+This is the first desklet and also first Gnome app I developed. It was a bit tricky for me to find helpful documentation. Here some things that were helpful for me.
+
+You might also want to check [my question about introduction documentation at StackOverflow](https://stackoverflow.com/questions/69312633/introduction-in-developing-cinnamon-shell-extension-desklet-in-cjs-gjs-gnom).
+
+### Gnome
+
+Kind of useful Documentation:
+
+- [GnomeShell Extensions wiki](https://wiki.gnome.org/Projects/GnomeShell/Extensions)
+- [Gnome developer documentation](https://developer.gnome.org/documentation/introduction.html)
+- [Gnome GTK API 4.0 documentation](https://docs.gtk.org/gtk4/#classes)
+- [Gnome's St widget library](https://gjs-docs.gnome.org/st10~1.0_api/) and the doc of [St's JS binding](https://www.roojs.com/seed/gir-1.2-gtk-3.0/seed/St.html)
+- [Introduction to Gnome JS (GJS) extensions](https://gjs.guide/extensions/overview/anatomy.html#prefs-js) in general
+- https://gjs.guide/guides/gtk/3/ Gtk / GJS Guide
+- This is actually pretty helpful! https://github.com/julio641742/gnome-shell-extension-reference
+
+### Cinnamon
+
+- source can be directly put in .local/share/cinnamon/uuid (where `uuid` is the UUID of you desklet)
+- use `Cinnamon Looking Glass` ("Melange") to
+    - reload code after changes!! (tab "Extension")
+    - see log messages
+- I had to restart Cinnamon in order to get the CSS reloaded, not sure if there is another possibility.
+- The directory of the desklets listed on https://cinnamon-spices.linuxmint.com/ is based on https://github.com/linuxmint/cinnamon-spices-desklets
+- The [scollin's developer's tools desklet](https://cinnamon-spices.linuxmint.com/desklets/view/17) is quite helpful!
+
+#### Documentation:
+
+- [A bit too short introduction in writing Cinnamon desklets](http://www.erikedrosa.com/2014/12/31/hello-world-desklet-tutorial.html)
+- Cinnamon-specific tutorials https://projects.linuxmint.com/reference/git/cinnamon-tutorials/
+- https://nickdurante.github.io/development/Writing-a-Cinnamon-Applet/
+- https://github.com/gustavo-iniguez-goya/arpsentinel-applet/wiki/Collaborating-and-resources
+    - "Read the source code (very useful): /usr/share/cinnamon/js/ui/"
+- [Source of the Cinnamon docs](https://github.com/linuxmint/cinnamon/tree/master/docs/reference)
+    - but also as [MarkDown version here](https://github.com/linuxmint/linuxmint.github.io/tree/master/reference/git) ... is this generated from the above?
---
+++ calc@ptandler/files/calc@ptandler/applet.js
@@ -0,0 +1,269 @@
+const Applet = imports.ui.applet;
+const PopupMenu = imports.ui.popupMenu;
+const St = imports.gi.St;
+const Clutter = imports.gi.Clutter;
+
+const ICON = "icon.png";
+
+function formatLog(...data) {
+    let str = "";
+    for (const d of data) {
+        if (d === null) str += "<null>"
+        else if (d === undefined) str += "<undefined>"
+        else str += typeof d === "string" || typeof d === "number" ? d : JSON.stringify(d, null, 2)
+    }
+    return str;
+}
+
+function logInfo(...data) {
+    global.log(formatLog(...data));
+}
+
+function logWarn(...data) {
+    global.logWarning(formatLog(...data));
+}
+
+function logError(...data) {
+    global.logError(formatLog(...data));
+}
+
+class EvalError {
+    constructor(message) {
+        this.message = message;
+    }
+
+    toString() {
+        return this.message;
+    }
+}
+
+/** avoid `eval()`
+ *
+ * @param input
+ */
+function evalExpression(input) {
+    try {
+        const helper_functions =
+            "var degToRad = (degrees) => degrees * (Math.PI / 180);" +
+            "var radToDeg = (rad) => rad / (Math.PI / 180);"
+        const result = Function(helper_functions + "with (Math) return " + input)();
+        if (result === undefined) return new EvalError("undefined");
+        if (result === null) return new EvalError("null");
+        return JSON.stringify(result);
+    } catch (e) {
+        return new EvalError("error: " + e.message);
+    }
+}
+
+class MiniCalc extends Applet.TextIconApplet {
+
+    constructor(metadata, orientation, panel_height, instance_id) {
+        logInfo("MiniCalc.constructor");
+        super(orientation, panel_height, instance_id);
+        this.metadata = metadata;
+
+        // private properties
+
+        this.history = [];
+        this.historyExpanded = false;
+        this.currentResult = "";
+
+        // applet setup
+
+        this.set_applet_icon_symbolic_path('');
+        this.set_applet_icon_symbolic_path(this.metadata.path + ICON);
+        this.set_applet_label("Mini-Calc");
+
+        this.menuManager = new PopupMenu.PopupMenuManager(this);
+        this.menu = new Applet.AppletPopupMenu(this, orientation);
+        this.menuManager.addMenu(this.menu);
+
+        // todo register keyboard shortcut #### and make it configurable!
+
+        this.buildLayout();
+    }
+
+    buildLayout() {
+        logInfo("MiniCalc.buildLayout")
+        // main container for the desklet
+        this.widgets = {}
+
+        this.widgets.headerItem = new PopupMenu.PopupBaseMenuItem({reactive: false});
+        this.menu.addMenuItem(this.widgets.headerItem);
+        this.widgets.headerItem.addActor(new St.Label({text: "Mini-Calc _ V" + this.metadata.version}));
+
+        // this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        this.widgets.expressionItem = new PopupMenu.PopupBaseMenuItem({reactive: false, focusOnHover: true});
+        this.menu.addMenuItem(this.widgets.expressionItem);
+        this.widgets.expressionBox = new St.BoxLayout({
+            style_class: "expression-box",
+            vertical: true,
+            x_align: Clutter.ActorAlign.FILL,
+            x_expand: true
+        })
+        this.widgets.expressionItem.addActor(this.widgets.expressionBox);
+        this.buildExpression();
+        this.updateExpression("1 + 2")
+
+        this.widgets.historyItem = new PopupMenu.PopupBaseMenuItem({reactive: false});
+        this.menu.addMenuItem(this.widgets.historyItem);
+        this.widgets.historyBox = new St.BoxLayout({
+            vertical: true,
+            style_class: "history-box"
+        })
+        this.widgets.historyItem.addActor(this.widgets.historyBox);
+        this.buildHistory();
+    }
+
+    buildExpression() {
+        this.widgets.input = new St.Entry();
+        this.widgets.input.connect('key-release-event', (widget, event) => {
+            const input = widget.get_text();
+            this.handleKeyPress(event, input);
+            return true; // event has been handled
+        });
+        this.widgets.expressionBox.add_actor(this.widgets.input);
+
+        this.widgets.result = new St.Label();
+        this.widgets.resultButton = new St.Button({});
+        this.widgets.resultButton.set_child(this.widgets.result);
+        this.widgets.resultButton.connect("clicked", (/*widget, event*/) => {
+            this.appendExpression(this.currentResult);
+            return true; // event has been handled
+        });
+        this.widgets.expressionBox.add_actor(this.widgets.resultButton);
+    }
+
+    buildHistory() {
+        // logInfo("MiniCalc.buildHistory: ", this.history);
+
+        this.widgets.historyBox.remove_all_children();
+        if (!this.history) return;
+
+        const histList = [...this.history]
+        logInfo("MiniCalc.buildHistory: length = ", histList.length);
+
+        const first = histList.shift();
+        if (!first) return;
+        // logInfo("MiniCalc.buildHistory: first = ", first);
+        this.addHistoryItemUI(first, this.widgets.historyBox);
+
+        // logInfo("MiniCalc.buildHistory: remaining length = ", histList.length);
+
+        if (!histList.length) return;
+
+        // logInfo("MiniCalc.buildHistory: remaining length = ", histList.length);
+
+        const button = new St.Button({style_class: "collapse-expand-button"});
+        this.widgets.historyBox.add_actor(button)
+        button.connect("clicked", (/*widget, event*/) => {
+            this.toggleHistoryExpanded()
+            return true; // event has been handled
+        });
+
+        if (!this.historyExpanded) {
+            // button.set_child(new St.Icon({icon_name: "go-up"}))
+            button.set_child(new St.Label({text: "expand history", style_class: "calc-displayText-primary"}))
+            return
+        }
+        // button.set_child(new St.Icon({icon_name: "go-down"}))
+        button.set_child(new St.Label({text: "hide history", style_class: "calc-displayText-primary"}))
+
+        let count = 0;
+        for (const item of histList) {
+            if (count++ > 10) break;
+            this.addHistoryItemUI(item, this.widgets.historyBox)
+        }
+    }
+
+    addHistoryItemUI(item, historyBox) {
+        const inputButton = new St.Button({
+            label: item.input
+        });
+        inputButton.connect('clicked', (/*widget, event*/) => {
+            this.appendExpression(item.input);
+            return true; // event has been handled
+        });
+        historyBox.add_actor(inputButton);
+
+        const resultButton = new St.Button({
+            label: "= " + item.result
+        });
+        resultButton.connect('clicked', (/*widget, event*/) => {
+            this.appendExpression(item.result);
+            return true; // event has been handled
+        });
+        historyBox.add_actor(resultButton);
+    }
+
+    pushToHistory(input, result) {
+        if (!input) return;
+        if (result instanceof EvalError) return;
+        logInfo("MiniCalc: pushToHistory: " + input + " -> " + result);
+        const item = {input, result};
+        this.history.unshift(item);
+        this.buildHistory();
+    }
+
+    toggleHistoryExpanded() {
+        this.historyExpanded = !this.historyExpanded
+        this.buildHistory()
+    }
+
+
+    on_applet_added_to_panel() {
+        logInfo("MiniCalc: added to panel");
+    }
+
+
+    on_applet_clicked(/*event*/) {
+        this.menu.toggle();
+    }
+
+    on_applet_removed_from_panel() {
+        logInfo("MiniCalc: removed from panel");
+        // this.settings.finalize();
+        // Main.keybindingManager.removeHotKey("keyToggle");
+    }
+
+
+    handleKeyPress(event, input) {
+        const result = this.updateResult(input);
+        const keySymbol = event.get_key_symbol();
+        if (keySymbol === Clutter.Return || keySymbol === Clutter.KP_Enter) {
+            this.pushToHistory(input, result);
+            this.updateExpression(result);
+        }
+    }
+
+    updateExpression(newInput) {
+        this.widgets.input.set_text(newInput);
+        this.updateResult(newInput);
+    }
+
+    appendExpression(newInput) {
+        if (!newInput) return;
+        const input = this.widgets.input.get_text();
+        this.updateExpression(input + (input ? " " : "") + newInput);
+    }
+
+    updateResult(input) {
+        const result = evalExpression(input);
+        // logInfo("MiniCalc: updateResult: " + input + " -> " + result);
+        if (result instanceof EvalError && result.toString().startsWith("error: expected expression, got '}'")) {
+            // this.widgets.result.set_text("...");
+        } else {
+            this.currentResult = result instanceof EvalError ? "" : result;
+            this.widgets.result.set_text(input
+                ? ((result instanceof EvalError ? " ? " : " = ") + result)
+                : ""
+            );
+        }
+        return result;
+    }
+}
+
+function main(metadata, orientation, panel_height, instance_id) {
+    return new MiniCalc(metadata, orientation, panel_height, instance_id);
+}
