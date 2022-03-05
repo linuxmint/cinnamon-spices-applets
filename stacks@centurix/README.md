0e676df486f752f0272bc65252b8b04771e53843
Chris Read <centurix@gmail.com>
2022-03-05 11:26:58 +1000

    Skeleton applet for Stacks

---
+++ stacks@centurix/README.md
@@ -0,0 +1,21 @@
+Stacks Manager Applet
+=====================
+
+Manage docker-compose stacks from a panel menu.
+
+Requirements
+------------
+
+* Installed, configured and working docker-compose
+
+Usage
+-----
+
+* List available stacks
+* Bring stacks up and down
+* Edit docker-compose files
+* Add new stacks by docker-compose file
+* Delete stacks
+* Show logs for each stack
+* Show running containers in each stack
+* Open browsers to each containers open ports
---
+++ stacks@centurix/files/applet.js
@@ -0,0 +1,166 @@
+const Applet = imports.ui.applet;
+const PopupMenu = imports.ui.popupMenu;
+const Lang = imports.lang;
+const Settings = imports.ui.settings;
+const Gtk = imports.gi.Gtk;
+const MessageTray = imports.ui.messageTray;
+const Main = imports.ui.main;
+const St = imports.gi.St;
+const GLib = imports.gi.GLib;
+const Gettext = imports.gettext;
+const UUID = "stacks@centurix";
+
+const EDITOR = '/usr/bin/xed';
+
+const APPLET_FOLDER = global.userdatadir + "/applets/stacks@centurix/";
+
+const ICON_UP = APPLET_FOLDER + "icons/docker_compose_up_128x128.png";
+const ICON_DOWN = APPLET_FOLDER + "icons/docker_compose_down_128x128.png";
+const ICON_MISSING = APPLET_FOLDER + "icons/docker_compose_missing_128x128.png";
+
+/**
+ * TODO
+ * ====
+ * 
+ * Add configuration for docker-compose storage area
+ * List stacks
+ * Add/Remove stacks
+ * Up/down a stack
+ * Edit a docker-compose.yml file using xed
+ * Show the running containers in the stack
+ * Show the logs
+ * Show the open ports and open browsers
+ */
+
+/**
+ * L10n support
+ **/
+Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale")
+
+function _(str) {
+  return Gettext.dgettext(UUID, str);
+}
+
+/**
+ * Applet manager
+ **/
+function Stacks(metadata, orientation, panelHeight, instanceId) {
+	this.settings = new Settings.AppletSettings(this, UUID, instanceId);
+	this._init(orientation, panelHeight, instanceId);
+}
+
+Stacks.prototype = {
+	__proto__: Applet.IconApplet.prototype,
+
+	_init: function(orientation, panelHeight, instanceId) {
+		Applet.IconApplet.prototype._init.call(this, orientation, panelHeight, instanceId);
+
+		try {
+		    this.menuManager = new PopupMenu.PopupMenuManager(this);
+			this.menu = new Applet.AppletPopupMenu(this, orientation);
+			this.menuManager.addMenu(this.menu);
+
+			this._msgsrc = new MessageTray.SystemNotificationSource("Stacks");
+			Main.messageTray.add(this._msgsrc);
+
+			this.refreshApplet();
+		} catch (e) {
+			global.log(UUID + "::_init: " + e);
+		}
+	},
+
+	newIconMenuItem: function(icon, label, callback, options = {}) {
+		try {
+			let newItem = new PopupMenu.PopupIconMenuItem(label, icon, St.IconType.FULLCOLOR, options);
+			if (callback) {
+				newItem.connect("activate", Lang.bind(this, callback));
+			}
+			return newItem;
+		} catch(e) {
+			global.log(UUID + "::newIconMenuItem: " + e);
+		}
+	},
+
+	newMenuItem: function(label, callback, options = {}) {
+		let newItem = new PopupMenu.PopupMenuItem(label, options);
+		if (callback) {
+			newItem.connect("activate", Lang.bind(this, callback));
+		}
+		return newItem;
+	},
+
+	newSwitchMenuItem: function(label, state, callback) {
+		let newItem = new PopupMenu.PopupSwitchMenuItem(label, state);
+		if (callback) {
+			newItem.connect("activate", Lang.bind(this, callback));
+		}
+		return newItem;
+	},
+
+	newSeparator: function() {
+		return new PopupMenu.PopupSeparatorMenuItem();
+	},
+
+	on_applet_clicked: function(event) {
+		this.menu.toggle();
+	},
+
+    refreshApplet: function() {
+        this.updateApplet(true)
+	},
+
+	notification: function(message) {
+		let notification = new MessageTray.Notification(this._msgsrc, "Stacks", message);
+		notification.setTransient(true);
+		this._msgsrc.notify(notification);
+	},
+
+	transitionMenu: function(message, refresh = false) {
+		this.set_applet_icon_path(ICON_DOWN);
+		this.set_applet_tooltip(message);
+		this.menu.removeAll();
+		this.menu.addMenuItem(this.newIconMenuItem('dialog-information', message, null, {reactive: false}));
+		if (refresh) {
+			this.menu.addMenuItem(this.newIconMenuItem('view-refresh', _('Refresh this menu'), this.refreshApplet));
+		}
+	},
+
+	openBrowser: function(url) {
+		let matches = (new RegExp('\\("(.*?)"\\)')).exec(url);
+		if (matches && matches.length > 0) {
+			Main.Util.spawnCommandLine("xdg-open http://" + matches[1]);
+		}
+	},
+
+	updateApplet: function(exists, status) {
+		try {
+			if (!exists) {
+				this.set_applet_icon_path(ICON_MISSING);
+				this.set_applet_tooltip(_("Stacks: Docker compose missing or not configured."));
+				this.notification(_("Docker compose missing or not configured."));
+			}
+
+			this.set_applet_icon_path(ICON_UP);
+
+			this.menu.removeAll();
+
+            this.subMenuSites = new PopupMenu.PopupSubMenuMenuItem(_('Running containers'));
+
+            this.subMenuSites.menu.addMenuItem(this.newIconMenuItem('emblem-web', 'Stack #1', this.openBrowser));
+            this.subMenuSites.menu.addMenuItem(this.newIconMenuItem('emblem-web', 'Stack #2', this.openBrowser));
+            this.subMenuSites.menu.addMenuItem(this.newIconMenuItem('emblem-web', 'Stack #3', this.openBrowser));
+            this.subMenuSites.menu.addMenuItem(this.newIconMenuItem('emblem-web', 'Stack #4', this.openBrowser));
+
+            this.menu.addMenuItem(this.subMenuSites);
+            this.menu.addMenuItem(this.newSeparator());
+			this.menu.addMenuItem(this.newIconMenuItem('view-refresh', _('Refresh this menu'), this.refreshApplet));
+
+		} catch(e) {
+			global.log(UUID + "::updateMenu: " + e);
+		}
+	}
+}
+
+function main(metadata, orientation, panelHeight, instanceId) {
+	return new Stacks(metadata, orientation, panelHeight, instanceId);
+}
