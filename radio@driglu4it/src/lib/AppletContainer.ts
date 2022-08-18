59bebb1c9dedeac4a6d3fad752db8bafef4104a8
Jonathan Heard <49979415+jonath92@users.noreply.github.com>
2022-08-18 21:35:39 +0100

    fix radio scroll issue (#4422)
    Co-authored-by: jonathanheard <jonathan.heard@prodyna.com>

--- radio@driglu4it/src/lib/AppletContainer.ts
+++ radio@driglu4it/src/lib/AppletContainer.ts
@@ -1,68 +1,62 @@
-const { Applet, AllowedLayout } = imports.ui.applet
-const { EventType } = imports.gi.Clutter
+const { Applet, AllowedLayout } = imports.ui.applet;
+const { EventType } = imports.gi.Clutter;
 
 interface Arguments {
-    onClick: () => void,
-    onScroll: (scrollDirection: imports.gi.Clutter.ScrollDirection) => void,
-    onMiddleClick: () => void,
-    onRightClick: () => void,
-    onMoved: () => void,
-    onRemoved: () => void
+  onClick: () => void;
+  onScroll: (scrollDirection: imports.gi.Clutter.ScrollDirection) => void;
+  onMiddleClick: () => void;
+  onRightClick: () => void;
+  onMoved: () => void;
+  onRemoved: () => void;
 }
 
 export function createAppletContainer(args: Arguments) {
+  const { onClick, onScroll, onMiddleClick, onMoved, onRemoved, onRightClick } =
+    args;
 
-    const {
-        onClick,
-        onScroll,
-        onMiddleClick,
-        onMoved,
-        onRemoved,
-        onRightClick
-    } = args
+  const applet = new Applet(
+    __meta.orientation,
+    __meta.panel.height,
+    __meta.instanceId
+  );
 
+  let appletReloaded = false;
 
-    const applet = new Applet(__meta.orientation, __meta.panel.height, __meta.instanceId);
+  applet.on_applet_clicked = () => {
+    onClick();
+    return true;
+  };
 
-    let appletReloaded = false;
+  applet.on_applet_middle_clicked = () => {
+    onMiddleClick();
+    return true;
+  };
 
-    applet.on_applet_clicked = () => {
-        onClick()
-        return true
+  applet.setAllowedLayout(AllowedLayout.BOTH);
+
+  applet.on_applet_reloaded = function () {
+    appletReloaded = true;
+  };
+
+  applet.on_applet_removed_from_panel = function () {
+    appletReloaded ? onMoved() : onRemoved();
+    appletReloaded = false;
+  };
+
+  applet.actor.connect("event", (actor, event) => {
+    if (event.type() !== EventType.BUTTON_PRESS) return false;
+
+    if (event.get_button() === 3) {
+      onRightClick();
+      return true;
     }
+    return false;
+  });
 
-    applet.on_applet_middle_clicked = () => {
-        onMiddleClick()
-        return true
-    }
+  applet.actor.connect("scroll-event", (actor, event) => {
+    onScroll(event.get_scroll_direction());
+    return false;
+  });
 
-    applet.setAllowedLayout(AllowedLayout.BOTH)
-
-    applet.on_applet_reloaded = function () {
-        appletReloaded = true
-    }
-
-    applet.on_applet_removed_from_panel = function () {
-        appletReloaded ? onMoved() : onRemoved()
-        appletReloaded = false
-    }
-
-    applet.actor.connect('event', (actor, event) => {
-        if (event.type() !== EventType.BUTTON_PRESS) return false
-
-        if (event.get_button() === 3) {
-            onRightClick()
-            return true
-        }
-        return false
-
-    })
-
-    applet.actor.connect('scroll-event', (actor, event) => {
-        onScroll(event.get_scroll_direction())
-
-        return false
-    })
-
-    return applet
+  return applet;
 }
--- radio@driglu4it/src/lib/Slider.ts
+++ radio@driglu4it/src/lib/Slider.ts
@@ -1,143 +1,164 @@
-const { DrawingArea } = imports.gi.St
-const { cairo_set_source_color, grab_pointer, ungrab_pointer } = imports.gi.Clutter
+const { DrawingArea } = imports.gi.St;
+const { cairo_set_source_color } = imports.gi.Clutter;
 
 interface SliderArguments {
-    /** The value initally applied. Should be between 0 and 1. If the value is below 0, the slider value is set to 0. If the value is above 1, the slider value is set to 1 */
-    initialValue?: number,
-    /**
-     * @param newValue value between 0 and 1
-     */
-    onValueChanged?: (newValue: number) => void
+  /** The value initally applied. Should be between 0 and 1. If the value is below 0, the slider value is set to 0. If the value is above 1, the slider value is set to 1 */
+  initialValue?: number;
+  /**
+   * @param newValue value between 0 and 1
+   */
+  onValueChanged?: (newValue: number) => void;
 }
 
 export function createSlider(args: SliderArguments) {
+  const style_class = "popup-slider-menu-item";
 
-    const style_class = 'popup-slider-menu-item'
+  const { initialValue, onValueChanged } = args;
 
-    const {
-        initialValue,
-        onValueChanged
-    } = args
+  let value: number;
+  if (initialValue != null) value = limitToMinMax(initialValue);
 
-    let value: number
-    if (initialValue != null) value = limitToMinMax(initialValue)
+  const drawing = new DrawingArea({
+    style_class,
+    reactive: true,
+    x_expand: true,
+  });
 
-    const drawing = new DrawingArea({
-        style_class,
-        reactive: true,
-        x_expand: true
-    })
+  drawing.connect("repaint", () => {
+    const cr = drawing.get_context();
+    const themeNode = drawing.get_theme_node();
+    const [width, height] = drawing.get_surface_size();
 
-    drawing.connect('repaint', () => {
+    const handleRadius = themeNode.get_length("-slider-handle-radius");
 
-        const cr = drawing.get_context();
-        const themeNode = drawing.get_theme_node();
-        const [width, height] = drawing.get_surface_size()
+    const sliderHeight = themeNode.get_length("-slider-height");
 
-        const handleRadius = themeNode.get_length('-slider-handle-radius');
+    const sliderBorderWidth = themeNode.get_length("-slider-border-width");
+    const sliderBorderRadius = Math.min(width, sliderHeight) / 2;
 
-        const sliderHeight = themeNode.get_length('-slider-height');
+    const sliderBorderColor = themeNode.get_color("-slider-border-color");
+    const sliderColor = themeNode.get_color("-slider-background-color");
 
-        const sliderBorderWidth = themeNode.get_length('-slider-border-width');
-        const sliderBorderRadius = Math.min(width, sliderHeight) / 2;
+    const sliderActiveBorderColor = themeNode.get_color(
+      "-slider-active-border-color"
+    );
+    const sliderActiveColor = themeNode.get_color(
+      "-slider-active-background-color"
+    );
 
-        const sliderBorderColor = themeNode.get_color('-slider-border-color');
-        const sliderColor = themeNode.get_color('-slider-background-color');
+    const TAU = Math.PI * 2;
 
-        const sliderActiveBorderColor = themeNode.get_color('-slider-active-border-color');
-        const sliderActiveColor = themeNode.get_color('-slider-active-background-color');
+    const handleX = handleRadius + (width - 2 * handleRadius) * value;
 
-        const TAU = Math.PI * 2;
+    cr.arc(
+      sliderBorderRadius + sliderBorderWidth,
+      height / 2,
+      sliderBorderRadius,
+      (TAU * 1) / 4,
+      (TAU * 3) / 4
+    );
+    cr.lineTo(handleX, (height - sliderHeight) / 2);
+    cr.lineTo(handleX, (height + sliderHeight) / 2);
+    cr.lineTo(
+      sliderBorderRadius + sliderBorderWidth,
+      (height + sliderHeight) / 2
+    );
+    cairo_set_source_color(cr, sliderActiveColor);
+    cr.fillPreserve();
+    cairo_set_source_color(cr, sliderActiveBorderColor);
+    cr.setLineWidth(sliderBorderWidth);
+    cr.stroke();
 
-        const handleX = handleRadius + (width - 2 * handleRadius) * value;
+    cr.arc(
+      width - sliderBorderRadius - sliderBorderWidth,
+      height / 2,
+      sliderBorderRadius,
+      (TAU * 3) / 4,
+      (TAU * 1) / 4
+    );
+    cr.lineTo(handleX, (height + sliderHeight) / 2);
+    cr.lineTo(handleX, (height - sliderHeight) / 2);
+    cr.lineTo(
+      width - sliderBorderRadius - sliderBorderWidth,
+      (height - sliderHeight) / 2
+    );
+    cairo_set_source_color(cr, sliderColor);
+    cr.fillPreserve();
+    cairo_set_source_color(cr, sliderBorderColor);
+    cr.setLineWidth(sliderBorderWidth);
+    cr.stroke();
 
-        cr.arc(sliderBorderRadius + sliderBorderWidth, height / 2, sliderBorderRadius, TAU * 1 / 4, TAU * 3 / 4);
-        cr.lineTo(handleX, (height - sliderHeight) / 2);
-        cr.lineTo(handleX, (height + sliderHeight) / 2);
-        cr.lineTo(sliderBorderRadius + sliderBorderWidth, (height + sliderHeight) / 2);
-        cairo_set_source_color(cr, sliderActiveColor);
-        cr.fillPreserve();
-        cairo_set_source_color(cr, sliderActiveBorderColor);
-        cr.setLineWidth(sliderBorderWidth);
-        cr.stroke();
+    const handleY = height / 2;
 
-        cr.arc(width - sliderBorderRadius - sliderBorderWidth, height / 2, sliderBorderRadius, TAU * 3 / 4, TAU * 1 / 4);
-        cr.lineTo(handleX, (height + sliderHeight) / 2);
-        cr.lineTo(handleX, (height - sliderHeight) / 2);
-        cr.lineTo(width - sliderBorderRadius - sliderBorderWidth, (height - sliderHeight) / 2);
-        cairo_set_source_color(cr, sliderColor);
-        cr.fillPreserve();
-        cairo_set_source_color(cr, sliderBorderColor);
-        cr.setLineWidth(sliderBorderWidth);
-        cr.stroke();
+    const color = themeNode.get_foreground_color();
+    cairo_set_source_color(cr, color);
+    cr.arc(handleX, handleY, handleRadius, 0, 2 * Math.PI);
+    cr.fill();
 
-        const handleY = height / 2;
+    cr.$dispose();
+  });
 
-        const color = themeNode.get_foreground_color();
-        cairo_set_source_color(cr, color);
-        cr.arc(handleX, handleY, handleRadius, 0, 2 * Math.PI);
-        cr.fill();
+  drawing.connect("button-press-event", (actor, event) => {
+    event.get_device().grab(drawing);
 
-        cr.$dispose();
-    })
+    const motionId = drawing.connect("motion-event", (actor, event) => {
+      moveHandle(event);
+      return false;
+    });
 
-    drawing.connect('button-press-event', (actor, event) => {
-        grab_pointer(drawing)
+    const buttonReleaseId = drawing.connect(
+      "button-release-event",
+      (actor, event) => {
+        drawing.disconnect(buttonReleaseId);
+        drawing.disconnect(motionId);
+        event.get_device().ungrab();
+        return false;
+      }
+    );
 
-        const motionId = drawing.connect('motion-event', (actor, event) => {
-            moveHandle(event)
-            return false
-        })
+    moveHandle(event);
 
-        const buttonReleaseId = drawing.connect('button-release-event', () => {
-            drawing.disconnect(buttonReleaseId)
-            drawing.disconnect(motionId)
-            ungrab_pointer()
-            return false
-        })
+    return false;
+  });
 
-        moveHandle(event)
+  function moveHandle(event: imports.gi.Clutter.Event) {
+    const [absX, absY] = event.get_coords();
 
-        return false
-    })
+    const [sliderX, sliderY] = drawing.get_transformed_position();
+    const relX = absX - (sliderX || 0);
 
-    function moveHandle(event: imports.gi.Clutter.Event) {
+    const width = drawing.width;
+    const handleRadius = drawing
+      .get_theme_node()
+      .get_length("-slider-handle-radius");
 
-        const [absX, absY] = event.get_coords()
+    const newValue = (relX - handleRadius) / (width - 2 * handleRadius);
+    const newValueLimitToMinMax = limitToMinMax(newValue);
 
-        const [sliderX, sliderY] = drawing.get_transformed_position();
-        const relX = absX - (sliderX || 0);
+    setValue(newValueLimitToMinMax);
+  }
 
-        const width = drawing.width;
-        const handleRadius = drawing.get_theme_node().get_length('-slider-handle-radius');
+  function limitToMinMax(value: number) {
+    return Math.max(Math.min(value, 1), 0);
+  }
 
-        const newValue = (relX - handleRadius) / (width - 2 * handleRadius)
-        const newValueLimitToMinMax = limitToMinMax(newValue)
+  function setValue(newValue: number, silent = false) {
+    const correctedValue = limitToMinMax(newValue);
 
-        setValue(newValueLimitToMinMax)
-    }
+    if (correctedValue === value) return;
 
-    function limitToMinMax(value: number) {
-        return Math.max(Math.min(value, 1), 0);
-    }
+    value = correctedValue;
+    if (!silent) onValueChanged?.(value);
+    drawing.queue_repaint();
+  }
 
-    function setValue(newValue: number, silent = false) {
-        const correctedValue = limitToMinMax(newValue)
+  function getValue() {
+    return value;
+  }
 
-        if (correctedValue === value) return
-
-        value = correctedValue
-        if (!silent) onValueChanged?.(value)
-        drawing.queue_repaint()
-    }
-
-    function getValue() {
-        return value
-    }
-
-    return {
-        actor: drawing,
-        setValue,
-        getValue
-    }
+  return {
+    actor: drawing,
+    setValue,
+    getValue,
+  };
 }
