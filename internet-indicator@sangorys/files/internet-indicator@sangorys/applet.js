04858ff0f1c3e4832d83d0c92662f3ee09d213b5
sangorys <sangorys@free.fr>
2022-11-11 18:31:20 +0100

    Inform the user during ping. Bug fix : unfreeze during ping
    - Clean the code
    - During the ping, the tray icon changes and the toast message is updated
    - The ping don't freeze the system anymore because asynchrone ping command

--- internet-indicator@sangorys/files/internet-indicator@sangorys/applet.js
+++ internet-indicator@sangorys/files/internet-indicator@sangorys/applet.js
@@ -4,10 +4,13 @@ const Mainloop = imports.mainloop;
 const Lang = imports.lang;
 const PopupMenu = imports.ui.popupMenu;
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
 const St = imports.gi.St;
 const Settings = imports.ui.settings;
-const QUtils = require('./js/QUtils.js');
-const QIcon = QUtils.QIcon;
+//const QUtils = require('./js/QUtils.js');
+//const QIcon = QUtils.QIcon;
+const Main = imports.ui.main;
+
 
 // FOR TRANSLATION
 const Gettext = imports.gettext;
@@ -19,6 +22,76 @@ var message="init";
 var oldStderr="init";
 var debug = 0;
 var verbose = 0;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+const TEMP_DIRECTORY = "/tmp";
+const LOG_PATH = TEMP_DIRECTORY + "/" + UUID + ".log";
+
+var debug = false;
+var debugLevel=0;
+
+
+function logifLevel(context, text, level) {
+    if (level < 0) {debugLevel += level;}
+    logif(context, text);
+    if (level > 0) {debugLevel += level;}
+}
+
+function logif(context, text) {
+    if (context)
+        log(String(text));
+}
+
+function logLevel(text, level) {
+    if (level < 0) {debugLevel += level;}
+    log(text);
+    if (level > 0) {debugLevel += level;}
+}
+
+
+function log(text) {
+    if (debug){
+        let indent=""
+        //global.log(text);
+        for (let i=0; i<debugLevel;i++) {indent += "  "}
+        if (typeof(text) == "string")
+            fileAppend(indent + text.replaceAll("\n",indent + "\n"));
+        else
+            fileAppend(indent + String(text));
+    }
+}
+
+
+function logm(text) {
+    if (debug){
+        if (typeof(text) == "string")
+        global.log(text);
+        else
+            global.log(String(text));
+        //fileAppend(text);
+    }
+}
+
+
+function msgbox(text) {
+    if (debug){
+        //global.log(text);
+        ;
+    }
+}
+
+
+function fileAppend(text) {
+    //if (debug){
+        try {
+            let file = Gio.file_new_for_path(LOG_PATH);
+            let out = file.append_to (Gio.FileCreateFlags.NONE, null);
+            out.write (text, null);
+            out.write ("\n", null);
+            out.close(null);
+        } catch(error) { global.logError(error) };
+    //}
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -46,19 +119,15 @@ class InternetIndicatorApplet extends Applet.TextIconApplet {
         printDebug("Start internet-indicator applet");
 
         this.set_applet_tooltip(_("Initializing"));
+        this.set_applet_icon_name("test-internet");
+
+        
         //this.set_applet_label("NordVPN");
         this.connected = false;
         
-		this.update_interval = 5; // By default
-		this.update_interval_when_no_internet = 3; // By default
-		this.update_interval_when_internet = 10; // By default
-
-
-        // Reload BTN : for future improvements...
-        /*let reload_btn = new PopupMenu.PopupIconMenuItem(_("Reload Applet"), 'view-refresh-symbolic', QIcon.SYMBOLIC, {hover: true});
-        reload_btn.connect('activate', this.reloadApplet.bind(this));
-        this._applet_context_menu.addMenuItem(reload_btn);
-        this._applet_context_menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());*/
+        this.update_interval = 5; // By default
+        this.update_interval_when_no_internet = 3; // By default
+        this.update_interval_when_internet = 10; // By default
 
 
         try {
@@ -79,21 +148,21 @@ class InternetIndicatorApplet extends Applet.TextIconApplet {
             this._contentSection = new PopupMenu.PopupMenuSection();
             this.menu.addMenuItem(this._contentSection);
 			
-            /*// First item: // For futur use
+            // First item:
             let item = new PopupMenu.PopupIconMenuItem("Test Internet speed", "internet", St.IconType.FULLCOLOR);
 
             item.connect('activate', Lang.bind(this, function() {
                            this.testInternetFree();
                          }));
-            this.menu.addMenuItem(item); */
+            this.menu.addMenuItem(item);
 
 
             // START THE PERIODIC TASK TO CHECK INTERNET CONNECTION
-			this._update_loop();
-		}
-		catch (e) {
-			global.logError(e);
-		}
+            this._update_loop();
+        }
+        catch (e) {
+          log(e);
+        }
     }
 
 
@@ -145,16 +214,17 @@ class InternetIndicatorApplet extends Applet.TextIconApplet {
     // THIS FUNCTION SHOULD BE REMOVED SOON IF NOT USED
     _new_freq(){
     	global.log(this.update_interval);
-        if (this._updateLoopID) {
-			Mainloop.source_remove(this._updateLoopID);
-		}
-        this._update_loop();
+      if (this._updateLoopID) {
+        Mainloop.source_remove(this._updateLoopID);
+      }
+      this._update_loop();
     }
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     _update_loop() {
-		this._send_ping();	
+      log("_update_loop()");
+      this._send_ping();	
 		this._updateLoopID = Mainloop.timeout_add(this.update_interval * 1000, Lang.bind(this, this._update_loop));
 	}
 
@@ -163,56 +233,93 @@ class InternetIndicatorApplet extends Applet.TextIconApplet {
     _send_ping() {
         
 		// INIT
+    log("_send_ping()");
+    let TextMessage = "";
 		let [result, stdout, stderr] = ["", "", ""];
 		nbTry = nbTry + 1;
 		printDebug("Scan " + nbTry + " (period=" + this.update_interval + "s)");
 
 		
-        //////////////////////////////////////////////
-        // RUN PING
-		try {
-			[result, stdout, stderr] = GLib.spawn_command_line_sync("ping www.google.fr -c 1 -w 1000");
-			//printDebug("result=" + result);
-			//printDebug("stderr=" + stderr);
-			//printDebug("stdout=" + stdout);
-			//printDebug("oldStderr=" + oldStderr);
-		  }
-		catch (e) {
-			global.logError(e);
-		}
-		
-        
-        //////////////////////////////////////////////
-        // ANALYZE THE PING ANSWER
+    this.set_applet_icon_name("test-internet");
+    this.set_applet_tooltip(_("Testing Internet connexion in progress..."));
+
+
+    //////////////////////////////////////////////
+    // RUN PING
+    // Avoid to use spawn_command_line_sync because it freeze Linux during ping execution
+
+    //Todo : move this gLib code to _run_cmd(command)
+    let loop = GLib.MainLoop.new(null, false);
+
+
+    try {
+
+      let proc = Gio.Subprocess.new(
+          //['sleep', '1'],
+            ['ping', 'www.google.fr', '-c', '1', '-w', '1000'],
+            Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+        );
+
+        proc.communicate_utf8_async(null, null, (proc, res) => {
+            try {
+                [, stdout, stderr] = proc.communicate_utf8_finish(res);
+
+                if (proc.get_successful()) {
+                    log(stdout);
+                } else {
+                    throw new Error(stderr);
+                }
+            } catch (e) {
+                log(e);
+            } finally {
+                loop.quit();
+            }
+        });
+    } catch (e) {
+        log(e);
+    }
+
+    try {
+      loop.run();
+    } catch (e) {
+      log(e);
+  }
+  
+    //////////////////////////////////////////////
+    // ANALYZE THE PING ANSWER
 		//printDebug("analyse");  
 		
 		if (stderr == "")
 		{
-			//printDebug("ping OK");  
+      TextMessage = _("Internet OK");
+      this.set_applet_tooltip(TextMessage);
+      this.set_applet_icon_name("internet");
+
+      //printDebug("ping OK");  
 			if (stderr.toString() != oldStderr)
 			{
 			  //printDebug("connected");
 			  oldStderr = stderr.toString();
-              let TextMessage = _("Internet OK");
-			  Util.spawnCommandLine("/usr/sbin/notify-send '" + TextMessage + "'");
-              this.set_applet_tooltip(TextMessage);
+			  //Util.spawnCommandLine("/usr/sbin/notify-send '" + TextMessage + "'");
+        Main.notify("Internet indicator", TextMessage);
 			  this.update_interval = this.update_interval_when_internet;
-			  this.set_applet_icon_name("internet");
 			}
 			//else {printDebug("No change : still connected");}
 		}
 		else
 		{
 			//printDebug("ping failed");  
-			if (stderr.toString() != oldStderr)
+      TextMessage = _("No Internet");
+      this.set_applet_tooltip(TextMessage);
+      this.set_applet_icon_name("no-internet");
+
+      if (stderr.toString() != oldStderr)
 			{
 			  //printDebug("connected");
 			  oldStderr = stderr.toString();
-              let TextMessage = _("No Internet");
-			  Util.spawnCommandLine("/usr/sbin/notify-send '" + TextMessage + "'");
-              this.set_applet_tooltip(TextMessage);
+			  //Util.spawnCommandLine("/usr/sbin/notify-send '" + TextMessage + "'");
+        Main.notify("Internet indicator", TextMessage);
 			  this.update_interval = this.update_interval_when_no_internet;
-			  this.set_applet_icon_name("no-internet");
 			}
 			//else {printDebug("No change : still connected");}
 		}
