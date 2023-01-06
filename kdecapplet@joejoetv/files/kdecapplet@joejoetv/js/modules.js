123607d71a2de1cb045b17a18a7f54b4c5983bdd
Johannes Schöneberger <hannes1033@gmail.com>
2023-01-07 00:49:02 +0100

    kdecapplet@joejoetv: Implement more modules
    - Implement Connectivity, FindMyPhone and RequestPhoto modules
    - Change how the module classes and additional settings are stored

--- kdecapplet@joejoetv/files/kdecapplet@joejoetv/js/modules.js
+++ kdecapplet@joejoetv/files/kdecapplet@joejoetv/js/modules.js
@@ -8,11 +8,8 @@ const GLib = imports.gi.GLib;
 const Lang = imports.lang;
 const Gettext = imports.gettext;
 
-const AppletUtils = require("./js/commonUtils.js");
+const CommonUtils = require("./js/commonUtils.js");
 const Dialogs = require("./js/dialogs.js");
-
-const UUID = "kdecapplet@joejoetv";
-const KDEConnectDBusName = "org.kde.kdeconnect";
 
 const DeviceBatteryInterface = '\
 <node> \
@@ -27,12 +24,47 @@ const DeviceBatteryInterface = '\
 </node>';
 const DeviceBatteryProxy = Gio.DBusProxy.makeProxyWrapper(DeviceBatteryInterface);
 
+const DeviceConnectivityInterface = '\
+<node> \
+    <interface name="org.kde.kdeconnect.device.connectivity_report"> \
+        <property name="cellularNetworkType" type="s" access="read" /> \
+        <property name="cellularNetworkStrength" type="i" access="read" /> \
+        <signal name="refreshed"> \
+            <arg name="cellularNetworkType" type="s" direction="out" /> \
+            <arg name="cellularNetworkStrength" type="i" direction="out" /> \
+        </signal> \
+    </interface> \
+</node>';
+const DeviceConnectivityProxy = Gio.DBusProxy.makeProxyWrapper(DeviceConnectivityInterface);
+
+const DeviceFindMyPhoneInterface = '\
+<node> \
+    <interface name="org.kde.kdeconnect.device.findmyphone"> \
+        <method name="ring"> \
+        </method> \
+    </interface> \
+</node>';
+const DeviceFindMyPhoneProxy = Gio.DBusProxy.makeProxyWrapper(DeviceFindMyPhoneInterface);
+
+const DeviceRequestPhotoInterface = '\
+<node> \
+    <interface name="org.kde.kdeconnect.device.photo"> \
+        <signal name="photoReceived"> \
+            <arg name="fileName" type="s" direction="out" /> \
+        </signal> \
+        <method name="requestPhoto"> \
+            <arg name="fileName" type="s" direction="in" /> \
+        </method> \
+    </interface> \
+</node>';
+const DeviceRequestPhotoProxy = Gio.DBusProxy.makeProxyWrapper(DeviceRequestPhotoInterface);
+
 // l10n support
-Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale");
+Gettext.bindtextdomain(CommonUtils.UUID, GLib.get_home_dir() + "/.local/share/locale");
 
 /*
 function _(str) {
-    return Gettext.dgettext(UUID, str);
+    return Gettext.dgettext(CommonUtils.UUID, str);
 }
 */
 
@@ -42,23 +74,33 @@ function _(str) {
     return str;
 }
 
+// Type of the Module, specifies if and where it its menu items are added
+var ModuleType = {
+    INFO: 0,
+    ACTION: 1,
+    PASSIVE: 2
+}
+
 // Module base class
 class KDECModule {
 
-    static requiredKDECModules = [];
+    static REQUIRED_KDEC_PLUGINS = [];
+    static MODULE_ID = "";
 
-    constructor(id, device, compatMode) {
+    constructor(id, type, device, compatMode) {
         // Have to be set by subclass
         this.id = id;
+        this.type = type;
 
+        // Signal manager to manage singals connected in the applet
         this._signals = new SignalManager.SignalManager(null);
 
         this.device = device;
         this.compatMode = compatMode;
 
+        // Get settings for module for convenience
         let applet = this.device.getApplet();
-
-        this.settings = applet.getModuleSettings(this.id);
+        this.options = applet.getModuleOptions(this.id);
     }
 
     info(msg) {
@@ -77,38 +119,64 @@ class KDECModule {
         return this.id;
     }
 
-    getRequiredKDECModules() {
-        return this.requiredKDECModules;
+    getType() {
+        return this.type;
     }
 
     destroy() {
         this._signals.disconnectAllSignals();
+        this.info("D E S T R O Y !");
     }
 
     getMenuItems() {
-        // Do nothing in base class
+        return [];
     }
 }
 
-class BatteryModule extends KDECModule {
+/* Module Class Template
 
-    static requiredKDECModules = ["kdeconnect_battery"];
+class Module extends KDECModule {
+
+    static REQUIRED_KDEC_PLUGINS = [];
+    static MODULE_ID = "";
 
     constructor(device, compatMode) {
-        super("battery", device, compatMode);
+        super(Module.MODULE_ID, ModuleType.INFO, device, compatMode);
+
+        // TODO: Implement
+    }
+
+    destroy() {
+        super.destroy();
+    }
+
+    getMenuItems() {
+        return [];
+    }
+}
+
+*/
+
+class BatteryModule extends KDECModule {
+
+    static REQUIRED_KDEC_PLUGINS = ["kdeconnect_battery"];
+    static MODULE_ID = "battery";
+
+    constructor(device, compatMode) {
+        super(BatteryModule.MODULE_ID, ModuleType.INFO, device, compatMode);
 
         this.charge = 0;
         this.isCharging = false;
 
         try {
-            this.batteryProxy = new DeviceBatteryProxy(Gio.DBus.session, KDEConnectDBusName, "/modules/kdeconnect/devices/"+this.device.getID()+"/battery");
+            this.batteryProxy = new DeviceBatteryProxy(Gio.DBus.session, CommonUtils.KDECONNECT_DBUS_NAME, "/modules/kdeconnect/devices/"+this.device.getID()+"/battery");
             
             this.charge = this.batteryProxy.charge;
             this.isCharging = this.batteryProxy.isCharging;
 
             this._onRefreshed = this.batteryProxy.connectSignal("refreshed", this.onRefreshed.bind(this));
         } catch (error) {
-            this.error("Error while connecting to battery interface: "+error);
+            this.error("Error while connecting to 'battery' DBus interface: "+error);
         }
 
         // Create Menu Item
@@ -159,15 +227,16 @@ class BatteryModule extends KDECModule {
 
 class DeviceInfoModule extends KDECModule {
 
-    static requiredKDECModules = [];
+    static REQUIRED_KDEC_PLUGINS = [];
+    static MODULE_ID = "deviceinfo";
 
     constructor(device, compatMode) {
-        super("device", device, compatMode);
+        super(DeviceInfoModule.MODULE_ID, ModuleType.INFO, device, compatMode);
 
         try {
-            this._signals.connect(this.device, "type-changed",)
+            this._signals.connect(this.device, "type-changed",this.onTypeChanged.bind(this));
         } catch (error) {
-            this.error("Error while connecting to battery interface: "+error);
+            this.error("Error while connecting 'type-changed' signal: "+error);
         }
 
         // Create Menu Item
@@ -208,7 +277,205 @@ class DeviceInfoModule extends KDECModule {
     }
 }
 
+class ConnectivityModule extends KDECModule {
 
+    static REQUIRED_KDEC_PLUGINS = ["kdeconnect_connectivity_report"];
+    static MODULE_ID = "connectivity";
+
+    constructor(device, compatMode) {
+        super(ConnectivityModule.MODULE_ID, ModuleType.INFO, device, compatMode);
+
+        this.signalStrength = 0;
+        this.networkType = "";
+
+        try {
+            this.connectivityProxy = new DeviceConnectivityProxy(Gio.DBus.session, CommonUtils.KDECONNECT_DBUS_NAME, "/modules/kdeconnect/devices/"+this.device.getID()+"/connectivity_report");
+            
+            this.signalStrength = this.connectivityProxy.cellularNetworkStrength;
+            this.networkType = this.connectivityProxy.cellularNetworkType;
+
+            this._onRefreshed = this.connectivityProxy.connectSignal("refreshed", this.onRefreshed.bind(this));
+        } catch (error) {
+            this.error("Error while connecting to 'connectivity_report' DBus interface: "+error);
+        }
+
+        // Create Menu Item
+        this.connectivityMenuItem = new PopupMenu.PopupIconMenuItem(this._getLabelText(), this._getConnectivityIconName(), St.IconType.SYMBOLIC, {reactive: false});
+    }
+
+    _getConnectivityIconName() {
+        // Signal strength is in range 0 - 4
+
+        switch (this.signalStrength) {
+            case 0:
+                return "network-cellular-signal-none-symbolic";
+            case 1:
+                return "network-cellular-signal-low-symbolic";
+            case 2:
+                return "network-cellular-signal-ok-symbolic";
+            case 3:
+                return "network-cellular-signal-good-symbolic";
+            case 4:
+                return "network-cellular-signal-excellent-symbolic";
+        
+            default:
+                return "network-cellular-no-route-symbolic";
+        }
+    }
+
+    _getLabelText() {
+        let signalStrengthText = "";
+
+        switch (this.signalStrength) {
+            case 0:
+                signalStrengthText = _("No Signal");
+                break;
+            case 1:
+                signalStrengthText = _("Low");
+                break;
+            case 2:
+                signalStrengthText = _("Ok");
+                break;
+            case 3:
+                signalStrengthText = _("Good");
+                break;
+            case 4:
+                signalStrengthText = _("Excellent");
+                break;
+        
+            default:
+                signalStrengthText = "?";
+                break;
+        }
+
+        let networkText = ""
+
+        if (this.networkType == "Unknown") {
+            networkText = _("Unknown");
+        } else {
+            networkText = this.networkType;
+        }
+
+        return signalStrengthText + " (" + networkText + ")";
+    }
+
+    onRefreshed(networkType, networkStrength) {
+        this.networkType = networkType;
+        this.signalStrength = networkStrength;
+
+        this.connectivityMenuItem.label.set_text(this._getLabelText());
+        this.connectivityMenuItem.setIconSymbolicName(this._getConnectivityIconName());
+    }
+
+    destroy() {
+        super.destroy();
+        if (this.connectivityProxy && this._onRefreshed) {
+            this.connectivityProxy.disconnectSignal(this._onRefreshed);
+        }
+    }
+
+    getMenuItems() {
+        return [this.connectivityMenuItem];
+    }
+}
+class FindMyPhoneModule extends KDECModule {
+
+    static REQUIRED_KDEC_PLUGINS = ["kdeconnect_findmyphone"];
+    static MODULE_ID = "findmyphone";
+
+    constructor(device, compatMode) {
+        super(FindMyPhoneModule.MODULE_ID, ModuleType.ACTION, device, compatMode);
+
+        try {
+            this.findMyPhoneProxy = new DeviceFindMyPhoneProxy(Gio.DBus.session, CommonUtils.KDECONNECT_DBUS_NAME, "/modules/kdeconnect/devices/"+this.device.getID()+"/findmyphone");
+        } catch (error) {
+            this.error("Error while connecting to 'findmyphone' DBus interface: "+error);
+        }
+
+        // Create Menu Item
+        this.findMyPhoneMenuItem = new PopupMenu.PopupIconMenuItem(_("Ring device"), "audio-volume-high-symbolic", St.IconType.SYMBOLIC, {});
+        let findMyPhoneMenuItemTooltip = new Tooltips.Tooltip(this.findMyPhoneMenuItem.actor, _("Click to ring the device"));
+        this._signals.connect(this.findMyPhoneMenuItem, "activate", this.ringDevice.bind(this));
+    }
+    
+    ringDevice() {
+        try {
+            this.findMyPhoneProxy.ringSync();
+        } catch (error) {
+            this.error("Error sending ring command: "+error);
+        }
+    }
+
+    getMenuItems() {
+        return [this.findMyPhoneMenuItem];
+    }
+}
+
+class RequestPhotoModule extends KDECModule {
+
+    static REQUIRED_KDEC_PLUGINS = ["kdeconnect_photo"];
+    static MODULE_ID = "requestphoto";
+
+    constructor(device, compatMode) {
+        super(RequestPhotoModule.MODULE_ID, ModuleType.INFO, device, compatMode);
+
+        try {
+            this.requestPhotoProxy = new DeviceRequestPhotoProxy(Gio.DBus.session, CommonUtils.KDECONNECT_DBUS_NAME, "/modules/kdeconnect/devices/"+this.device.getID()+"/photo");
+
+            this._onPhotoReceived = this.requestPhotoProxy.connectSignal("photoReceived", this.onPhotoReceived.bind(this));
+        } catch (error) {
+            this.error("Error while connecting to 'connectivity_report' DBus interface: "+error);
+        }
+
+        // Create Menu Item
+        this.requestPhotoMenuItem = new PopupMenu.PopupIconMenuItem(_("Request Photo"), "camery-photo-symbolic", St.IconType.SYMBOLIC, {});
+        let requestPhotoMenuItemTooltip = new Tooltips.Tooltip(this.requestPhotoMenuItem.actor, _("Click to request a photo from the device"));
+        this._signals.connect(this.requestPhotoMenuItem, "activate", function() {
+            Dialogs.openReceivePhotoDialog(this.device.getApplet().metadata, this.device.getName(), this.requestPhotoCallback.bind(this));
+        });
+    }
+
+    requestPhotoCallback(status, filename, stderr) {
+        switch (status) {
+            case Dialogs.DialogStatus.SUCCESS:
+                try {
+                    this.requestPhotoProxy.requestPhotoSync(filename);
+                } catch (error) {
+                    this.error("Error while sending photo request to DBus service: "+error);
+                }
+                break;
+
+            case Dialogs.DialogStatus.CANCEL:
+                this.info("Dialog for selecting photo save location was canceled.");
+                break;
+        
+            default:
+                // Error
+                this.error("Error while opening photo save location dialog: "+stderr);
+                break;
+        }
+    }
+
+    onPhotoReceived(fileName) {
+        this.info("Received Photo from device: "+fileName);
+    }
+
+    destroy() {
+        super.destroy();
+        if (this.requestPhotoProxy && this._onPhotoReceived) {
+            this.requestPhotoProxy.disconnectSignal(this._onPhotoReceived);
+        }
+    }
+
+    getMenuItems() {
+        return [this.connectivityMenuItem];
+    }
+}
+
+
+
+
+/*
 const SUPPORTED_MODULES = [
     "battery",
     "deviceinfo",
@@ -236,13 +503,28 @@ const ADDITIONAL_MODULE_SETTING = {
     "sms": [],
     "telephony": []
 };
+*/
 
-let moduleClasses = {
-    "battery": BatteryModule,
-    "deviceinfo": DeviceInfoModule
-}
+// Register module classes
+let moduleClasses = {}
 
-let additionalSettings = {
-    "battery": [],
-    "deviceinfo": []
-}
+moduleClasses[BatteryModule.MODULE_ID] = BatteryModule;
+moduleClasses[DeviceInfoModule.MODULE_ID] = DeviceInfoModule;
+moduleClasses[ConnectivityModule.MODULE_ID] = ConnectivityModule;
+moduleClasses[FindMyPhoneModule.MODULE_ID] = FindMyPhoneModule;
+moduleClasses[RequestPhotoModule.MODULE_ID] = RequestPhotoModule;
+
+// Register additional settings
+
+let additionalSettings = {}
+
+// TODO: actually add aditional settings
+
+additionalSettings[BatteryModule.MODULE_ID] = [];
+additionalSettings[DeviceInfoModule.MODULE_ID] = [];
+additionalSettings[ConnectivityModule.MODULE_ID] = [];
+additionalSettings[FindMyPhoneModule.MODULE_ID] = [];
+additionalSettings[RequestPhotoModule.MODULE_ID] = [];
+
+// Create array of module IDs
+let modules = Object.keys(moduleClasses);
