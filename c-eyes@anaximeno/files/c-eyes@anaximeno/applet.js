3dc227e465a890e8a52d749d3f43005248701ef6
anaximeno <anaximenobrito@gmail.com>
2023-01-24 13:50:30 -0100

    fix: repaint immediately after eye settings changed

--- c-eyes@anaximeno/files/c-eyes@anaximeno/applet.js
+++ c-eyes@anaximeno/files/c-eyes@anaximeno/applet.js
@@ -352,6 +352,7 @@ class Eye extends Applet.Applet {
         const margin = 2 * this.eye_margin;
         this.area.set_width(EYE_AREA_WIDTH + margin);
         this.area.set_height(EYE_AREA_HEIGHT + margin);
+        this.area.queue_repaint();
     }
 
     setMouseCircleActive(enabled) {
