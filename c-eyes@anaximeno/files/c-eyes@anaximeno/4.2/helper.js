58775d65f0295744d2ec2b73ebce94197337c6a0
anaximeno <anaximenobrito@gmail.com>
2023-02-26 21:08:35 -0100

    [c-eyes@anaximeno] v1.10.9 - new features, fixes, performance updates
    * New eye modes
    
    * New click animations
    
    * Improved performance
    
    * New configurations

--- c-eyes@anaximeno/files/c-eyes@anaximeno/4.2/helper.js
+++ c-eyes@anaximeno/files/c-eyes@anaximeno/4.2/helper.js
@@ -1,18 +1,43 @@
-const GLib = imports.gi.GLib;
+/* helper.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+'use strict';
 
-function debounce(fn, timeout, options = {priority: GLib.PRIORITY_DEFAULT}) {
-    let sourceId = null;
+const Util = imports.misc.util;
 
-    return function (...args) {
-        const debouncedFunc = () => {
-            sourceId = null;
-            fn.apply(this, args);
-        };
+class Debouncer {
+    constructor() {
+        this._sourceId = 0;
+    }
 
-        if (sourceId) {
-            GLib.Source.remove(sourceId);
+    debounce(fn, timeout) {
+        return function (...args) {
+            if (this._sourceId > 0) {
+                Util.clearTimeout(this._sourceId);
+            }
+    
+            this._sourceId = Util.setTimeout(() => {
+                if (this._sourceId > 0) {
+                    Util.clearTimeout(this._sourceId);
+                    this._sourceId = 0;
+                }
+    
+                fn.apply(this, args);
+            }, timeout);
         }
-
-        sourceId = GLib.timeout_add(options.priority, timeout, debouncedFunc);
     }
 }
