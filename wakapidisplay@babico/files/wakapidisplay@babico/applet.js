c4ca15d689d70b2dc4b24923064c61d6c37d9554
Müslüm Barış Korkmazer <muslumbaris@ngxstorage.com>
2023-07-26 11:49:32 +0300

    Soup3 support added

--- wakapidisplay@babico/files/wakapidisplay@babico/applet.js
+++ wakapidisplay@babico/files/wakapidisplay@babico/applet.js
@@ -1,26 +1,9 @@
 const Applet = imports.ui.applet;
 const Mainloop = imports.mainloop;
-const Soup = imports.gi.Soup;
 const Settings = imports.ui.settings;
 const Util = imports.misc.util;
-
-const _httpSession = new Soup.SessionAsync();
-Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
-
-const makeHttpRequest = function (method, uri, cb) {
-	uri = uri.replace(/([^:])\/{2,}/, '$1/');
-	return new Promise((resolve, reject) => {
-		const request = Soup.Message.new(method, uri);
-		request.request_headers.append('accept', 'application/json');
-		_httpSession.queue_message(request, (_httpSession, message) => {
-			if (message.status_code === 200) {
-				cb(resolve, message);
-			} else {
-				reject(`Failed to acquire request (${message.status_code})`);
-			}
-		});
-	});
-}
+const { HttpLib } = require("./lib/httpLib.js");
+const http = new HttpLib();
 
 function WakapiDisplay(metadata, orientation, panel_height, instance_id) {
 	this._init(metadata, orientation, panel_height, instance_id);
@@ -92,12 +75,10 @@ WakapiDisplay.prototype = {
 		}
 	},
 
-	requestStatusBar: function () {
+	requestStatusBar: async function () {
 		// make the request to the status bar endpoint
-		return makeHttpRequest(
-			'GET', `${this.apiLink}/api/users/current/statusbar/today?api_key=${this.apiKey}`,
-			(resolve, message) => resolve(JSON.parse(message.response_body.data))
-		);
+		const response = await http.LoadJsonAsync(`${this.apiLink}/api/users/current/statusbar/today`, { "api_key": this.apiKey }, null, "GET")
+		return response.Data;
 	},
 
 	updateUI: function () {
