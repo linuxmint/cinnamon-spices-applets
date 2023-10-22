e134697dbc27dfafe164fe6c35285d3c0a4422b9
claudiux <claude.clerc@gmail.com>
2023-10-23 01:59:42 +0200

    v0.0.2 - Allows translations + French translation

--- moonphase@techi-freki/files/moonphase@techi-freki/js/moon.js
+++ moonphase@techi-freki/files/moonphase@techi-freki/js/moon.js
@@ -1,12 +1,24 @@
 const SunCalc = require('./lib/suncalc');
 const { DefaultIconSet, AltIconSet } = require('./js/iconSet');
 
+const UUID = "moonphase@techi-freki";
+const { get_home_dir } = imports.gi.GLib;
+const Gettext = imports.gettext;
+Gettext.bindtextdomain(UUID, get_home_dir() + "/.local/share/locale")
+
 class Moon {
     constructor(currentDate, useAltIcons) {
         this.currentDate = currentDate;
         this.age = this._getAge();
         this.currentPhaseIcon = this._getCurrentPhaseIcon(useAltIcons);
         this.currentPhaseName = this._getCurrentPhaseName();
+    }
+    // translation
+    _(str) {
+        let translated = Gettext.dgettext(UUID, str);
+        if (translated !== str)
+            return translated;
+        return str;
     }
     _getAge() {
         return SunCalc.getMoonIllumination(this.currentDate).phase;
@@ -23,14 +35,14 @@ class Moon {
     _getCurrentPhaseName() {
         const age = Math.round(this.age * 28) / 28;
 
-        if (age === 0) return 'New Moon';
-        if (age < 0.25) return 'Waxing Crescent';
-        if (age === 0.25) return 'First Quarter';
-        if (age < 0.5) return 'Waxing Gibbous';
-        if (age === 0.5) return 'Full Moon';
-        if (age < 0.75) return 'Waning Gibbous';
-        if (age === 0.75) return 'Last Quarter';
-        if (age <= 1) return 'Waning Crescent';
-        return 'New Moon';
+        if (age === 0) return this._("New Moon");
+        if (age < 0.25) return this._("Waxing Crescent");
+        if (age === 0.25) return this._("First Quarter");
+        if (age < 0.5) return this._("Waxing Gibbous");
+        if (age === 0.5) return this._("Full Moon");
+        if (age < 0.75) return this._("Waning Gibbous");
+        if (age === 0.75) return this._("Last Quarter");
+        if (age <= 1) return this._("Waning Crescent");
+        return this._("New Moon");
     }
 }
