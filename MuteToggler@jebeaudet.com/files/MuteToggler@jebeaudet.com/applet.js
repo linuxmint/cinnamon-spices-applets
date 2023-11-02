25db7f18abdd61bc74116f705690b5a20921f7ab
sphh <s.p.helma@gmx.net>
2023-11-02 13:16:15 +0000

    [MuteToggler@jebeaudet.com] Automatically decides which 'amixer' parameter to use (Closes #3979) (#5090)
    * Puts the applet name first in log entries.
    
    * Uses 'sh' instead of 'bash'.
    
    * Uses 'TextDecoder' instead of 'toString'.
    
    * Tries two versions of 'amixer' commands. (Closes #3979)
    
    On some installations it is not enough to call 'amixer'. There it an
    additional parameter is needed: 'amixer -D pulse' for amixer to find the
    proper channel. This PR checks, if it can find the 'Capture' channel
    with 'amixer' and if not successful with 'amixer -D pulse'.
    
    * Adds missing import.
    
    * Removes 'TextDecoder'.
    
    Apparently this is not needed in this applet.
    
    * Falls back to 'amixer' if no suitable parameter is found.
    
    As suggested by @jebeaudet in
    https://github.com/linuxmint/cinnamon-spices-applets/pull/5090#discussion_r1379477152.

--- MuteToggler@jebeaudet.com/files/MuteToggler@jebeaudet.com/applet.js
+++ MuteToggler@jebeaudet.com/files/MuteToggler@jebeaudet.com/applet.js
@@ -4,6 +4,7 @@ const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 const Util = imports.misc.util;
 const Settings = imports.ui.settings;
+const GLib = imports.gi.GLib;
 
 const UUID = 'MuteToggler@jebeaudet.com';
 
@@ -13,14 +14,14 @@ MyApplet.prototype = {
     
     _init: function(metadata, orientation, panel_height, instance_id) {
         try {
-            global.log("Initializing MuteToggler applet")
+            global.log("MuteToggler: Initializing applet")
             Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
             this.uuid = UUID;
             
             this.set_applet_tooltip(_("Click to mute/unmute microphone"));
 
             try {
-                global.log("Initializing MuteToggler Settings")
+                global.log("MuteToggler: Initializing settings")
                 this.settings = new Settings.AppletSettings(this, this.uuid, instance_id);
                 this.settings.bindProperty(Settings.BindingDirection.IN,
                                      "keybinding",
@@ -37,12 +38,26 @@ MyApplet.prototype = {
             if (this.settings) {
                 this.on_settings_changed();
             }
+
+            this.amixer = "amixer";
+            const parameters = ["", " -D pulse"];
+            for (let param of parameters) {
+                let cmd = this.amixer + param + " scontrols";
+                global.log("MuteToggler: Test mixer command '" + cmd + "'");
+                let [res, stdout] = GLib.spawn_command_line_sync(cmd);
+                var string = new TextDecoder().decode(stdout);
+                if (res && string.indexOf("'Capture'") != -1) {
+                    this.amixer += param;
+                    global.log("MuteToggler: Use mixer command '" + this.amixer + "'");
+                    break;
+                } 
+            }
             
             this.set_not_muted_icon();
             this.is_audio_muted();
             
             this.refresh_loop();
-            global.log("Done initializing MuteToggler applet")
+            global.log("MuteToggler: Done initializing applet")
         } catch (e) {
             global.logError(e);
         }
@@ -62,13 +77,13 @@ MyApplet.prototype = {
     is_audio_muted: function() {
         try {
             let cmd = [
-                "bash",
+                "sh",
                 "-c",
-                "amixer sget Capture"
+                this.amixer + " sget Capture"
                 ];
             Util.spawn_async(cmd, (stdout) => {
                 try{
-                    if(stdout.toString().indexOf("] [on]") != -1){
+                    if(stdout.indexOf("] [on]") != -1){
                         this.set_not_muted_icon();
                     } else {
                         this.set_muted_icon();
@@ -95,9 +110,9 @@ MyApplet.prototype = {
     on_applet_clicked: function(event) {
         try{
             let cmd = [
-                "bash",
+                "sh",
                 "-c",
-                "amixer set Capture toggle"
+                this.amixer + " set Capture toggle"
             ];
             Util.spawn_async(cmd, (stdout) => {
                 this.is_audio_muted();
