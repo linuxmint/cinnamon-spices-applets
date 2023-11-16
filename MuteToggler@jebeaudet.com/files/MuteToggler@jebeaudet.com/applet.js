d534f53fc8037c7995dc2e6d1739b53c9e09dfe5
claudiux <33965039+claudiux@users.noreply.github.com>
2023-11-16 04:21:45 +0100

    MuteToggler@jebeaudet.com: Fixes #5102 (#5103)

--- MuteToggler@jebeaudet.com/files/MuteToggler@jebeaudet.com/applet.js
+++ MuteToggler@jebeaudet.com/files/MuteToggler@jebeaudet.com/applet.js
@@ -6,33 +6,59 @@ const Util = imports.misc.util;
 const Settings = imports.ui.settings;
 const GLib = imports.gi.GLib;
 
-const UUID = 'MuteToggler@jebeaudet.com';
+const {to_string} = require("./lib/to-string");
 
+const APPNAME = "MuteToggler";
+const UUID = APPNAME + "@jebeaudet.com";
+
+var VERBOSE = true; // VERBOSE value will be changed according to this applet settings.
+
+/**
+ * Usage of log and logError:
+ * log("Any message here") to log the message only if VERBOSE is set to true.
+ * log("Any message here", true) to log the message even if VERBOSE is set to false.
+ * logError("Any error message") to log the error message regardless of the VERBOSE value.
+ */
+function log(message, alwaysLog=false) {
+  if (VERBOSE|| alwaysLog) global.log("[" + APPNAME + "]: " + message);
+}
+
+function logError(error) {
+  global.logError("\n[" + APPNAME + "]: " + error + "\n")
+}
 
 MyApplet.prototype = {
     __proto__: Applet.IconApplet.prototype,
-    
+
     _init: function(metadata, orientation, panel_height, instance_id) {
         try {
-            global.log("MuteToggler: Initializing applet")
+            this.settings = new Settings.AppletSettings(this, UUID, instance_id);
+            VERBOSE = this.settings.getValue("verbose");
+            log("Initializing applet");
             Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
-            this.uuid = UUID;
-            
+            //~ this.uuid = UUID;
+
             this.set_applet_tooltip(_("Click to mute/unmute microphone"));
 
             try {
-                global.log("MuteToggler: Initializing settings")
-                this.settings = new Settings.AppletSettings(this, this.uuid, instance_id);
                 this.settings.bindProperty(Settings.BindingDirection.IN,
                                      "keybinding",
                                      "keybinding",
                                      this.on_settings_changed,
-                                     null);      
+                                     null);
+                this.settings.bindProperty(Settings.BindingDirection.IN,
+                                     "verbose",
+                                     "verbose",
+                                     this.on_settings_changed,
+                                     null);
+
+                log("Initializing settings")
             } catch (e) {
-                global.logError(e);
-                global.log("Error initializing from Settings, continuing.")
+                logError(e);
+                log("Error initializing from Settings, continuing.", true);
                 this.settings = null;
                 this.keybinding = null;
+                this.verbose = true
             }
 
             if (this.settings) {
@@ -43,31 +69,37 @@ MyApplet.prototype = {
             const parameters = ["", " -D pulse"];
             for (let param of parameters) {
                 let cmd = this.amixer + param + " scontrols";
-                global.log("MuteToggler: Test mixer command '" + cmd + "'");
+                log("Test mixer command '" + cmd + "'");
                 let [res, stdout] = GLib.spawn_command_line_sync(cmd);
-                if (res && stdout.toString().indexOf("'Capture'") != -1) {
+                if (res && to_string(stdout).indexOf("'Capture'") != -1) {
                     this.amixer += param;
-                    global.log("MuteToggler: Use mixer command '" + this.amixer + "'");
+                    log("Use mixer command '" + this.amixer + "'");
                     break;
-                } 
+                }
             }
-            
+
             this.set_not_muted_icon();
             this.is_audio_muted();
-            
+
             this.refresh_loop();
-            global.log("MuteToggler: Done initializing applet")
+            log("Done initializing applet")
         } catch (e) {
-            global.logError(e);
+            logError(e);
         }
     },
 
     on_settings_changed: function() {
         if (this.keybinding != null) {
-            Main.keybindingManager.addHotKey(this.uuid, this.keybinding, Lang.bind(this, this.on_applet_clicked));
+            // "unassigned" is not a valid value:
+            if (this.keybinding === "unassigned")
+                this.keybinding ="";
+            if (this.keybinding.length > 0)
+                Main.keybindingManager.addHotKey(UUID, this.keybinding, Lang.bind(this, this.on_applet_clicked));
         }
+
+        VERBOSE = this.verbose;
     },
-    
+
     refresh_loop: function() {
         this.is_audio_muted();
         Mainloop.timeout_add(1000, Lang.bind(this, this.refresh_loop));
@@ -88,11 +120,11 @@ MyApplet.prototype = {
                         this.set_muted_icon();
                     }
                 }catch(e){
-                    global.logError(e);
+                    logError(e);
                 }
             });
         } catch (e) {
-            global.logError(e);
+            logError(e);
         }
     },
 
@@ -105,7 +137,7 @@ MyApplet.prototype = {
         this.current_icon = "muted";
         this.set_applet_icon_name("muted");
     },
-    
+
     on_applet_clicked: function(event) {
         try{
             let cmd = [
@@ -117,7 +149,7 @@ MyApplet.prototype = {
                 this.is_audio_muted();
         });
         }catch(e){
-            global.logError(e);
+            logError(e);
         }
     }
 };
