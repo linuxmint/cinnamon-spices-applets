746df36e7969b4e2e6d4642fd16c45e51089295c
Techi-Freki <techifreki@proton.me>
2024-04-29 11:22:33 -0400

    using element generator for currentPhaseUi

--- moonphase@techi-freki/files/moonphase@techi-freki/js/ui/currentPhaseUi.js
+++ moonphase@techi-freki/files/moonphase@techi-freki/js/ui/currentPhaseUi.js
@@ -1,6 +1,7 @@
-const { Align, BoxLayout, IconType, Icon, Label } = imports.gi.St;
+const { Align, BoxLayout } = imports.gi.St;
 const { ActorAlign } = imports.gi.Clutter;
 const { UiElement } = require('./js/ui/elements/uiElement');
+const { ElementGenerator } = require('./js/ui/elements/elementGenerator');
 
 class CurrentPhaseUi extends UiElement {
     constructor (app) {
@@ -10,36 +11,18 @@ class CurrentPhaseUi extends UiElement {
             x_align: ActorAlign.CENTER,
             y_align: Align.MIDDLE
         });
+        this.elementGenerator = new ElementGenerator();
     }
 
     create() {
-        // TODO: use element generator
-        const parent = new BoxLayout();
-        const iconParent = new BoxLayout({ style_class: 'margin-5' });
-        const infoParent = new BoxLayout({ vertical: true, style_class: 'margin-5; text-align-left' });
-        const illumParent = new BoxLayout({ style_class: 'padding-top-5-strict' });
-        const dateLabel = new Label({ text: new Date().toLocaleDateString() });
-        const timeLabel = new Label({ text: new Date().toLocaleTimeString() });
-        const illumLabel = new Label({ text: `${ Math.floor(this.app.moon.illumination.fraction * 100 * 100) / 100 }%` });
-        const phaseLabel = new Label({ text: this.app.moon.currentPhaseName, style_class: 'margin-bottom-5' });
-        const phaseIcon = new Icon({
-            icon_name: this.app.moon.currentPhaseIcon,
-            icon_type: IconType.SYMBOLIC,
-            icon_size: 64
-        });
+        const illumLabel = this.elementGenerator.generateLabel(`${ Math.floor(this.app.moon.illumination.fraction * 100 * 100) / 100 }%`);
+        const illumLayout = this.elementGenerator.generateLayout([illumLabel], false, 'padding-top-5-strict');
 
-        iconParent.add(phaseIcon);
-        illumParent.add(illumLabel);
+        const phaseLabel = this.elementGenerator.generateLabel(this.app.moon.currentPhaseName, 'margin-bottom-5');
+        const dateLabel = this.elementGenerator.generateLabel(new Date().toLocaleDateString());
+        const timeLabel = this.elementGenerator.generateLabel(new Date().toLocaleTimeString());
+        const infoLayout = this.elementGenerator.generateLayout([phaseLabel, dateLabel, timeLabel, illumLayout], true, 'margin-5; text-align-left');
 
-        infoParent.add(phaseLabel);
-        infoParent.add(dateLabel);
-        infoParent.add(timeLabel);
-        infoParent.add(illumParent);
-
-        parent.add(iconParent);
-        parent.add(infoParent);
-        parent.add(illumParent);
-
-        this.actor.add_actor(parent);
+        this.actor.add_actor(this.elementGenerator.generateElement(this.app.moon.currentPhaseIcon, 64, [infoLayout], 'margin-5'));
     }
 }
