9ce95d1a14b1890876000e5ba5cf7827cec52735
Gr3qi <floyd0122@gmail.com>
2024-07-27 10:20:01 +0100

    add full condition table

--- weather@mockturtl/files/weather@mockturtl/3.8/weather-applet.js
+++ weather@mockturtl/files/weather@mockturtl/3.8/weather-applet.js
@@ -17265,7 +17265,7 @@ function SwissMeteoIconToCondition(icon) {
             };
         case 102:
             return {
-                customIcon: "night-cloudy-symbolic",
+                customIcon: "night-alt-cloudy-symbolic",
                 main: _("Partly Cloudy"),
                 description: _("Partly Cloudy"),
                 icons: ["weather-few-clouds-night", "weather-few-clouds"]
@@ -17279,7 +17279,7 @@ function SwissMeteoIconToCondition(icon) {
             };
         case 103:
             return {
-                customIcon: "night-cloudy-symbolic",
+                customIcon: "night-alt-cloudy-symbolic",
                 main: _("Mostly Cloudy"),
                 description: _("Mostly Cloudy"),
                 icons: ["weather-clouds-night", "weather-few-clouds-night"]
@@ -17293,7 +17293,7 @@ function SwissMeteoIconToCondition(icon) {
             };
         case 104:
             return {
-                customIcon: "night-cloudy-symbolic",
+                customIcon: "night-alt-cloudy-symbolic",
                 main: _("Overcast"),
                 description: _("Overcast"),
                 icons: ["weather-overcast", "weather-clouds-night", "weather-clouds"]
@@ -17329,10 +17329,391 @@ function SwissMeteoIconToCondition(icon) {
             };
         case 107:
             return {
-                customIcon: "night-sleet-symbolic",
+                customIcon: "night-alt-sleet-symbolic",
                 main: _("Scattered Sleet"),
                 description: _("Scattered Sleet"),
                 icons: ["weather-freezing-rain"]
+            };
+        case 8:
+            return {
+                customIcon: "day-snow-symbolic",
+                main: _("Snow Showers"),
+                description: _("Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 108:
+            return {
+                customIcon: "night-alt-snow-symbolic",
+                main: _("Snow Showers"),
+                description: _("Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 9:
+            return {
+                customIcon: "day-showers-symbolic",
+                main: _("Showers"),
+                description: _("Showers"),
+                icons: ["weather-showers-day", "weather-showers"]
+            };
+        case 109:
+            return {
+                customIcon: "night-alt-showers-symbolic",
+                main: _("Showers"),
+                description: _("Showers"),
+                icons: ["weather-showers-night", "weather-showers"]
+            };
+        case 10:
+            return {
+                customIcon: "day-sleet-symbolic",
+                main: _("Sleet"),
+                description: _("Sleet"),
+                icons: ["weather-freezing-rain"]
+            };
+        case 110:
+            return {
+                customIcon: "night-alt-sleet-symbolic",
+                main: _("Sleet"),
+                description: _("Sleet"),
+                icons: ["weather-freezing-rain"]
+            };
+        case 11:
+            return {
+                customIcon: "day-snow-symbolic",
+                main: _("Snow Showers"),
+                description: _("Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 111:
+            return {
+                customIcon: "night-alt-snow-symbolic",
+                main: _("Snow Showers"),
+                description: _("Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 12:
+            return {
+                customIcon: "day-thunderstorm-symbolic",
+                main: _("Chance of Thunderstorms"),
+                description: _("Chance of Thunderstorms"),
+                icons: ["weather-storm"]
+            };
+        case 112:
+            return {
+                customIcon: "night-alt-thunderstorm-symbolic",
+                main: _("Chance of Thunderstorms"),
+                description: _("Chance of Thunderstorms"),
+                icons: ["weather-storm"]
+            };
+        case 13:
+            return {
+                customIcon: "day-thunderstorm-symbolic",
+                main: _("Thunderstorms"),
+                description: _("Thunderstorms"),
+                icons: ["weather-storm"]
+            };
+        case 113:
+            return {
+                customIcon: "night-alt-thunderstorm-symbolic",
+                main: _("Thunderstorms"),
+                description: _("Thunderstorms"),
+                icons: ["weather-storm"]
+            };
+        case 14:
+        case 114:
+            return {
+                customIcon: "rain-symbolic",
+                main: _("Light Rain"),
+                description: _("Light Rain"),
+                icons: ["weather-showers", "weather-showers"]
+            };
+        case 15:
+        case 115:
+            return {
+                customIcon: "sleet-symbolic",
+                main: _("Light Sleet"),
+                description: _("Light Sleet"),
+                icons: ["weather-freezing-rain"]
+            };
+        case 16:
+        case 116:
+            return {
+                customIcon: "snow-symbolic",
+                main: _("Light Snow Showers"),
+                description: _("Light Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 17:
+        case 117:
+            return {
+                customIcon: "rain-symbolic",
+                main: _("Intermittent Rain"),
+                description: _("Intermittent Rain"),
+                icons: ["weather-showers", "weather-showers", "weather-rain"]
+            };
+        case 18:
+        case 118:
+            return {
+                customIcon: "sleet-symbolic",
+                main: _("Intermittent Sleet"),
+                description: _("Intermittent Sleet"),
+                icons: ["weather-freezing-rain"]
+            };
+        case 19:
+        case 119:
+            return {
+                customIcon: "snow-symbolic",
+                main: _("Intermittent Snow"),
+                description: _("Intermittent Snow"),
+                icons: ["weather-snow"]
+            };
+        case 20:
+        case 120:
+            return {
+                customIcon: "rain-symbolic",
+                main: _("Rain"),
+                description: _("Rain"),
+                icons: ["weather-showers", "weather-rain"]
+            };
+        case 21:
+        case 121:
+            return {
+                customIcon: "sleet-symbolic",
+                main: _("Sleet"),
+                description: _("Sleet"),
+                icons: ["weather-freezing-rain"]
+            };
+        case 22:
+        case 122:
+            return {
+                customIcon: "snow-symbolic",
+                main: _("Snow"),
+                description: _("Snow"),
+                icons: ["weather-snow"]
+            };
+        case 23:
+        case 123:
+            return {
+                customIcon: "thunderstorm-symbolic",
+                main: _("Chance of Thunderstorms"),
+                description: _("Chance of Thunderstorms"),
+                icons: ["weather-storm"]
+            };
+        case 24:
+        case 124:
+            return {
+                customIcon: "thunderstorm-symbolic",
+                main: _("Thunderstorms"),
+                description: _("Thunderstorms"),
+                icons: ["weather-storm"]
+            };
+        case 25:
+        case 125:
+            return {
+                customIcon: "thunderstorm-symbolic",
+                main: _("Frequent Thunderstorms"),
+                description: _("Frequent Thunderstorms"),
+                icons: ["weather-storm"]
+            };
+        case 26:
+            return {
+                customIcon: "day-cloudy-symbolic",
+                main: _("High Clouds"),
+                description: _("High Clouds"),
+                icons: ["weather-clouds"]
+            };
+        case 126:
+            return {
+                customIcon: "night-alt-cloudy-symbolic",
+                main: _("High Clouds"),
+                description: _("High Clouds"),
+                icons: ["weather-clouds"]
+            };
+        case 27:
+        case 127:
+            return {
+                customIcon: "cloudy-symbolic",
+                main: _("Low Clouds"),
+                description: _("Low Clouds"),
+                icons: ["weather-clouds"]
+            };
+        case 28:
+            return {
+                customIcon: "fog-symbolic",
+                main: _("Fog"),
+                description: _("Fog"),
+                icons: ["weather-fog"]
+            };
+        case 29:
+            return {
+                customIcon: "day-showers-symbolic",
+                main: _("Scattered Showers"),
+                description: _("Scattered Showers"),
+                icons: ["weather-showers-scattered-day", "weather-showers-day", "weather-showers-scattered", "weather-showers"]
+            };
+        case 129:
+            return {
+                customIcon: "night-alt-showers-symbolic",
+                main: _("Scattered Showers"),
+                description: _("Scattered Showers"),
+                icons: ["weather-showers-scattered-night", "weather-showers-night", "weather-showers-scattered", "weather-showers"]
+            };
+        case 30:
+            return {
+                customIcon: "day-snow-symbolic",
+                main: _("Scattered Snow Showers"),
+                description: _("Scattered Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 130:
+            return {
+                customIcon: "night-alt-snow-symbolic",
+                main: _("Scattered Snow Showers"),
+                description: _("Scattered Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 31:
+            return {
+                customIcon: "day-sleet-symbolic",
+                main: _("Scattered Sleet"),
+                description: _("Scattered Sleet"),
+                icons: ["weather-freezing-rain"]
+            };
+        case 131:
+            return {
+                customIcon: "night-alt-sleet-symbolic",
+                main: _("Scattered Sleet"),
+                description: _("Scattered Sleet"),
+                icons: ["weather-freezing-rain"]
+            };
+        case 32:
+            return {
+                customIcon: "day-showers-symbolic",
+                main: _("Showers"),
+                description: _("Showers"),
+                icons: ["weather-showers-day", "weather-showers"]
+            };
+        case 132:
+            return {
+                customIcon: "night-alt-showers-symbolic",
+                main: _("Showers"),
+                description: _("Showers"),
+                icons: ["weather-showers-night", "weather-showers"]
+            };
+        case 33:
+            return {
+                customIcon: "day-rain-symbolic",
+                main: _("Heavy Rain"),
+                description: _("Heavy Rain"),
+                icons: ["weather-showers-day", "weather-showers"]
+            };
+        case 133:
+            return {
+                customIcon: "night-alt-rain-symbolic",
+                main: _("Heavy Rain"),
+                description: _("Heavy Rain"),
+                icons: ["weather-showers-night", "weather-showers"]
+            };
+        case 34:
+            return {
+                customIcon: "day-snow-symbolic",
+                main: _("Heavy Snow"),
+                description: _("Heavy Snow"),
+                icons: ["weather-snow"]
+            };
+        case 134:
+            return {
+                customIcon: "night-alt-snow-symbolic",
+                main: _("Heavy Snow"),
+                description: _("Heavy Snow"),
+                icons: ["weather-snow"]
+            };
+        case 35:
+        case 135:
+            return {
+                customIcon: "cloudy-symbolic",
+                main: _("Overcast and Dry"),
+                description: _("Overcast and Dry"),
+                icons: ["weather-clear"]
+            };
+        case 36:
+            return {
+                customIcon: "day-thunderstorm-symbolic",
+                main: _("Slightly Stormy"),
+                description: _("Slightly Stormy"),
+                icons: ["weather-storm"]
+            };
+        case 136:
+            return {
+                customIcon: "night-alt-thunderstorm-symbolic",
+                main: _("Slightly Stormy"),
+                description: _("Slightly Stormy"),
+                icons: ["weather-storm"]
+            };
+        case 37:
+            return {
+                customIcon: "day-snow-thunderstorm-symbolic",
+                main: _("Stormy Snow Showers"),
+                description: _("Stormy Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 137:
+            return {
+                customIcon: "night-alt-snow-thunderstorm-symbolic",
+                main: _("Stormy Snow Showers"),
+                description: _("Stormy Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 38:
+            return {
+                customIcon: "day-storm-showers-symbolic",
+                main: _("Thundery Showers"),
+                description: _("Thundery Showers"),
+                icons: ["weather-storm"]
+            };
+        case 138:
+            return {
+                customIcon: "night-alt-storm-showers-symbolic",
+                main: _("Thundery Showers"),
+                description: _("Thundery Showers"),
+                icons: ["weather-storm"]
+            };
+        case 39:
+            return {
+                customIcon: "day-snow-thunderstorm-symbolic",
+                main: _("Thundery Snow Showers"),
+                description: _("Thundery Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 139:
+            return {
+                customIcon: "night-alt-snow-thunderstorm-symbolic",
+                main: _("Thundery Snow Showers"),
+                description: _("Thundery Snow Showers"),
+                icons: ["weather-snow"]
+            };
+        case 40:
+        case 140:
+            return {
+                customIcon: "thunderstorm-symbolic",
+                main: _("Slightly Stormy"),
+                description: _("Slightly Stormy"),
+                icons: ["weather-storm"]
+            };
+        case 41:
+        case 141:
+            return {
+                customIcon: "thunderstorm-symbolic",
+                main: _("Slightly Stormy"),
+                description: _("Slightly Stormy"),
+                icons: ["weather-storm"]
+            };
+        case 42:
+        case 142:
+            return {
+                customIcon: "snow-storm-symbolic",
+                main: _("Thundery Snow Showers"),
+                description: _("Thundery Snow Showers"),
+                icons: ["weather-snow"]
             };
         default:
             logger_Logger.Error(`SwissMeteo Unknown icon: ${icon}`);
