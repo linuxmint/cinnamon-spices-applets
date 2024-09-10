768fcf8e13e8897c76ba03debb9375eb610fcbbd
kriegcc <github-kriegcc@krieg.cc>
2024-09-10 15:42:08 +0200

    fish@kriegcc: Update Metadata type
    Use correct  type from library.

--- fish@kriegcc/src/types/types.ts
+++ fish@kriegcc/src/types/types.ts
@@ -1,17 +1,4 @@
-// original definition, see applet.d.ts (imports.ui.applet, interface AppletMetadata)
-export type Metadata = {
-  uuid: string
-  name: string
-  description: string
-  "max-instances"?: number
-  version?: string
-  multiversion?: boolean
-  "cinnamon-version"?: string[]
-  state?: number
-  path: string
-  error?: string
-  force_loaded: boolean
-}
+export type Metadata = imports.ui.applet.AppletMetadata
 
 // representation of setting keys defined in settings-schema.json.
 export type AppletSettingsProps = {
