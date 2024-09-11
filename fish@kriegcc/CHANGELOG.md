85f9e178cd441bae80c47c2c72425d654590d956
kriegcc <github-kriegcc@krieg.cc>
2024-09-11 17:55:53 +0200

    fish@kriegcc: Version 0.1.1 (#6409)
    * Minor adjustment after initial release:
      - Add correct URL to spices website
      - Fix URLs in README for rendering on Spices website

--- fish@kriegcc/CHANGELOG.md
+++ fish@kriegcc/CHANGELOG.md
@@ -6,6 +6,13 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+## [0.1.1] - 2024-09-11
+
+### Fixed
+
+- Reference to this applet on Cinnamon Spices Website
+- Documentation and image references in README so they are correctly working when viewed on the Spices Website
+
 ## [0.1.0] - 2024-08-22
 
 ### Added
--- fish@kriegcc/src/consts/common.ts
+++ fish@kriegcc/src/consts/common.ts
@@ -2,7 +2,6 @@ export const UUID = "fish@kriegcc"
 
 export const KNOWN_USEFUL_PROGRAMS = ["ps", "who", "uptime", "tail"]
 
-// TODO: set correct ID after release
-export const FISH_APPLET_CINNAMON_SPICES_WEBSITE = "https://cinnamon-spices.linuxmint.com/applets/view/xy"
+export const FISH_APPLET_CINNAMON_SPICES_WEBSITE = "https://cinnamon-spices.linuxmint.com/applets/view/401"
 export const REPORT_BUGS_INSTRUCTIONS_WEBSITE =
   "https://github.com/linuxmint/cinnamon-spices-applets/blob/master/.github/CONTRIBUTING.md"
