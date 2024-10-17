d46b4b1afcd800172682e926ee6041aef9771149
claudiux <claude.clerc@gmail.com>
2024-10-17 19:55:02 +0200

    v7.2.0 - Fixes #6504

--- sound150@claudiux/files/sound150@claudiux/scripts/get_album_art.sh
+++ sound150@claudiux/files/sound150@claudiux/scripts/get_album_art.sh
@@ -27,8 +27,19 @@ XESAM_URL=$(playerctl -a metadata "xesam:url")
         exit 0
 }
 [[ -z $XESAM_URL ]] && exit 1
+#~ OLDDECFILE="${XDG_RUNTIME_DIR}/sound150Decoded"
+#~ OLDDECODED=""
+#~ [[ -f $OLDDECFILE ]] && OLDDECODED=$( cat $OLDDECFILE )
 DECODED=$(urldecode "${XESAM_URL:7}")
-#~ echo $DECODED
-ffmpeg -i "$DECODED" -an -c:v copy $PATHTOFILE > /dev/null 2>&1
+#~ echo -n $DECODED > $OLDDECFILE
+#~ [[ "$OLDDECODED"="$DECODED" ]] && exit 1
+[[ -d $ARTDIR ]] && rm -f $ARTDIR/*
+#ffmpeg -i "$DECODED" -an -c:v copy $PATHTOFILE > /dev/null 2>&1
+#duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$DECODED");
+DUREE=$( ffprobe  -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$DECODED" );
+duration=${DUREE%.*}
+THUMBTIME=$(date -d@$(( $duration / 3 )) -u +%H:%M:%S)
+ffmpeg -ss ${THUMBTIME}.000 -i "$DECODED" -vframes 1 $PATHTOFILE > /dev/null 2>&1
+
 echo -n $PATHTOFILE
 exit 0
