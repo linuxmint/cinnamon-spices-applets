5289698066c3e3c666a3fdda1c51a17367eaa35a
Rico <mailvonrico@gmail.com>
2024-11-16 18:34:04 +0100

    init popup menu

--- rssNewsticker@KopfDesDaemons/files/rssNewsticker@KopfDesDaemons/applet.js
+++ rssNewsticker@KopfDesDaemons/files/rssNewsticker@KopfDesDaemons/applet.js
@@ -5,7 +5,7 @@ const Gio = imports.gi.Gio;
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Settings = imports.ui.settings;
-
+const PopupMenu = imports.ui.popupMenu;
 
 function MyApplet(metadata, orientation, panel_height, instance_id) {
     this._init(metadata, orientation, panel_height, instance_id);
@@ -27,23 +27,24 @@ MyApplet.prototype = {
         this.rssURL = this.settings.getValue("rssURL") || "https://rss.nytimes.com/services/xml/rss/nyt/World.xml";
         this.tickerSeperator = this.settings.getValue("tickerSeperator") || "*******";
         this.refreshInterval = this.settings.getValue("refreshInterval") || 300;
-        this.tickerSpeed = this.settings.getValue("tickerSpeed") || 100;
+        this.tickerSpeed = this.settings.getValue("tickerSpeed") || 300;
 
         // Bind settings properties
-        const boundSettings = [
-            "rssURL",
-            "tickerSeperator",
-            "refreshInterval",
-            "tickerSpeed"
-        ]
-        boundSettings.forEach(setting => {
-            this.settings.bindProperty(Settings.BindingDirection.IN, setting, setting, this.on_settings_changed, null);
-        });
+        this.settings.bindProperty(Settings.BindingDirection.IN, 'rssURL', 'rssURL', this._updateFeed, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, 'tickerSeperator', 'tickerSeperator');
+        this.settings.bindProperty(Settings.BindingDirection.IN, 'refreshInterval', 'refreshInterval', this._updateFeed, null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, 'tickerSpeed', 'tickerSpeed');
 
         this._headlineIndex = 0;
         this._headlines = [];
         this._tickerText = "";
         this._tickerPosition = 0;
+        this._error = false;
+
+        // Create popup menu
+        this.menuManager = new PopupMenu.PopupMenuManager(this);
+        this.menu = new Applet.AppletPopupMenu(this, orientation);
+        this.menuManager.addMenu(this.menu);
 
         this._updateFeed();
         this._tickerLoop();
@@ -60,11 +61,16 @@ MyApplet.prototype = {
                 if (success) {
                     let feed = contents.toString();
                     this._parseFeed(feed);
+                    this._error = false;
                 } else {
                     global.logError("Could not load RSS-Feed");
+                    this._tickerText = _("Could not load RSS-Feed");
+                    this._error = true;
                 }
             } catch (e) {
                 global.logError("Error while loading RSS: " + e.message);
+                this._tickerText = _("Error while loading RSS: " + e.message);
+                this._error = true;
             }
         }));
         return true;
@@ -86,41 +92,81 @@ MyApplet.prototype = {
             });
         });
 
-        if (this._headlines.length > 0) {
-            this._headlineIndex = 0;
-            this._tickerText = this._headlines[0].title + " - ";
-            this._tickerPosition = 0;
+        this._buildMenu();
+    },
+
+    _buildMenu: function () {
+        // Menü zurücksetzen
+        this.menu.removeAll();
+
+        if (this._headlines.length === 0) {
+            let menuItem = new PopupMenu.PopupMenuItem("Keine Nachrichten verfügbar");
+            this.menu.addMenuItem(menuItem);
+            return;
         }
+
+        // PopupMenuSection als Container erstellen
+        let menuSection = new PopupMenu.PopupMenuSection();
+
+        // ScrollView erstellen und an den MenuSection Container anhängen
+        let scrollView = new St.ScrollView({
+            x_fill: true,
+            y_fill: true,
+            style_class: "popup-menu-content"  // Styling-Klasse für ScrollView
+        });
+        scrollView.set_policy(St.PolicyType.NEVER, St.PolicyType.AUTOMATIC);  // nur vertikales Scrollen erlauben
+
+        // BoxLayout als Container für die Menüeinträge innerhalb des ScrollView erstellen
+        let menuContainer = new St.BoxLayout({ vertical: true });
+        scrollView.add_actor(menuContainer);
+
+        // Menüelemente hinzufügen
+        this._headlines.forEach(item => {
+            let menuItem = new PopupMenu.PopupMenuItem(item.title);
+
+            let descriptionLabel = new St.Label({
+                text: item.description,
+                style_class: "popup-inactive-menu-item"
+            });
+            menuItem.actor.add_actor(descriptionLabel);
+
+            // Klick-Event zum Öffnen des Links
+            menuItem.connect("activate", () => {
+                Gio.app_info_launch_default_for_uri(item.link, null);
+            });
+
+            menuContainer.add_child(menuItem.actor);  // Menüeintrag in den Container hinzufügen
+        });
+
+        // ScrollView zum Menü-Abschnitt hinzufügen
+        menuSection.actor.add_actor(scrollView);
+
+        // Begrenzung der maximalen Größe des ScrollViews
+        scrollView.style = "max-height: 400px; max-width: 300px;";
+
+        // Menüabschnitt zum Menü hinzufügen
+        this.menu.addMenuItem(menuSection);
     },
 
     _tickerLoop: function () {
         if (this._headlines.length > 0) {
             this._tickerText = this._headlines.map(item => item.title).join(' ' + this.tickerSeperator + ' ');
+        }
 
-            // set window of 60 characters
-            const displayText = this._tickerText.substring(this._tickerPosition, this._tickerPosition + 60);
+        const displayText = this._tickerText.substring(this._tickerPosition, this._tickerPosition + 60);
 
-            // display text
-            this.set_applet_label(displayText);
-            this._tickerPosition += 1;
+        this.set_applet_label(displayText);
+        this._tickerPosition += 1;
 
-            // endless loop
-            if (this._tickerPosition >= this._tickerText.length) {
-                this._tickerPosition = 0;
-            }
+        if (this._tickerPosition >= this._tickerText.length) {
+            this._tickerPosition = 0;
         }
 
         Mainloop.timeout_add(this.tickerSpeed, Lang.bind(this, this._tickerLoop));
     },
 
-    // click on applet
     on_applet_clicked: function (event) {
-        if (this._headlines.length > 0) {
-            let headline = this._headlines[this._headlineIndex];
-
-            // open link in browser
-            Gio.app_info_launch_default_for_uri(headline.link, null);
-        }
+        this.menu.toggle();
     },
 
     on_applet_removed_from_panel: function () {
