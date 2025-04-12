ef1a9eb97405492fbb00721b4f3ea428ab135178
sophie-kuehn <sophie@posteo.de>
2025-04-12 23:15:59 +0200

    NVidia GPU Temperature Indicator: Added temperature unit settings and PO files

--- nvidia-temp@sophie-la-li/files/nvidia-temp@sophie-la-li/applet.js
+++ nvidia-temp@sophie-la-li/files/nvidia-temp@sophie-la-li/applet.js
@@ -14,21 +14,25 @@ NvGpuTempApplet.prototype = {
         Applet.TextApplet.prototype._init.call(this, orientation, instance_id);
 
         this.set_applet_tooltip(_("GPU Temperature"));
-        this.set_applet_label("?°");
+        this.set_applet_label("?");
 
         this.state = {};
         this.settings = new Settings.AppletSettings(this.state, metadata.uuid, instance_id);
-        this.settings.bindProperty(Settings.BindingDirection.IN, 'interval', 'interval');
+        this.settings.bindProperty(Settings.BindingDirection.IN, 'interval', 'interval', () => this.refresh(), null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, 'show-unit', 'showUnit', () => this.refresh(), null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, 'show-unit-letter', 'showUnitLetter', () => this.refresh(), null);
+        this.settings.bindProperty(Settings.BindingDirection.IN, 'use-fahrenheit', 'useFahrenheit', () => this.refresh(), null);
 
-        this.updateTemperature();
-        this.loopId = Mainloop.timeout_add(this.state.interval, () => this.updateTemperature());
+        this.refresh();
     },
 
-    on_settings_changed: function() {
+    refresh: function() {
         if (this.loopId > 0) {
             Mainloop.source_remove(this.loopId);
+            this.loopId = 0;
         }
-        this.loopId = 0;
+
+        this.updateTemperatureUnit();
         this.updateTemperature();
         this.loopId = Mainloop.timeout_add(this.state.interval, () => this.updateTemperature());
     },
@@ -44,8 +48,18 @@ NvGpuTempApplet.prototype = {
 
     updateTemperature: function() {
         let [result, stdout, stderr] = GLib.spawn_command_line_sync('nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits');
-        this.set_applet_label(stdout.toString().trim() + " °"); 
+        let value = stdout.toString().trim();
+        if (this.state.useFahrenheit) value = Math.round((value * 1.8) + 32);
+        this.set_applet_label(value + this.tempUnit); 
         return true;
+    },
+
+    updateTemperatureUnit: function() {
+        this.tempUnit = '';
+        if (!this.state.showUnit) return;
+        this.tempUnit += ' °';
+        if (!this.state.showUnitLetter) return;
+        this.tempUnit += this.state.useFahrenheit ? 'F' : 'C';
     }
 };
 
