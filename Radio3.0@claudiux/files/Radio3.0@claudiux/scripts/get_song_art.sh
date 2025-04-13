e7f6d6103b08b6d2782c3e795d096843a6b49bba
claudiux <claude.clerc@gmail.com>
2025-04-13 17:55:32 +0200

    Radio3.0 v3.3.1: The 'Try to download it from YT' menu option works again.

--- Radio3.0@claudiux/files/Radio3.0@claudiux/scripts/get_song_art.sh
+++ Radio3.0@claudiux/files/Radio3.0@claudiux/scripts/get_song_art.sh
@@ -6,10 +6,10 @@ SONG_ART_DIR=${XDG_RUNTIME_DIR}/AlbumArt/song-art
 [[ -d $SONG_ART_DIR ]] || mkdir -p $SONG_ART_DIR
 
 #~ yt-dlp ytsearch1:"${TITLE}" --get-id > /tmp/ytid
-#~ yt-dlp -I 2 ytsearch2:"${TITLE}" --get-id > ${XDG_RUNTIME_DIR}/ytid
+yt-dlp -I 2 ytsearch2:"${TITLE}" --get-id > ${XDG_RUNTIME_DIR}/ytid
 # num is a random integer between 1 and 2 (num contains 1 or 2):
-num=$((RANDOM%(2)+1))
-yt-dlp -I ${num} ytsearch${num}:"${TITLE}" --get-id > ${XDG_RUNTIME_DIR}/ytid
+#~ num=$((RANDOM%(2)+1))
+#~ yt-dlp -I ${num} ytsearch${num}:"${TITLE}" --get-id > ${XDG_RUNTIME_DIR}/ytid
 YTID=$(cat ${XDG_RUNTIME_DIR}/ytid)
 SONG_ART_FILE="$SONG_ART_DIR/R3SongArt$RANDOM$RANDOM"
 [[ -d $SONG_ART_DIR ]] && rm -f $SONG_ART_DIR/R3SongArt*
