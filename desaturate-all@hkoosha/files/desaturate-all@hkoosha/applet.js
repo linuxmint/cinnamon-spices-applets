43b8c5b76760160875d2824e196e4867e1391bde
Kevin Langman <klangman@gmail.com>
2025-04-27 15:04:25 -0400

    [desaturate-all@hkoosha] Add some new features
    1. Added "bedtime mode" where the user can configure an enable/disable time
    2. Added an option to restore the previous effect state on Cinnamon startup
    3. Added the ability to control the saturation amount (gray-scale 0% to 100%)

--- desaturate-all@hkoosha/files/desaturate-all@hkoosha/applet.js
+++ desaturate-all@hkoosha/files/desaturate-all@hkoosha/applet.js
@@ -4,6 +4,7 @@ const Settings = imports.ui.settings;
 const UUID = "desaturate-all@hkoosha";
 const Clutter = imports.gi.Clutter;
 const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
 
 function MyApplet() {
     this._init.apply(this, arguments);
@@ -12,7 +13,7 @@ function MyApplet() {
 MyApplet.prototype = {
     __proto__: Applet.IconApplet.prototype,
 
-    _init: function(aMetadata, aOrientation, aPanelHeight, aInstanceId) {
+    _init(aMetadata, aOrientation, aPanelHeight, aInstanceId) {
         Applet.IconApplet.prototype._init.call(this, aOrientation, aPanelHeight, aInstanceId);
 
         if (Applet.hasOwnProperty("AllowedLayout")) {
@@ -23,25 +24,104 @@ MyApplet.prototype = {
         this.set_applet_icon_symbolic_name("applications-graphics");
 
         this.settings = new Settings.AppletSettings(this, UUID, this.instance_id);
+
         this.settings.bind("keybinding", "keybinding", this.on_keybinding_changed);
+        this.settings.bind("saturation", "saturation", this.on_saturation_changed);
+        this.settings.bind("resume-on-startup", "resumeOnStartup");
+        this.settings.bind("state", "state");
+
+        this.settings.connect( "changed::automatic",         Lang.bind(this, this.on_automatic_changed));
+        this.settings.connect( "changed::start-timechooser", Lang.bind(this, this.on_time_changed));
+        this.settings.connect( "changed::end-timechooser",   Lang.bind(this, this.on_time_changed));
 
         this.on_keybinding_changed();
-    },
-
-    _toggleEffect: function() {
-        if (Main.uiGroup.has_effects() && Main.uiGroup.get_effects().indexOf(this.effect) > -1) {
-            Main.uiGroup.remove_effect(this.effect);
+        this.on_saturation_changed();
+        if (!this.settings.getValue("automatic")) {
+            if (this.resumeOnStartup && this.state)
+                this._toggleEffect();
         } else {
-            Main.uiGroup.add_effect(this.effect);
+           this._toggleEffect_based_on_time("init");
         }
     },
 
-    on_applet_clicked: function() {
+    _toggleEffect(enable = null) {
+        let effectEnabled = Main.uiGroup.has_effects() && Main.uiGroup.get_effects().indexOf(this.effect) > -1;
+        if (enable != true && effectEnabled) {
+            Main.uiGroup.remove_effect(this.effect);
+            this.settings.setValue("state", false);
+        } else if (enable != false && !effectEnabled){
+            Main.uiGroup.add_effect(this.effect);
+            this.settings.setValue("state", true);
+        }
+    },
+
+    _toggleEffect_based_on_time() {
+        if (this.toggleDelay) {
+            Mainloop.source_remove(this.toggleDelay);
+            this.toggleDelay = null;
+        }
+
+        if (!this.settings.getValue("automatic"))
+           return;
+
+        const date = new Date();
+        const enableAt = new Date();
+        const disableAt = new Date();
+
+        let enableTime = this.settings.getValue("start-timechooser");
+        enableAt.setHours( enableTime.h );
+        enableAt.setMinutes( enableTime.m );
+        enableAt.setSeconds( 0 );
+
+        let disableTime = this.settings.getValue("end-timechooser");
+        disableAt.setHours( disableTime.h );
+        disableAt.setMinutes( disableTime.m );
+        disableAt.setSeconds( 0 );
+
+        if (disableAt < enableAt)
+           disableAt.setDate( disableAt.getDate() + 1 );
+
+        let enable = (date < disableAt && date >= enableAt);
+        this._toggleEffect( enable );
+
+        let diffTime;
+        if (enable) {
+           diffTime = Math.abs(disableAt - date) / 1000;
+           //log( `Disabling in ${diffTime} seconds` );
+        } else {
+           diffTime = Math.abs(enableAt - date) / 1000;
+           //log( `Enabling in ${diffTime} seconds` );
+        }
+        this.toggleDelay = Mainloop.timeout_add_seconds(diffTime, () => {this.toggleDelay = null; this._toggleEffect_based_on_time();} );
+    },
+
+    on_automatic_changed(signal, key, oldValue, value) {
+        if (oldValue != value) {
+           this._toggleEffect_based_on_time();
+        }
+    },
+
+    on_time_changed(signal, key, oldValue, value) {
+       if (oldValue.h !== value.h || oldValue.m !== value.m) {
+          this._toggleEffect_based_on_time();
+       }
+    },
+
+    on_applet_clicked() {
         this._toggleEffect();
     },
 
     on_keybinding_changed() {
         Main.keybindingManager.addHotKey(UUID, this.keybinding, Lang.bind(this, this._toggleEffect));
+    },
+
+    on_saturation_changed() {
+        if (!this.satDelay) {
+           this.effect.set_factor((100-this.saturation)/100);
+        } else {
+           Mainloop.source_remove(this.satDelay);
+        }
+        this.satDelay = Mainloop.timeout_add(200, () => this.satDelay = null );
     }
 };
 
