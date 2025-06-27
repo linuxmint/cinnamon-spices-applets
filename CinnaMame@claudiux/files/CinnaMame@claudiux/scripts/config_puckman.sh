ecfe2426d34833d52d5a93fa933fbdfb07802cc8
claudiux <claude.clerc@gmail.com>
2025-06-27 19:32:20 +0200

    CinnaMame v1.0.0: Remove unused files

--- CinnaMame@claudiux/files/CinnaMame@claudiux/scripts/config_puckman.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### Create ~/.mame/cfg if necessary:
-mkdir -p $HOME/.mame/cfg
-### Pause 1 second:
-sleep 1
-### Copy puckman.zip at the right place:
-cp -f -a $HOME/.local/share/cinnamon/applets/CinnaMame@claudiux/files/puckman.cfg $HOME/.mame/cfg/puckman.cfg
-exit 0
--- CinnaMame@claudiux/files/CinnaMame@claudiux/scripts/install_puckman.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### Create ~/mame/roms if necessary:
-mkdir -p $HOME/mame/roms
-### Pause 1 second:
-sleep 1
-### Copy puckman.zip at the right place:
-cp -f -a $HOME/.local/share/cinnamon/applets/CinnaMame@claudiux/files/puckman.zip $HOME/mame/roms/puckman.zip
-exit 0
