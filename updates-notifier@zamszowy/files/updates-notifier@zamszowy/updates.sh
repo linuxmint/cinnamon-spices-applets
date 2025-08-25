a0527b192ff6d584b58922c2638c45f7690394bb
Tomasz <zamszowy1@gmail.com>
2025-08-25 19:39:49 +0200

    updates-notifier@zamszowy: Fix reporting updates when there are none (#7561)
    * Closes #7554

--- updates-notifier@zamszowy/files/updates-notifier@zamszowy/updates.sh
+++ updates-notifier@zamszowy/files/updates-notifier@zamszowy/updates.sh
@@ -7,15 +7,19 @@ readonly DIR
 
 case "$1" in
 check)
-    pkcon get-updates | awk '/^Results:/{flag=1; next} flag && NF' &> "$DIR"/updates
-    wc -l <"$DIR"/updates
+    if out=$(pkcon get-updates --plain 2>&1); then
+        awk '/^Results:/{flag=1; next} flag && NF' <<< "$out" >"$DIR"/updates
+        wc -l <"$DIR"/updates
+    else
+        echo 0
+    fi
     ;;
 view)
     /usr/bin/cjs "$DIR"/info-window.js "$(wc -l <"$DIR"/updates) updates" "$DIR"/updates
     ;;
 command)
     readonly cmd=$2
-    gnome-terminal --wait --  /usr/bin/bash -c "echo \"Executing $cmd\"; $cmd; echo -en \"\nDone - press enter to exit\"; read"
+    gnome-terminal --wait -- /usr/bin/bash -c "echo \"Executing $cmd\"; $cmd; echo -en \"\nDone - press enter to exit\"; read"
     ;;
 *)
     exit 1
