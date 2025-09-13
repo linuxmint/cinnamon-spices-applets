c4c73c3e039a197d0758bf23de5815e2653a47d7
Hans Peyrot <hans@eflow.com.mx>
2025-09-13 11:46:15 -0600

    add options, improve instructions

--- maximus-title@hanspr/README.md
+++ maximus-title@hanspr/README.md
@@ -15,5 +15,13 @@ This applet is desinged to work in pair with:
 * Edit the panel and reposition at the center of the panel
   * If in edit mode you do not have a window name active, move the mouse over the applets, until you see it light up and move to the center
 
+# Configuration options
+* Apply regex to title option
+  * It allow you to setup a regex on the title to clean it before display it
+  * If you don't know how to create e regex or are not sure of it use, leave it blank
+* Maximum title length
+  * Length of title so it fits better on your current panel width
+
 # Limitations
 * No support for multiple monitors
+* It will only work on top, bottom panels that have enogh with.
--- maximus-title@hanspr/files/maximus-title@hanspr/applet.js
+++ maximus-title@hanspr/files/maximus-title@hanspr/applet.js
@@ -1,27 +1,39 @@
-const Applet = imports.ui.applet;
-const SignalManager = imports.misc.signalManager;
+const Applet = imports.ui.applet
+const SignalManager = imports.misc.signalManager
+const Settings = imports.ui.settings;
 
 class MyApplet extends Applet.TextIconApplet {
     constructor(metadata, orientation, panelHeight, instanceId) {
         try {
-            super(orientation, panelHeight, instanceId);
-            this.metadata = metadata;
-            this.uuid = metadata.uuid;
-            this.orientation = orientation;
-            this.panelHeight = panelHeight;
-            this.instanceId = instanceId;
-            this.appletPath = metadata.path;
-            this.connectSignals();
+            super(orientation, panelHeight, instanceId)
+            this.metadata = metadata
+            this.uuid = metadata.uuid
+            this.orientation = orientation
+            this.panelHeight = panelHeight
+            this.instanceId = instanceId
+            this.appletPath = metadata.path
+            this.regex = null
+            this.bindSettings()
+            this.connectSignals()
+            this.lastTitle = ""
+            this._regexChanged()
             setTimeout(() => {
                 this.initialized = true
-            }, 500);
+            }, 500)
         } catch (e) {
-            global.logError(e);
+            global.logError(e)
         }
     }
 
+    bindSettings() {
+        this.settings = new Settings.AppletSettings(this, this.uuid, this.instanceId)
+
+        this.settings.bind("title-regex", "titleRegex", this._regexChanged)
+        this.settings.bind("title-len", "titleLength", this._lengthChange)
+    }
+
     connectSignals() {
-        this.signalManager = new SignalManager.SignalManager(null);
+        this.signalManager = new SignalManager.SignalManager(null)
         this.signalManager.connect(global.display, 'notify::focus-window', () => {
             let w = global.display.focus_window
             if (w) {
@@ -32,19 +44,43 @@ class MyApplet extends Applet.TextIconApplet {
             } else {
                 this._onTitleChange()
             }
-        }, this);
+        }, this)
     }
 
     _onTitleChange(title) {
-        if (title != undefined) {
-            title = title.substring(0, 60)
-        } else {
-            title = global.display.focus_window.get_title().substring(0, 60)
+        if (title == undefined) {
+            if (global.display.focus_window() == null) {
+                return
+            }
+            title = global.display.focus_window.get_title()
         }
-        this.set_applet_label(title);
+        if (this.lastTitle == title) {
+            return
+        }
+        this.lastTitle = title
+        if (this.regex != null) {
+            title = title.replace(this.regex, "")
+        }
+        title = title.substring(0, this.titleLength)
+        this.set_applet_label(title)
+    }
+
+    _lengthChange() {
+        this._onTitleChange(this.lastTitle + " ")
+    }
+
+    _regexChanged() {
+        if (this.titleRegex != "") {
+            try {
+                this.regex = new RegExp(this.titleRegex)
+            } catch (e) {
+                console.log("Regex error:", e)
+                this.regex = null
+            }
+        }
     }
 }
 
 function main(metadata, orientation, panelHeight, instanceId) {
-    return new MyApplet(metadata, orientation, panelHeight, instanceId);
+    return new MyApplet(metadata, orientation, panelHeight, instanceId)
 }
