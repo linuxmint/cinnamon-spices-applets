5a2b97f6858c1209983fbb19fe880b9264a6725c
Tomasz Nowak <zamszowy1@gmail.com>
2025-10-20 21:17:53 +0200

    updates-notifier@zamszowy: fix refreshing updates when firmware option is changed

--- updates-notifier@zamszowy/files/updates-notifier@zamszowy/applet.js
+++ updates-notifier@zamszowy/files/updates-notifier@zamszowy/applet.js
@@ -91,7 +91,7 @@ UpdatesNotifier.prototype = {
         this.settings.bind("level-2", "level2", this._update, null);
 
         this.settings.bind("refresh-when-no-updates", "refreshWhenNoUpdates", this._update, null);
-        this.settings.bind("show-firmware", "showFirmware", this._refreshUpdatesInfo, null);
+        this.settings.bind("show-firmware", "showFirmware", () => this._refreshUpdatesInfo(), null);
         this.settings.bind("show-window-on-click", "showWindowOnClick", this._update, null);
         this.settings.bind("commandUpdate-show", "commandUpdateShow", this._update, null);
         this.settings.bind("commandUpgrade", "commandUpgrade", null, null);
