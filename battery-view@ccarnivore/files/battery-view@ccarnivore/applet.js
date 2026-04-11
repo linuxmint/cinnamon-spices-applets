25643a25fde9187a2d970124432360fec38f4e5b
roman <roman.robel@exitb.de>
2026-04-12 00:16:22 +0200

    battery-view@ccarnivore: use GLib.get_user_data_dir() instead of GLib.get_home_dir() + hard coded directory

--- battery-view@ccarnivore/files/battery-view@ccarnivore/applet.js
+++ battery-view@ccarnivore/files/battery-view@ccarnivore/applet.js
@@ -9,7 +9,7 @@ const Gettext = imports.gettext;
 
 const UUID = "battery-view@ccarnivore";
 
-Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale");
+Gettext.bindtextdomain(UUID, GLib.get_user_data_dir() + "/locale");
 
 function _(str) {
     return Gettext.dgettext(UUID, str);
