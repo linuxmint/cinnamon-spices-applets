7ac05f913547dcda5c3994f4c7f4d8cd4be8a624
Hilyxx <w.lebb@outlook.com>
2026-07-13 08:41:51 +0200

    Use GLib.get_user_data_dir() instead

--- FM-Radio@hilyxx/files/FM-Radio@hilyxx/applet.js
+++ FM-Radio@hilyxx/files/FM-Radio@hilyxx/applet.js
@@ -10,7 +10,7 @@ const Settings = imports.ui.settings;
 const Gettext = imports.gettext;
 
 const UUID = "FM-Radio@hilyxx";
-Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale");
+Gettext.bindtextdomain(UUID, GLib.get_user_data_dir() + "/locale");
 
 function _(str) {
     return Gettext.dgettext(UUID, str);
