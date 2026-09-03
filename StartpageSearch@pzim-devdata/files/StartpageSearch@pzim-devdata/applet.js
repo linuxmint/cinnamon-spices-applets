0474526fb7ed7c4d435fcde26a3444dc1a2063af
pzim <52496172+pzim-devdata@users.noreply.github.com>
2026-09-03 18:55:52 +0200

    Update applet.js

--- StartpageSearch@pzim-devdata/files/StartpageSearch@pzim-devdata/applet.js
+++ StartpageSearch@pzim-devdata/files/StartpageSearch@pzim-devdata/applet.js
@@ -3,12 +3,19 @@ const Applet = imports.ui.applet;
 const St = imports.gi.St;
 const PopupMenu = imports.ui.popupMenu;
 const Util = imports.misc.util;
-const Lang = imports.lang;
+const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
+const Tooltips = imports.ui.tooltips;
 const Gettext = imports.gettext.domain("StartpageSearch@pzim-devdata");
 const _ = Gettext.gettext;
 
 const SEARCH_URL = "https://www.startpage.com/sp/search?query=";
+// si true : taper une URL dans le champ + Entrée ouvre aussi directement le lien
+const DETECT_URLS_IN_ENTRY = true;
+
+// détection d'URL : schéma explicite, www., ou domaine nu avec TLD valide
+const URL_RE =
+    /^((https?:\/\/)[^\s]+|(www\.)[^\s]+|[a-z0-9][a-z0-9-]*(\.[a-z0-9-]+)+\.[a-z]{2,}[^\s]*)$/i;
 
 function SearchApplet(metadata, orientation, panel_height, instance_id) {
     this._init(metadata, orientation, panel_height, instance_id);
@@ -21,22 +28,60 @@ SearchApplet.prototype = {
         Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
         this.set_applet_icon_path(metadata.path + "/icon2.svg");
         this.set_applet_tooltip(_("Startpage Search"));
+        this._focusDone = true;
+        this._known = {};
+        this._titles = {};
 
         this.menuManager = new PopupMenu.PopupMenuManager(this);
         this.menu = new Applet.AppletPopupMenu(this, orientation);
         this.menuManager.addMenu(this.menu);
 
+        let vbox = new St.BoxLayout({ vertical: true, style: "padding: 10px;" });
+
         this.entry = new St.Entry({
             name: "search-entry",
             hint_text: _("Search terms..."),
             track_hover: true,
-            can_focus: true
+            can_focus: true,
+            style: "width: 320px;"
         });
-        let bin = new St.Bin({ x_align: St.Align.MIDDLE, y_align: St.Align.MIDDLE });
-        bin.set_child(this.entry);
-        this.menu.addActor(bin);
-        this.entry.clutter_text.connect("activate",
-            () => this._search());
+        this.entry.clutter_text.connect("activate", () => this._search());
+        vbox.add(this.entry, { expand: true, x_fill: true });
+
+        let hbox = new St.BoxLayout({ vertical: false, style: "padding-top: 8px;" });
+
+        this.searchBtn = new St.Button({
+            reactive: true, track_hover: true, can_focus: true
+        });
+        this.searchBtn.set_child(new St.Icon({
+            icon_name: "system-search-symbolic", icon_size: 20
+        }));
+        this.searchBtn.connect("clicked", () => this._search());
+        hbox.add(this.searchBtn, { expand: true, x_fill: true });
+
+        this.pasteBtn = new St.Button({
+            reactive: true, track_hover: true, can_focus: true
+        });
+        this.pasteBtn.set_child(new St.Icon({
+            icon_name: "edit-paste-symbolic", icon_size: 20
+        }));
+        this.pasteBtn.connect("clicked", () => this._paste());
+        hbox.add(this.pasteBtn, { expand: true, x_fill: true });
+
+        this.pasteSearchBtn = new St.Button({
+            label: _("Paste & Search"),
+            reactive: true, track_hover: true, can_focus: true
+        });
+        this.pasteSearchBtn.connect("clicked", () => this._paste_search());
+        hbox.add(this.pasteSearchBtn, { expand: true, x_fill: true });
+
+        vbox.add(hbox, { expand: false, x_fill: true });
+
+        this.menu.addActor(vbox);
+
+        new Tooltips.Tooltip(this.searchBtn, _("Search"));
+        new Tooltips.Tooltip(this.pasteBtn, _("Paste clipboard"));
+        new Tooltips.Tooltip(this.pasteSearchBtn, _("Paste clipboard and search"));
     },
 
     on_applet_clicked: function () {
@@ -49,13 +94,107 @@ SearchApplet.prototype = {
         }
     },
 
+    _is_url: function (t) {
+        return (t || "").trim().length > 0 && URL_RE.test(t.trim());
+    },
+
+    _normalize: function (t) {
+        t = t.trim();
+        // domaine nu ("example.com") : xdg-open exige un schéma explicite
+        if (!/^https?:\/\//i.test(t)) t = "http://" + t;
+        return t;
+    },
+
+    _paste: function () {
+        St.Clipboard.get_default().get_text(St.ClipboardType.CLIPBOARD, (clip, txt) => {
+            if (txt && txt.length) {
+                this.entry.get_clutter_text().set_text(txt.trim());
+                global.stage.set_key_focus(this.entry);
+            }
+        });
+    },
+
+    _paste_search: function () {
+        St.Clipboard.get_default().get_text(St.ClipboardType.CLIPBOARD, (clip, txt) => {
+            let q = (txt || "").trim();
+            if (!q) return;
+            if (this._is_url(q)) {
+                this._launch_open(this._normalize(q));
+            } else {
+                this._launch(q);
+            }
+        });
+    },
+
     _search: function () {
         let q = this.entry.get_text().trim();
-        if (!q) return;
-        let url = SEARCH_URL + encodeURIComponent(q);
-        Util.spawn(["xdg-open", url]);
+        if (q) {
+            if (DETECT_URLS_IN_ENTRY && this._is_url(q)) {
+                this._launch_open(this._normalize(q));
+            } else {
+                this._launch(q);
+            }
+        }
         this.entry.set_text("");
         this.menu.close();
+    },
+
+    // ouvre une URL directe avec la même mécanique de focus
+    _launch_open: function (url) {
+        this._snapshot();
+        Util.spawn(["xdg-open", url]);
+        this._schedule_focus();
+    },
+
+    _launch: function (q) {
+        this._snapshot();
+        Util.spawn(["xdg-open", SEARCH_URL + encodeURIComponent(q)]);
+        this._schedule_focus();
+    },
+
+    _snapshot: function () {
+        this._known = {};
+        this._titles = {};
+        let actors = global.get_window_actors();
+        for (let i = 0; i < actors.length; i++) {
+            let w = actors[i].meta_window;
+            this._known[w.get_stable_sequence()] = true;
+            let t = (w.get_title() || "").trim();
+            if (t) this._titles[t] = true;
+        }
+    },
+
+    _schedule_focus: function () {
+        this._focusDone = false;
+        let delays = [500, 1000, 1500, 2200, 3000];
+        for (let i = 0; i < delays.length; i++) {
+            Mainloop.timeout_add(delays[i], () => {
+                this._focus_browser();
+                return false;   // exécution unique
+            });
+        }
+    },
+
+    _focus_browser: function () {
+        if (this._focusDone) return;
+        let actors = global.get_window_actors();
+        for (let i = 0; i < actors.length; i++) {
+            let w = actors[i].meta_window;
+            if (!(w.get_stable_sequence() in this._known)) {
+                Main.activateWindow(w, global.get_current_time());
+                this._focusDone = true;
+                return;
+            }
+        }
+        for (let i = 0; i < actors.length; i++) {
+            let w = actors[i].meta_window;
+            let t = (w.get_title() || "").trim();
+            if (t && !(t in this._titles)) {
+                Main.activateWindow(w, global.get_current_time());
+                this._focusDone = true;
+                return;
+            }
+        }
     }
 };
 
