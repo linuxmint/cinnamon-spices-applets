9e9cb6d32ed4d7bd66dcae2675e2f531613a1d01
Erick <17180367+woodpile72@users.noreply.github.com>
2026-09-10 09:33:06 -0500

    sound150@claudiux: Keep sliders within popup menu (#9034)

--- sound150@claudiux/files/sound150@claudiux/6.6/lib/balanceSlider.js
+++ sound150@claudiux/files/sound150@claudiux/6.6/lib/balanceSlider.js
@@ -30,6 +30,7 @@ class BalanceSlider extends PopupMenu.PopupSliderMenuItem {
         });
         if (this._slider)
             this.removeActor(this._slider);
+        this._slider.style = "min-width: 6em;";
         this.addActor(this.icon, {span: 0});
         this.addActor(this._slider, {
             span: -1,
